93f04b8d2f46be5c467aec085d5088df974db817
GitHub Action <actions@github.com>
2026-01-13 18:33:55 +0000

    data: update news and rates at Tue Jan 13 18:33:55 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "384,33"
     },
     "binance": {
-      "usdt": "600,00"
+      "usdt": "605,70"
     },
     "kontigo": {
       "usd": "N/A"
@@ -20,27 +20,27 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "El Nacional",
-      "title": "Detuvieron a 16 adolescentes en Anzoátegui acusados de celebrar captura de Maduro: fueron excarcelados con medidas cautelares\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/01/detuvieron-a-16-adolescentes-en-anzoategui-acusados-de-celebrar-captura-de-maduro-fueron-excarcelados-con-medidas-cautelares/",
+      "title": "Familiares de presos políticos siguen esperando mientras crece el temor de que se frenen las excarcelaciones\nPOLÍTICA",
+      "link": "https://www.elnacional.com/2026/01/familiares-de-presos-politicos-siguen-esperando-mientras-crece-el-temor-de-que-se-frenen-las-excarcelaciones/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Reportan la muerte de 1.850 personas en las protestas en Irán",
+      "title": "Trump cancela las conversaciones con Irán y promete a los manifestantes que \"la ayuda está en camino\"",
       "link": "https://cnnespanol.cnn.com/mundo/live-news/protesta-iran-numero-de-fallecidos-aranceles-ee-uu-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/teheran.jpeg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "«No nos dejen solos»: Familiares de presos políticos protestaron en la UCV para que sigan las liberaciones",
-      "link": "https://caraotadigital.net/venezuela/no-nos-dejen-solos-familiares-de-presos-politicos-protestaron-en-la-ucv-para-que-sigan-las-liberaciones/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/presos-8.jpg"
+      "title": "Incautaron más de seis toneladas de drogas durante la Operación Soledad en Bolívar, esto reveló Cabello",
+      "link": "https://caraotadigital.net/venezuela/incautaron-mas-de-seis-toneladas-de-drogas-durante-la-operacion-soledad-en-bolivar-esto-revelo-cabello/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/imagediosdado21828.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "LIBERADOS 15 ADOLESCENTES EN BARCELONA: HABÍAN SIDO ACUSADOS DE «ACTOS TERRORISTAS»",
-      "link": "https://noticierodigital.com/2026/01/liberados-15-adolescentes-en-barcelona-habian-sido-acusados-de-actos-terroristas/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2022/01/presos-carceles-enfermospsiquiatricos-5ene2021.jpg"
+      "title": "GOBIERNO DE TRUMP DICE QUE ES INVIABLE REVISAR DEPORTACIONES AL CECOT POR SITUACIÓN EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/01/gobierno-de-trump-dice-que-es-inviable-revisar-deportaciones-al-cecot-por-situacion-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/05/Cecot_-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
@@ -50,16 +50,16 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "Denuncian que presos políticos excarcelados enfrentan medidas cautelares y procesos abiertos",
-      "link": "https://laverdad.com/denuncian-que-presos-politicos-excarcelados-enfrentan-medidas-cautelares-y-procesos-abiertos/",
+      "title": "Excarcelan a la venezolano-española Sofía Sahagun: “va camino a Madrid”",
+      "link": "https://laverdad.com/excarcelan-a-la-venezolano-espanola-sofia-sahagun-va-camino-a-madrid/",
       "image": "https://laverdad.com/wp-content/uploads/2026/01/Julio-Iglesias.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Reuters: Venezuela comienza a revertir los recortes de producción de petróleo mientras reanudan las exportaciones",
-      "link": "https://diarioversionfinal.com/politica-dinero/reuters-venezuela-comienza-a-revertir-los-recortes-de-produccion-de-petroleo-mientras-reanudan-las-exportaciones/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/f.elconfidencial.com_original_40a_8b1_257_40a8b125784af6ae88f3385d57a5bfdd-750x430.jpg"
+      "title": "Petroleros solo invertirán con María Corina: “Diosdado hizo que Delcy mostrara su debilidad”",
+      "link": "https://diarioversionfinal.com/politica-dinero/petroleros-solo-invertiran-con-maria-corina-diosdado-hizo-que-delcy-mostrara-su-debilidad/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/10/ap25290822396015-20251018075729134-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-01-13T17:27:17.691Z"
+  "lastUpdate": "2026-01-13T18:32:31.110Z"
 };
