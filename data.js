90cb907aedd776a9610c3e52dbfda9a5477fed19
GitHub Action <actions@github.com>
2026-01-13 15:28:03 +0000

    data: update news and rates at Tue Jan 13 15:28:03 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "384,33"
     },
     "binance": {
-      "usdt": "562,50"
+      "usdt": "595,99"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,52 +14,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Quién es Yaakov Harary, el preso político argentino-israelí liberado en las últimas horas",
-      "link": "https://www.infobae.com/venezuela/2026/01/13/quien-es-yaakov-harary-el-preso-politico-argentino-israeli-liberado-en-las-ultimas-horas/",
-      "image": "https://www.infobae.com/resizer/v2/S773VONLPRBHJDJFDTXREEHOGY.jpg?auth=6d4f4b67422507ad7d7ab536a1862221ca1ff198de5cddb76426976ee936fb79&smart=true&width=350&height=197&quality=85"
+      "title": "Tras la liberación de sus presos políticos en Venezuela, Italia elevó su representación diplomática en Caracas al rango de Embajada",
+      "link": "https://www.infobae.com/venezuela/2026/01/13/italia-elevo-su-representacion-diplomatica-en-venezuela-al-rango-de-embajada/",
+      "image": "https://www.infobae.com/resizer/v2/GKKGJE6PYYH7MACG5BUSBA6UWI.jpg?auth=1f22e8b9b238640b8c8a9665575f9221c129a6ae75de4bab4597967c3cc880b0&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "FMI y Banco Mundial se reunieron para discutir apoyo financiero a Venezuela\nECONOMÍA",
-      "link": "https://www.elnacional.com/2026/01/fmi-y-banco-mundial-discutieron-apoyo-financiero-a-venezuela/",
+      "title": "Entre la normalidad y el temor: así iniciaron las clases en Venezuela tras el ataque de EE UU\nEDUCACIÓN",
+      "link": "https://www.elnacional.com/2026/01/asi-iniciaron-las-clases-en-venezuela-tras-el-ataque-de-ee-uu/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Reportan la muerte de más de 500 manifestantes en Irán y se teme que la cifra sea mayor",
+      "title": "Reportan la muerte de 1.850 personas en las protestas en Irán",
       "link": "https://cnnespanol.cnn.com/mundo/live-news/protesta-iran-numero-de-fallecidos-aranceles-ee-uu-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/teheran.jpeg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Cerca del 90% de viviendas cercanas a La Carlota resultaron afectadas tras ataques de EEUU, según Gustavo Duque",
-      "link": "https://caraotadigital.net/venezuela/cerca-del-90-de-viviendas-cercanas-a-la-carlota-resultaron-afectadas-tras-ataques-de-eeuu-segun-gustavo-duque/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Duque.jpg"
+      "title": "Pentágono usó avión con apariencia civil en ataque en el Caribe, según el NYT",
+      "link": "https://caraotadigital.net/eeuu/pentagono-uso-avion-con-apariencia-civil-en-ataque-en-el-caribe-segun-el-nyt/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2025/10/US-ATTACLK-BOAT-IN-CARIBBEAN.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "QUINTA NOCHE DE ESPERA POR PRESOS POLÍTICOS EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/01/quinta-noche-de-espera-por-presos-politicos-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/IMG-20260113-WA0029-627x376.jpg"
+      "title": "HIJO DE JUAN PABLO GUANIPA: «ALGO ESTÁ TRABANDO EXCARCELACIÓN DE PRESOS POLÍTICOS»",
+      "link": "https://noticierodigital.com/2026/01/hijo-de-juan-pablo-guanipa-algo-esta-trabando-excarcelacion-de-presos-politicos/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/07/juan-pablo-guanipa-ramon-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Atentado en Neiva contra directivos de cárcel deja un niño de 11 años herido",
-      "link": "https://www.noticiascaracol.com/colombia/atentado-en-neiva-contra-directivos-de-carcel-deja-un-nino-de-11-anos-herido-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/d1c25c0/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F93%2F90%2Fcb907ba342de98156ac31245c20a%2Fdirectivos-carcel-neiva.jpg"
+      "title": "¿Por qué la Aerocivil investiga desempeño de piloto de avioneta en la que murió Yeison Jiménez?",
+      "link": "https://www.noticiascaracol.com/colombia/por-que-la-aerocivil-investiga-desempeno-de-piloto-de-avioneta-en-la-que-murio-yeison-jimenez-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/053c017/2147483647/strip/true/crop/1280x722+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F4c%2F3a%2F01e935804e0295f46848157b4027%2Fyeison-jimenez-10.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Detienen en Ciudad de México a 6 integrantes del “Tren de Aragua”",
-      "link": "https://laverdad.com/detienen-en-ciudad-de-mexico-a-6-integrantes-del-tren-de-aragua/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/01/Avatar_-Fire-and-Ash.jpg"
+      "title": "Machado y González Urrutia denuncian incumplimiento de anuncio de excarcelaciones",
+      "link": "https://laverdad.com/machado-y-gonzalez-urrutia-denuncian-incumplimiento-de-anuncio-de-excarcelaciones/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/01/Chiquinquira-Delgado-y-Jorge-Ramos.jpg"
     },
     {
       "source": "Diario Versión Final",
       "title": "¿Dónde está Tarek El Aissami? Piden a Delcy entregarlo a los EE.UU.",
       "link": "https://diarioversionfinal.com/politica-dinero/donde-esta-tarek-el-aissami-piden-a-delcy-entregarlo-a-los-ee-uu/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2023/10/FMI-750x430.jpg"
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Primera-ONU-1.jpg-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-01-13T14:26:34.534Z"
+  "lastUpdate": "2026-01-13T15:26:40.475Z"
 };
