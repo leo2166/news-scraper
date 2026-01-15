0e2e10a1ef881722e083fda9db6731e954f6b5ff
GitHub Action <actions@github.com>
2026-01-15 21:24:05 +0000

    data: update news and rates at Thu Jan 15 21:24:05 UTC 2026

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "339,15",
-      "eur": "395,27"
+      "usd": "341,74",
+      "eur": "396,48"
     },
     "binance": {
-      "usdt": "497,10"
+      "usdt": "506,80"
     },
     "kontigo": {
       "usd": "N/A"
@@ -32,34 +32,34 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "“Impuesto al pecado”: La polémica propuesta de candidato a gobernador de Florida contra modelos de OnlyFans",
-      "link": "https://caraotadigital.net/eeuu/impuesto-al-pecado-la-polemica-propuesta-de-candidato-a-gobernador-de-florida-contra-modelos-de-onlyfans/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/ONLY.jpg"
+      "title": "Memoria y cuenta: Delcy Rodríguez presentó reforma parcial de la Ley de Hidrocarburos ante la Asamblea Nacional",
+      "link": "https://caraotadigital.net/venezuela/memoria-y-cuenta-delcy-rodriguez-presento-reforma-parcial-de-la-ley-de-hidrocarburos-ante-la-asamblea-nacional/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Rodriguez-CD-8.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "POLICÍA DE SUCRE ACLARA QUE NO EXISTE ORDEN QUE IMPIDA REVISAR TELÉFONOS EN ALCABALAS",
-      "link": "https://noticierodigital.com/2026/01/policia-de-sucre-aclara-que-no-existe-orden-que-impida-revisar-telefonos-en-alcabalas/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/IMG-20260115-WA0094-627x376.jpg"
+      "title": "JUAN BARRETO: «EXCARCELACIONES DEBEN CONVERTIRSE EN LIBERTAD PLENA»",
+      "link": "https://noticierodigital.com/2026/01/juan-barreto-excarcelaciones-deben-convertirse-en-libertad-plena/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/IMG-20260115-WA0157-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Fotos inéditas del rescate de Zulma Guzmán y relato de rescatista: \"Ansiosa por no ser recuperada”\"",
-      "link": "https://www.noticiascaracol.com/mundo/fotos-ineditas-del-rescate-de-zulma-guzman-y-relato-de-rescatista-ansiosa-por-no-ser-recuperada-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/40ca126/2147483647/strip/true/crop/1280x722+0+14/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F16%2F63%2F0aee80dd45478cf09e4701f06921%2Fzulma-guzman-1.jpg"
+      "title": "Antonio García, jefe del Eln, dijo a AFP que apoyaría unión de grupos ilegales",
+      "link": "https://www.noticiascaracol.com/colombia/antonio-garcia-jefe-del-eln-dijo-a-afp-que-apoyaria-union-de-grupos-ilegales-cb20",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/6733c4a/2147483647/strip/true/crop/1278x721+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F80%2F8a%2Fe4f6103546dc94757b4ec094b3bb%2Fantonio-garcia.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Machado tras reunión con Trump: “Sepan que contamos con él para la libertad de Venezuela”",
-      "link": "https://laverdad.com/machado-tras-reunion-con-trump-sepan-que-contamos-con-el-para-la-libertad-de-venezuela/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/01/Estelita-del-Llano.jpg"
+      "title": "Exigen libertad plena para los 5 periodistas que permanecen detenidos",
+      "link": "https://laverdad.com/exigen-libertad-plena-para-los-5-periodistas-que-permanecen-detenidos/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/01/Ricardo-Sanchez-Pitcher-del-Ano-LVBP-2025-2026.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "MCM sale de reunión en la Casa Blanca: \"Sepan que contamos con Trump para la libertad\"",
-      "link": "https://diarioversionfinal.com/politica-dinero/mcm-sale-de-reunion-en-la-casa-blanca-sepan-que-contamos-con-trump-para-la-libertad/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/09/trump-eeuu-firma_10335288_20250331155349-750x430.jpg"
+      "title": "Rodríguez: “Si me tocase como presidenta encargada ir a Washington lo haré de pie, no arrastrada”",
+      "link": "https://diarioversionfinal.com/politica-dinero/rodriguez-si-me-tocase-como-presidenta-encargada-ir-a-washington-lo-hare-de-pie-no-arrastrada/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/25ae9d805580667316d362aa8280580c-88638253-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-01-15T20:26:39.210Z"
+  "lastUpdate": "2026-01-15T21:22:41.935Z"
 };
