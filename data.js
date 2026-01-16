b1d1680fb11cf0fb3d2985cce9d2298fc9509e8a
GitHub Action <actions@github.com>
2026-01-16 21:23:06 +0000

    data: update news and rates at Fri Jan 16 21:23:06 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "396,48"
     },
     "binance": {
-      "usdt": "489,87"
+      "usdt": "479,87"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,14 +14,14 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Así fue el operativo de salida de María Corina Machado de Venezuela para ir a recibir el Premio Nobel de la Paz",
-      "link": "https://www.infobae.com/venezuela/2026/01/16/asi-fue-el-operativo-de-salida-de-maria-corina-machado-de-venezuela-para-ir-a-recibir-el-premio-nobel-de-la-paz/",
-      "image": "https://www.infobae.com/resizer/v2/EC552D4KFFCL3F2MKJAWDVLGZY.jpg?auth=0443f469cf5bed50a6b9e37b7610dd604feba5f09eb82aca37e1396740d3308f&smart=true&width=350&height=197&quality=85"
+      "title": "Donald Trump aseguró que quedó “muy impresionado” con María Corina Machado",
+      "link": "https://www.infobae.com/estados-unidos/2026/01/16/donald-trump-aseguro-que-quedo-muy-impresionado-con-maria-corina-machado/",
+      "image": "https://www.infobae.com/resizer/v2/RBQNF57Q7FEVDEO5YNQJLSLW6U.jpg?auth=c18b722efb7ee16fdc6aa0e98bc56099d06941920155f505dd4b9a7e0c9838a3&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "María Corina Machado: Venezuela será libre muy pronto con el apoyo de Donald Trump\nPOLÍTICA",
-      "link": "https://www.elnacional.com/2026/01/maria-corina-machado-venezuela-sera-libre-muy-pronto-con-el-apoyo-de-donald-trump/",
+      "title": "Trump dice que seguirá en contacto con María Corina Machado: \"Quedé muy impresionado, es una gran mujer\"\nEE UU",
+      "link": "https://www.elnacional.com/2026/01/trump-dice-que-seguira-en-contacto-con-maria-corina-machado-quede-muy-impresionado-es-una-gran-mujer/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -32,9 +32,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "Lo que reveló Estados Unidos  sobre la venta del petróleo venezolano tras la captura de Nicolás Maduro",
-      "link": "https://caraotadigital.net/eeuu/lo-que-revelo-estados-unidos-sobre-la-venta-del-petroleo-venezolano-tras-la-captura-de-nicolas-maduro/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/OIL-VENEZUELA.jpg"
+      "title": "EN VIDEO: Hijo de Maduro dio nuevos detalles de cómo está a dos semanas de su detención",
+      "link": "https://caraotadigital.net/venezuela/en-video-hijo-de-maduro-dio-nuevos-detalles-de-como-esta-a-dos-semanas-de-su-detencion/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Maduro-Guerra-CD-1.jpg"
     },
     {
       "source": "Noticiero Digital",
@@ -50,16 +50,16 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "UNT propone reforma a la Ley de Hidrocarburos para atraer inversiones duraderas",
-      "link": "https://laverdad.com/unt-propone-reforma-a-la-ley-de-hidrocarburos-para-atraer-inversiones-duraderas/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/01/Bad-Bunny-2.jpg"
+      "title": "Difunden imágenes del encuentro entre Delcy Rodríguez y el director de la CIA",
+      "link": "https://laverdad.com/difunden-imagenes-del-encuentro-entre-delcy-rodriguez-y-el-director-de-la-cia/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/01/Yadier-Molina.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Trump insinúa que trabaja en colaboración con Delcy Rodríguez para evitar un caso como Irak",
-      "link": "https://diarioversionfinal.com/mundo/trump-insinua-que-trabaja-en-colaboracion-con-delcy-rodriguez-para-evitar-un-caso-como-irak/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/WhatsApp-Image-2026-01-16-at-3.57.56-PM.jpeg"
+      "title": "Cabello afirma que gobierno no descansará hasta traer de vuelta a Maduro y refuerza seguridad",
+      "link": "https://diarioversionfinal.com/politica-dinero/cabello-afirma-que-gobierno-no-descansara-hasta-traer-de-vuelta-a-maduro-y-refuerza-seguridad/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/WhatsApp-Image-2026-01-16-at-4.40.37-PM-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-01-16T20:22:43.703Z"
+  "lastUpdate": "2026-01-16T21:21:41.923Z"
 };
