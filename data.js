c600e3d6947359ed74e2ebdeb2690115e2e9c4ae
GitHub Action <actions@github.com>
2026-01-17 01:44:16 +0000

    data: update news and rates at Sat Jan 17 01:44:16 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "400,49"
     },
     "binance": {
-      "usdt": "481,69"
+      "usdt": "454,98"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,14 +14,14 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Donald Trump aseguró que quedó “muy impresionado” con María Corina Machado",
-      "link": "https://www.infobae.com/estados-unidos/2026/01/16/donald-trump-aseguro-que-quedo-muy-impresionado-con-maria-corina-machado/",
-      "image": "https://www.infobae.com/resizer/v2/RBQNF57Q7FEVDEO5YNQJLSLW6U.jpg?auth=c18b722efb7ee16fdc6aa0e98bc56099d06941920155f505dd4b9a7e0c9838a3&smart=true&width=350&height=197&quality=85"
+      "title": "Venezuela prepara la reanudación de la venta oficial de dólares",
+      "link": "https://www.infobae.com/venezuela/2026/01/17/venezuela-prepara-la-reanudacion-de-la-venta-oficial-de-dolares/",
+      "image": "https://www.infobae.com/resizer/v2/UZSROSSUB5EHDMT3GQKWENHH6A.jpg?auth=6c92b611020d65d1057e913170857004e3de81fb7fb7d57336ca6febb2fabb52&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Trump dice que seguirá en contacto con María Corina Machado: \"Quedé muy impresionado, es una gran mujer\"\nEE UU",
-      "link": "https://www.elnacional.com/2026/01/trump-dice-que-seguira-en-contacto-con-maria-corina-machado-quede-muy-impresionado-es-una-gran-mujer/",
+      "title": "¿Puede Trump quedarse con la medalla Nobel que le dio Machado?\nEE UU",
+      "link": "https://www.elnacional.com/2026/01/puede-trump-quedarse-con-la-medalla-nobel-que-le-dio-machado/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -32,21 +32,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "Venezuela anuncia que exportará por primera vez gas licuado de petróleo",
-      "link": "https://caraotadigital.net/venezuela/venezuela-anuncia-que-exportara-por-primera-vez-gas-licuado-de-petroleo/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/gas-1.jpg"
+      "title": "Horror en Guárico por los brutales maltratos a lo que era sometido un adolescente por sus propios padres",
+      "link": "https://caraotadigital.net/sucesos/horror-en-guarico-por-los-brutales-maltratos-a-lo-que-era-sometido-un-adolescente-por-sus-propios-padres/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/image-33.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EE. UU. PIDIÓ EXTREMAR PRECAUCIONES EN VUELOS SOBRE CENTROAMÉRICA Y SUDAMÉRICA",
-      "link": "https://noticierodigital.com/2026/01/ee-uu-pidio-extremar-precauciones-en-vuelos-sobre-centroamerica-y-sudamerica/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/12/faa-eeuu-627x376.jpg"
+      "title": "EE. UU. AGILIZA AMPLIACIÓN DE LICENCIA PETROLERA PARA CHEVRON",
+      "link": "https://noticierodigital.com/2026/01/ee-uu-agiliza-ampliacion-de-licencia-petrolera-para-chevron/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/04/chevron-licencia-eeuu-27may2022-1-627x376.webp"
     },
     {
       "source": "Caracol Noticias",
-      "title": "María Corina Machado dice que Delcy Rodríguez en Venezuela está \"cumpliendo órdenes\" de Trump",
-      "link": "https://www.noticiascaracol.com/mundo/maria-corina-machado-dice-que-delcy-rodriguez-en-venezuela-esta-cumpliendo-ordenes-de-trump-cb20",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/83394df/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Ffd%2F0a%2F5d20b13849a6ad7810fc3aad70f7%2Fmaria-corina-machado-y-delcy-rodriguez.jpg"
+      "title": "Habla familia de pareja de esposos que falleció en trágico accidente en Bogotá: \"Nos los quitaron\"",
+      "link": "https://www.noticiascaracol.com/colombia/bogota/habla-familia-de-pareja-de-esposos-que-fallecio-en-tragico-accidente-en-bogota-nos-los-quitaron-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/e0c71c2/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F47%2Ff2%2F54b69e4442edbe4536277522a598%2Fmovilidad-bogota-92.jpg"
     },
     {
       "source": "La Verdad",
@@ -56,10 +56,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Cabello afirma que gobierno no descansará hasta traer de vuelta a Maduro y refuerza seguridad",
-      "link": "https://diarioversionfinal.com/politica-dinero/cabello-afirma-que-gobierno-no-descansara-hasta-traer-de-vuelta-a-maduro-y-refuerza-seguridad/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/Trump-2-22-12-2025-750x430.png"
+      "title": "Delcy Rodríguez impulsa reestructuración ministerial tras captura de Maduro",
+      "link": "https://diarioversionfinal.com/politica-dinero/delcy-rodriguez-impulsa-reestructuracion-ministerial-tras-captura-de-maduro/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Trump-Maria-Corina-12-01-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-01-16T23:21:02.850Z"
+  "lastUpdate": "2026-01-17T01:42:52.904Z"
 };
