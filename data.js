c167058f47dec5bcab9fa95771c797b6ecbcc9f0
GitHub Action <actions@github.com>
2026-01-20 23:24:56 +0000

    data: update news and rates at Tue Jan 20 23:24:56 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "407,17"
     },
     "binance": {
-      "usdt": "470,00"
+      "usdt": "477,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Donald Trump dijo que evalúa involucrar a María Corina Machado en la transición de Venezuela: “Estamos hablando con ella”",
-      "link": "https://www.infobae.com/venezuela/2026/01/20/donald-trump-dijo-que-evalua-involucrar-a-maria-corina-machado-en-la-transicion-de-venezuela-estamos-hablando-con-ella/",
-      "image": "https://www.infobae.com/resizer/v2/2GUY2XTDBZFZBJAAMWECAHBF7E.jpg?auth=f411ab5481c4b23543013d64268611c14997da8224eeb148139fad04976fa725&smart=true&width=350&height=197&quality=85"
+      "title": "Jorge Rodríguez anunció una reforma al Código Electoral de Venezuela para eliminar “trabas burocráticas”",
+      "link": "https://www.infobae.com/venezuela/2026/01/20/jorge-rodriguez-anuncio-una-reforma-al-codigo-electoral-de-venezuela-para-eliminar-trabas-burocraticas/",
+      "image": "https://www.infobae.com/resizer/v2/LQSYOORNRBFBHBNCSPF5LRGO7A.JPG?auth=52cd9c62004257200fecc23eb9d22d2f95b8b6a3fd92deda2cd7500cb21bb472&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -32,21 +32,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "La seria amenaza de Trump por negativa de Macron de integrar “Consejo de Paz” para supervisar Gaza",
-      "link": "https://caraotadigital.net/eeuu/la-seria-amenaza-de-trump-por-negativa-de-macron-de-integrar-consejo-de-paz-para-supervisar-gaza/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/TRUMP-MACRON.jpg"
+      "title": "VIDEO: Jorge Rodríguez insiste en normativas electorales: «Un nuevo código electoral que agrupe y simplifique los proceso»",
+      "link": "https://caraotadigital.net/venezuela/video-jorge-rodriguez-insiste-en-normativas-electorales-un-nuevo-codigo-electoral-que-agrupe-y-simplifique-los-proceso/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Rodriguez-CD-12.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "GIL TILDÓ DE «DELIRANTES» LOS COMENTARIOS DE MILEI SOBRE MADURO",
-      "link": "https://noticierodigital.com/2026/01/gil-tildo-de-delirantes-los-comentarios-de-milei-sobre-maduro/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/09/yvan-gil-venezuela-627x376.jpg"
+      "title": "TRUMP PLANTEÓ LA POSIBILIDAD DE INVOLUCRAR A MCM EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/01/trump-planteo-la-posibilidad-de-involucrar-a-mcm-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/donald-trump-venezuela-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Gobierno publica decreto que elimina prima especial de cerca de $16 millones para los congresistas",
-      "link": "https://www.noticiascaracol.com/politica/gobierno-publica-decreto-que-elimina-prima-especial-de-cerca-de-16-millones-para-los-congresistas-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/fb2de43/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fa2%2Fab%2F9d7a71a9489baf04a2efe9712b7d%2Fcongreso-prima.jpg"
+      "title": "Nuevo descarrilamiento de tren en España: autoridades reportan un muerto y varios heridos",
+      "link": "https://www.noticiascaracol.com/mundo/nuevo-descarrilamiento-de-tren-en-espana-autoridades-reportan-20-heridos-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/0a903b2/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F29%2F60%2F4d468d8647d18ad5aa116e80ed9f%2Fplantilla-fotos-27.jpg"
     },
     {
       "source": "La Verdad",
@@ -61,5 +61,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/06/jorge-rodriguez-5-de-julio-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-01-20T22:23:54.551Z"
+  "lastUpdate": "2026-01-20T23:23:32.870Z"
 };
