7b9fa604e42e2a52a5e043b8a255466ba63669b0
GitHub Action <actions@github.com>
2026-01-22 19:26:57 +0000

    data: update news and rates at Thu Jan 22 19:26:57 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "409,65"
     },
     "binance": {
-      "usdt": "474,37"
+      "usdt": "484,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Denuncian que se desconoce el paradero del líder sindical venezolano José Elías Torres tras 50 días detenido",
-      "link": "https://www.infobae.com/venezuela/2026/01/22/denuncian-que-se-desconoce-el-paradero-del-lider-sindical-venezolano-jose-elias-torres-tras-50-dias-detenido/",
-      "image": "https://www.infobae.com/resizer/v2/UN42BEAVC5CMPNASSYNI5XYV44.jpg?auth=b649cac6c8533b54277242e106b48aaad49d2f7ab1a72bb53e5e448212324337&smart=true&width=350&height=197&quality=85"
+      "title": "Estados Unidos apuesta por la diplomacia en Venezuela: designó a una nueva encargada de negocios de la Oficina de Asuntos Externos",
+      "link": "https://www.infobae.com/estados-unidos/2026/01/22/estados-unidos-apuesta-por-la-diplomacia-en-venezuela-designo-a-una-nueva-encargada-de-negocios-de-la-oficina-de-asuntos-externos/",
+      "image": "https://www.infobae.com/resizer/v2/EEHGUAA6SZCI7NL47RKOULADZE.PNG?auth=dfd0379c267b29d09bcb291812a7936986c448196b4d6e7466ed65302ce875bf&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -26,40 +26,40 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Elecciones en EE.UU. 2024, en vivo: Trump, Harris y voto por estados",
-      "link": "https://cnnespanol.cnn.com/2024/11/05/resultados-elecciones-eeuu-2024-quien-gana-kamala-trump-orix/",
-      "image": "https://cnnespanol.cnn.com/wp-content/uploads/2024/11/GettyImages-2182507455.jpg?quality=100&strip=info&w=460&h=260&crop=1"
+      "title": "Por qué el acuerdo de Trump para Groenlandia suena mucho a uno que existe desde 1951",
+      "link": "https://cnnespanol.cnn.com/2026/01/22/eeuu/trump-groenlandia-acuerdo-1951-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/acuerdo-trump-y-gr.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Condenaron a un estudiante a 15 de prisión, había denunciado un bote de aguas negras a través de VenApp",
-      "link": "https://caraotadigital.net/venezuela/condenaron-a-un-estudiante-a-15-de-prision-habia-denunciado-un-bote-de-aguas-negras-a-traves-de-venapp/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/imageestudiante21829.jpg"
+      "title": "El dramático momento en el que una recogepelotas se desmayó en pleno Abierto de Australia, esto hizo una tenista",
+      "link": "https://caraotadigital.net/deportes/el-dramatico-momento-en-el-que-una-recogepelotas-se-desmayo-en-pleno-abierto-de-australia-esto-hizo-una-tenista/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/RECOGEPELOTAS.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "ESTUDIANTES DE LA ULA ALZAN LA VOZ POR LA LIBERTAD DE SUS PROFESORES Y MERIDEÑOS PRESOS",
-      "link": "https://noticierodigital.com/2026/01/estudiantes-de-la-ula-alzan-la-voz-por-la-libertad-de-sus-profesores-y-meridenos-presos/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/IMG-20260122-WA0117-627x376.jpg"
+      "title": "BALANCE DE FORO PENAL HASTA ESTE 22 DE ENERO CONTABILIZA 780 PRESOS POLÍTICOS",
+      "link": "https://noticierodigital.com/2026/01/balance-de-foro-penal-hasta-este-22-de-enero-contabiliza-780-presos-politicos/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/05/foro-penal-referencial-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Caso talio: Zulma Guzmán se lanzó al río Támesis intentando huir, confirma fuente de Interpol",
-      "link": "https://www.noticiascaracol.com/los-informantes/caso-talio-zulma-guzman-se-lanzo-al-rio-tamesis-intentando-huir-confirma-fuente-de-interpol-pr30",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/f71118f/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F70%2Fe8%2F24435a1c41d1aacc6b345d86ece1%2Fzulma-guzman-no-intento-quitarse-la-vida-salto-para-evadir-la-captura-y-planeaba-escapar-a-brasil.jpg"
+      "title": "Denuncian reservas hospedaje canceladas en Medellín para conciertos de Bad Bunny: SIC se pronunció",
+      "link": "https://www.noticiascaracol.com/colombia/denuncian-reservas-hospedaje-canceladas-en-medellin-para-conciertos-de-bad-bunny-sic-se-pronuncio-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/1ef2b5e/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Ff6%2Fa6%2F6cbf030c47c5aa7a050090d09485%2Fbad-bunny-medellin-en-la-sic.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Laura Dogu es la nueva jefa de la misión diplomática de EE. UU. en Venezuela",
-      "link": "https://laverdad.com/laura-dogu-es-la-nueva-jefa-de-la-mision-diplomatica-de-ee-uu-en-venezuela/",
+      "title": "Miraflores tilda de “fake” el reporte de The Guardian sobre los hermanos Rodríguez",
+      "link": "https://laverdad.com/miraflores-tilda-de-fake-el-reporte-de-the-guardian-sobre-los-hermanos-rodriguez/",
       "image": "https://laverdad.com/wp-content/uploads/2026/01/Bad-Bunny-y-Rosalia.jpg"
     },
     {
       "source": "Diario Versión Final",
       "title": "El proceso de transición en Venezuela “no ha comenzado”, aseguran especialistas",
       "link": "https://diarioversionfinal.com/politica-dinero/el-proceso-de-transicion-en-venezuela-no-ha-comenzado-aseguran-especialistas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/Maduro-24-12-2025-750x430.jpg"
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/2c8b618aefa17f3c0cd305ad3093e941ef792778-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-01-22T18:34:33.057Z"
+  "lastUpdate": "2026-01-22T19:25:34.315Z"
 };
