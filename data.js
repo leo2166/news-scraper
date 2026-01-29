322a3dd43f6a209892bf431e50f3f6c460043e7b
GitHub Action <actions@github.com>
2026-01-29 14:04:58 +0000

    data: update news and rates at Thu Jan 29 14:04:58 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "434,43"
     },
     "binance": {
-      "usdt": "550,69"
+      "usdt": "556,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,33 +14,33 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Duro reclamo de militares y familiares venezolanos: “Carta Abierta a la vergüenza nacional, Vladimir Padrino López”",
-      "link": "https://www.infobae.com/venezuela/2026/01/29/duro-reclamo-de-militares-y-familiares-venezolanos-carta-abierta-a-la-verguenza-nacional-vladimir-padrino-lopez/",
-      "image": "https://www.infobae.com/resizer/v2/IA2IWMQWQFC27IZOFVLKLUSHKE.jpg?auth=5d70cc8bc37857cac938b0056a1c28f68197be375885d1265831cb0f1e0c2ca0&smart=true&width=350&height=197&quality=85"
+      "title": "María Corina Machado dijo que le llamó la atención que Petro pida que Maduro sea juzgado en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/01/29/maria-corina-machado-dijo-que-le-llamo-la-atencion-que-petro-pida-que-maduro-sea-juzgado-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/K4TJL6QBNRFVDAIYJJOUYWXXR4.JPG?auth=aaf9f5c8b091cad38c38426e56337d86a467530f46c0b70378cbabeaa16f0197&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "¿Donald Trump anunció una dolarización oficial en Venezuela?\nECONOMÍA",
-      "link": "https://www.elnacional.com/2026/01/donald-trump-anuncio-una-dolarizacion-oficial-en-venezuela/",
+      "title": "Reuters: EE UU devolverá a Venezuela petrolero M/T Sophia incautado en enero\nECONOMÍA",
+      "link": "https://www.elnacional.com/2026/01/ee-uu-devolvera-a-venezuela-petrolero-m-t-sophia-incautado/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Corte de apelaciones dice que el fin del TPS para los venezolanos fue ilegal",
-      "link": "https://cnnespanol.cnn.com/2026/01/29/eeuu/corte-fin-tps-venezuela-haiti-ilegal-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2199404085-20260103131843137.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "Homan dice que las agencias están trabajando en un \"plan de retirada\" en Minneapolis",
+      "link": "https://cnnespanol.cnn.com/eeuu/live-news/minneapolis-ice-trump-29-01-26-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/zar-fronterizo.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Preocupación internacional por el virus Nipah: Alta mortalidad, sin vacunas y síntomas graves",
-      "link": "https://caraotadigital.net/mundo/preocupacion-internacional-por-el-virus-nipah-alta-mortalidad-sin-vacunas-y-sintomas-graves/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Virus-Nipah-CD.jpg"
+      "title": "Diosdado Cabello dice que liberaron a otros 23 presos políticos y les envió un mensaje",
+      "link": "https://caraotadigital.net/venezuela/diosdado-cabello-dice-que-liberaron-a-otros-23-presos-politicos-y-les-envio-un-mensaje/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Cabello-27.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "ALBARES DICE QUE LA UE SE ALINEA CON ESPAÑA EN SU APOYO AL DIÁLOGO POLÍTICO EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/01/albares-dice-que-la-ue-se-alinea-con-espana-en-su-apoyo-al-dialogo-politico-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2024/10/jose-manuel-albares.jpg"
+      "title": "PERÚ ANUNCIA LA LIBERACIÓN DE OTROS DOS PERUANOS DETENIDOS EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/01/peru-anuncia-la-liberacion-de-otros-dos-peruanos-detenidos-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/Diseno-sin-titulo-1-14-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
@@ -50,16 +50,16 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "Hija de González Urrutia revela que ella y su esposo tienen prohibido declarar tras excarcelación",
-      "link": "https://laverdad.com/hija-de-gonzalez-urrutia-revela-que-ella-y-su-esposo-tienen-prohibido-declarar-tras-excarcelacion/",
+      "title": "Difunden imágenes del encuentro entre Machado y Rubio",
+      "link": "https://laverdad.com/difunden-imagenes-del-encuentro-entre-machado-y-rubio/",
       "image": "https://laverdad.com/wp-content/uploads/2026/01/LG-2026-01-1-scaled.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Fanb jura lealtad a Delcy Rodríguez como nueva Comandante en Jefe",
-      "link": "https://diarioversionfinal.com/politica-dinero/fanb-jura-lealtad-a-delcy-rodriguez-como-nueva-comandante-en-jefe/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2024/09/canciller-espanol-jose-manuel-albares-22019-750x430.jpg"
+      "title": "Tribunal de apelaciones de EE. UU. declara ilegal la cancelación del TPS para venezolanos",
+      "link": "https://diarioversionfinal.com/mundo/tribunal-de-apelaciones-de-ee-uu-declara-ilegal-la-cancelacion-del-tps-para-venezolanos/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/02405464_xl-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-01-29T12:58:53.856Z"
+  "lastUpdate": "2026-01-29T14:03:35.875Z"
 };
