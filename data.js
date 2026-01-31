f41c5a6f646f3c18c15597f50ddc2548ee8c1df8
GitHub Action <actions@github.com>
2026-01-31 04:12:20 +0000

    data: update news and rates at Sat Jan 31 04:12:20 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "440,48"
     },
     "binance": {
-      "usdt": "513,80"
+      "usdt": "537,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Delcy Rodríguez anunció una ley de amnistía general y el cierre de la cárcel del Helicoide",
-      "link": "https://www.infobae.com/venezuela/2026/01/31/delcy-rodriguez-anuncio-una-ley-de-amnistia-general-y-el-cierre-del-la-carcel-del-helicoide/",
-      "image": "https://www.infobae.com/resizer/v2/5TPJ7QUA4FEZ5GAERCEHRDVRUQ.JPG?auth=a75ae8ea3c28c7a8262a924c85da73db99678c3531b6a71d7795939e45686fd4&smart=true&width=350&height=197&quality=85"
+      "title": "Foro Penal cuestionó la propuesta de Delcy Rodríguez para los presos políticos: “El indulto es perdón, mientras la amnistía es olvido”",
+      "link": "https://www.infobae.com/venezuela/2026/01/31/foro-penal-cuestiono-la-propuesta-de-delcy-rodriguez-para-los-presos-politicos-el-indulto-es-perdon-mientras-la-amnistia-es-olvido/",
+      "image": "https://www.infobae.com/resizer/v2/SLWXQKAJJRBZTO7ACAXJHGHHBU.jpg?auth=842ab529ce6bae8258209819a26e855c07d843e1d1773fb20cf0acd3954e1c8c&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -38,15 +38,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "COPA AIRLINES REACTIVÓ LA VENTA DE BOLETOS ENTRE VENEZUELA Y EE. UU.",
-      "link": "https://noticierodigital.com/2026/01/copa-airlines-reactivo-la-venta-de-boletos-entre-venezuela-y-ee-uu/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/05/copa-airlines-627x376.jpg"
+      "title": "EE. UU. CONFIRMÓ LA LIBERACIÓN DE TODOS LOS CIUDADANOS ESTADOUNIDENSES EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/01/ee-uu-confirmo-la-liberacion-de-todos-los-ciudadanos-estadounidenses-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/10/embajada-de-eeuu-en-caracas-venezuela-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Banco de la República fija la tasa de interés en 10,25%: subió 100 puntos básicos",
-      "link": "https://www.noticiascaracol.com/economia/banco-de-la-republica-fija-la-tasa-de-interes-en-10-25-subio-100-puntos-basicos-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/138c1b4/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F9e%2Fff%2F6af7bcca4e0d826a954dba57fa8e%2Ftasa-de-interes.jpg"
+      "title": "Relato de colombo-israelí que estuvo secuestrado por Hamás: \"Siempre tuve fe de que iba regresar\"",
+      "link": "https://www.noticiascaracol.com/mundo/elkana-bohbot-colombo-israeli-que-estuvo-secuestrado-por-hamas-relato-lo-que-vivio-siempre-tuve-fe-de-que-iba-regresar-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/e7def1f/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F5e%2F5c%2F025909fb4aa7ab2cc4755457a08e%2Fdiseno-1.jpg"
     },
     {
       "source": "La Verdad",
@@ -58,8 +58,8 @@ window.DASHBOARD_DATA = {
       "source": "Diario Versión Final",
       "title": "Presidenta (e) confirma Ley de Amnistía y anuncia que El Helicoide se convertirá en centro social",
       "link": "https://diarioversionfinal.com/politica-dinero/presidenta-e-confirma-ley-de-amnistia-y-anuncia-que-el-helicoide-se-convertira-en-centro-social/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Laura-Dogu-30-01-2026-750x430.png"
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Regreso-a-Venezuela-2-30-01-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-01-31T02:00:23.177Z"
+  "lastUpdate": "2026-01-31T04:10:58.000Z"
 };
