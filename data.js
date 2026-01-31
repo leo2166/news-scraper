7df79e4607e38c55679b191a9421032c4120d856
GitHub Action <actions@github.com>
2026-01-31 02:01:46 +0000

    data: update news and rates at Sat Jan 31 02:01:46 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "440,48"
     },
     "binance": {
-      "usdt": "537,50"
+      "usdt": "513,80"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,39 +14,39 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "El régimen de Venezuela liberó a un ciudadano estadounidense-peruano",
-      "link": "https://www.infobae.com/venezuela/2026/01/30/el-regimen-de-venezuela-libero-a-un-ciudadano-estadounidense-peruano/",
-      "image": "https://www.infobae.com/resizer/v2/TBMLUYNMCVGCLAMQLDQURPCMPA.jpg?auth=7e9ed78f72abf3403aabc3cf7a90c9b925cbba07bc1ef9efe302a49c4eccf535&smart=true&width=350&height=197&quality=85"
+      "title": "Delcy Rodríguez anunció una ley de amnistía general y el cierre de la cárcel del Helicoide",
+      "link": "https://www.infobae.com/venezuela/2026/01/31/delcy-rodriguez-anuncio-una-ley-de-amnistia-general-y-el-cierre-del-la-carcel-del-helicoide/",
+      "image": "https://www.infobae.com/resizer/v2/5TPJ7QUA4FEZ5GAERCEHRDVRUQ.JPG?auth=a75ae8ea3c28c7a8262a924c85da73db99678c3531b6a71d7795939e45686fd4&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "María Corina Machado en reunión con líderes europeos: “Avanzaremos con fuerza y determinación”\nPOLÍTICA",
-      "link": "https://www.elnacional.com/2026/01/maria-corina-machado-en-reunion-con-lideres-europeos-avanzaremos-con-fuerza-y-determinacion/",
+      "title": "Delcy Rodríguez presentará a la AN ley de amnistía general\nPOLÍTICA",
+      "link": "https://www.elnacional.com/2026/01/anuncian-ley-de-amnistia-general/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump amenaza con sancionar la venta de petróleo a Cuba. ¿Es solo para México?",
-      "link": "https://cnnespanol.cnn.com/2026/01/30/latinoamerica/trump-sanciones-petroleo-cuba-mexico-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2258486563.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Delcy Rodríguez anuncia ley de amnistía en Venezuela y la reconversión de El Helicoide",
+      "link": "https://cnnespanol.cnn.com/2026/01/30/venezuela/delcy-rodriguez-anuncia-ley-amnistia-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/f5a920c82e780c831e93383867f2bb1f81fa28e7.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Exxon pide “transición democrática en Venezuela” para poder hacer inversiones petroleras",
-      "link": "https://caraotadigital.net/eeuu/exxon-pide-transicion-democratica-en-venezuela-para-poder-hacer-inversiones-petroleras/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/Exxon-Mobil-Darren-Woods.jpg"
+      "title": "La emotiva despedida de Macaulay Culkin a Catherine O’Hara: «Mamá, pensé que teníamos tiempo»",
+      "link": "https://caraotadigital.net/caraota-show/la-emotiva-despedida-macaulay-culkin-a-catherine-ohara-mama-pense-que-teniamos-tiempo/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/01/image-56.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "LAURA DOGU, NUEVA ENCARGADA DE NEGOCIOS DE EE. UU., ARRIBARÁ A CARACAS ESTE SÁBADO",
-      "link": "https://noticierodigital.com/2026/01/laura-dogu-nueva-encargada-de-negocios-de-ee-uu-arribara-a-caracas-este-sabado/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/laura-dogu-627x376.jpg"
+      "title": "COPA AIRLINES REACTIVÓ LA VENTA DE BOLETOS ENTRE VENEZUELA Y EE. UU.",
+      "link": "https://noticierodigital.com/2026/01/copa-airlines-reactivo-la-venta-de-boletos-entre-venezuela-y-ee-uu/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/05/copa-airlines-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "¿Por qué Ecuador elevó en 900% la tarifa de transporte de petróleo colombiano? Respuesta de ministra",
-      "link": "https://www.noticiascaracol.com/economia/por-que-ecuador-elevo-en-900-la-tarifa-de-transporte-de-petroleo-colombiano-respuesta-de-ministra-ex40",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/1b3059e/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F52%2F09%2F3532124144989a544246297c516c%2Fines-manzano.jpg"
+      "title": "Banco de la República fija la tasa de interés en 10,25%: subió 100 puntos básicos",
+      "link": "https://www.noticiascaracol.com/economia/banco-de-la-republica-fija-la-tasa-de-interes-en-10-25-subio-100-puntos-basicos-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/138c1b4/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F9e%2Fff%2F6af7bcca4e0d826a954dba57fa8e%2Ftasa-de-interes.jpg"
     },
     {
       "source": "La Verdad",
@@ -56,10 +56,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Delcy prepara anuncio de amnistía general y cierre de El Helicoide, según Vladimir Villegas",
-      "link": "https://diarioversionfinal.com/politica-dinero/delcy-prepara-anuncio-de-amnistia-general-y-cierre-de-el-helicoide-segun-vladimir-villegas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2024/10/Cancilleria1.jpg"
+      "title": "Presidenta (e) confirma Ley de Amnistía y anuncia que El Helicoide se convertirá en centro social",
+      "link": "https://diarioversionfinal.com/politica-dinero/presidenta-e-confirma-ley-de-amnistia-y-anuncia-que-el-helicoide-se-convertira-en-centro-social/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Laura-Dogu-30-01-2026-750x430.png"
     }
   ],
-  "lastUpdate": "2026-01-30T23:26:13.616Z"
+  "lastUpdate": "2026-01-31T02:00:23.177Z"
 };
