b2e07e4222b8ff578101c8ea69cf794622bf0583
GitHub Action <actions@github.com>
2026-02-06 14:40:53 +0000

    data: update news and rates at Fri Feb  6 14:40:53 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "449,86"
     },
     "binance": {
-      "usdt": "567,67"
+      "usdt": "573,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Diputados opositores pidieron que la amnistía establezca garantías para los presos políticos liberados en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/02/06/diputados-opositores-pidieron-que-la-amnistia-establezca-garantias-para-los-presos-politicos-liberados-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/2QKBPYVHJZAIPM4YDTNFBFMS6M.jpg?auth=1ad8747c42ae292bc21b78df1a250a4217bc726dfeb4a946d14f8f5ee7d1ea57&smart=true&width=350&height=197&quality=85"
+      "title": "Jorge Rodríguez habló de la amnistía en Venezuela: “Nosotros pedimos perdón y tenemos que perdonar también”",
+      "link": "https://www.infobae.com/venezuela/2026/02/06/jorge-rodriguez-hablo-de-la-amnistia-en-venezuela-nosotros-pedimos-perdon-y-tenemos-que-perdonar-tambien/",
+      "image": "https://www.infobae.com/resizer/v2/RC2G7BWNA5GWTC3NRJPLTPOO2A.jpg?auth=222a81e17b084430c0006375eb60c6fc4bab71f1995a13e7c0a7f1968e738081&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -26,15 +26,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Del conejo inocente al conejo malo: Liam y Bad Bunny desnudan la política migratoria de Trump",
+      "title": "El conejo Liam y el conejo Bad Bunny desnudan la política inmigratoria de Trump",
       "link": "https://cnnespanol.cnn.com/2026/02/06/eeuu/conejo-inocente-conejo-malo-liam-bad-bunny-politica-trump-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/liam-conejo-bad-bunny.png?c=16x9&q=h_720,w_1280,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/liam-conejo-bad-bunny-2.png?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "VIDEO: Delcy Rodríguez agradeció a la AN por la Ley de Amnistía: «Debemos superar nuestras diferencias»",
-      "link": "https://caraotadigital.net/venezuela/video-delcy-rodriguez-agradecio-a-la-an-por-la-ley-de-amnistia-debemos-superar-nuestras-diferencias/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/Rodriguez-CD.jpg"
+      "title": "EN VIDEO: Exalcalde de Mérida, Carlos García, fue trasladado a un hospital por el ICE debido a una descompensación",
+      "link": "https://caraotadigital.net/eeuu/en-video-exalcalde-de-merida-carlos-garcia-fue-trasladado-a-un-hospital-por-el-ice-debido-a-una-descompensacion/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/Carlos.jpg"
     },
     {
       "source": "Caracol Noticias",
@@ -44,16 +44,16 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "EE. UU. ataca otro buque en el Pacífico ligado al narcotráfico y mata a 2 tripulantes",
-      "link": "https://laverdad.com/ee-uu-ataca-otro-buque-en-el-pacifico-ligado-al-narcotrafico-y-mata-a-2-tripulantes/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/02/LUCIANO-DALESSANDRO-1.jpg"
+      "title": "Arrestan a hombre por golpear a su hermana",
+      "link": "https://laverdad.com/arrestan-a-hombre-por-golpear-a-su-hermana/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/02/Panama-8-7-Venezuela-J1-Serie-de-Las-Americas.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "EE. UU. potencia su mando en Venezuela con Laura Dogu: \"Su llegada es una demostración de poder\"",
-      "link": "https://diarioversionfinal.com/politica-dinero/ee-uu-potencia-su-mando-en-venezuela-con-laura-dogu-su-llegada-es-una-demostracion-de-poder/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-06-at-9.24.23-AM.jpeg"
+      "title": "Cacería del ICE con datos de Medicaid pone en jaque a hospitales y pacientes inmigrantes",
+      "link": "https://diarioversionfinal.com/xvf-portada/vf-extra/caceria-del-ice-con-datos-de-medicaid-pone-en-jaque-a-hospitales-y-pacientes-inmigrantes/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-06-at-11.12.07-AM-1-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-02-06T13:01:07.858Z"
+  "lastUpdate": "2026-02-06T14:39:22.578Z"
 };
