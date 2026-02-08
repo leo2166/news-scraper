019f0e9dbd557c4b47b48c5e71d8a932ee8a3963
GitHub Action <actions@github.com>
2026-02-08 18:39:13 +0000

    data: update news and rates at Sun Feb  8 18:39:13 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "451,88"
     },
     "binance": {
-      "usdt": "570,10"
+      "usdt": "567,67"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,14 +14,14 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Desconfianza, deserciones y pérdida de beneficios: crecen las fracturas internas en el chavismo tras la captura de Maduro",
-      "link": "https://www.infobae.com/venezuela/2026/02/08/desconfianza-deserciones-y-perdida-de-beneficios-crecen-las-fracturas-internas-en-el-chavismo-tras-la-captura-de-maduro/",
-      "image": "https://www.infobae.com/resizer/v2/D2BPNGHB4BCDHIQ27W2SVCNKQQ.JPG?auth=baa9713daf23b813cc8bd5c95f8fe10412bd31ca388828a642caf56b1d18aec3&smart=true&width=350&height=197&quality=85"
+      "title": "El régimen chavista excarceló al preso político venezolano Juan Pablo Guanipa",
+      "link": "https://www.infobae.com/venezuela/2026/02/08/el-regimen-chavista-excarcelo-al-preso-politico-venezolano-juan-pablo-guanipa/",
+      "image": "https://www.infobae.com/resizer/v2/EUFEEEWHDBHQXFMVPFXZOLB6GY.jpg?auth=47326215144ecddb90116464d9f6903af55700407b379277c0d75a20b841744e&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "\"Que sean todos\": familiares de presos políticos exhibieron pancartas durante el Maratón CAF en Caracas\nPROTESTAS",
-      "link": "https://www.elnacional.com/2026/02/que-sean-todos-familiares-de-presos-politicos-exhibieron-pancartas-durante-el-maraton-caf-en-caracas/",
+      "title": "Juan Pablo Guanipa fue excarcelado luego de ocho meses de detención\nPOLÍTICA",
+      "link": "https://www.elnacional.com/2026/02/juan-pablo-guanipa-fue-excarcelado-luego-de-ocho-meses-de-detencion/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -32,28 +32,28 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "EN VIDEO: El resbalón viral que sufrió Shakira durante su primer concierto en El Salvador tras dos décadas",
-      "link": "https://caraotadigital.net/caraota-show/en-video-el-resbalon-viral-que-sufrio-shakira-durante-su-primer-concierto-en-el-salvador-tras-dos-decadas/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/shakira.jpg"
+      "title": "Juan Pablo Guanipa y Perkins Rocha fueron excarcelados este 8Feb",
+      "link": "https://caraotadigital.net/venezuela/juan-pablo-guanipa-y-perkins-rocha-fueron-excarcelados-este-8feb/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/guanipa-rocha.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Asesinan a dos policías durante operativo en Anorí, Antioquia; un tercer uniformado logró huir",
-      "link": "https://www.noticiascaracol.com/colombia/asesinan-a-dos-policias-durante-operativo-en-anori-antioquia-un-tercer-uniformado-logro-huir-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/7d45857/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F5b%2Ffb%2F05dde2604c71bfb0e15b2dc11ea1%2Ftamano-imagenes-30.jpg"
+      "title": "Revelan video clave del momento exacto en que escoltas de senador Castellanos son atacados en Arauca",
+      "link": "https://www.noticiascaracol.com/colombia/revelan-video-clave-del-momento-exacto-en-que-escoltas-de-senador-castellanos-son-atacados-en-arauca-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/c6a13cd/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F1b%2F1e%2Fdd6d07d24bbfac22b7fff2071752%2Ftamano-imagenes-28.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "ABC: Zapatero propone a Rodríguez Torres como posible ministro de Defensa",
-      "link": "https://laverdad.com/abc-zapatero-propone-a-rodriguez-torres-como-posible-ministro-de-defensa/",
+      "title": "Machado celebra excarcelación de Guanipa: “Eres un héroe y la historia lo reconocerá”",
+      "link": "https://laverdad.com/machado-celebra-excarcelacion-de-guanipa-eres-un-heroe-y-la-historia-lo-reconocera/",
       "image": "https://laverdad.com/wp-content/uploads/2026/02/SaveClip.App_626517030_18554550109017697_7816180784225291009_n.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Foro Penal valora diálogo con Gobierno venezolano sobre la ley de amnistía",
-      "link": "https://diarioversionfinal.com/politica-dinero/foro-penal-valora-dialogo-con-gobierno-venezolano-sobre-la-ley-de-amnistia/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-08-at-12.49.34-PM-750x430.jpeg"
+      "title": "Juan Pablo Guanipa: \"Hoy salimos en libertad, mucho por hablar sobre Venezuela\"",
+      "link": "https://diarioversionfinal.com/politica-dinero/juan-pablo-guanipa-hoy-salimos-en-libertad-mucho-por-hablar-sobre-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/maria-corina-machado-oslo-1280x853-1-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-02-08T17:27:45.242Z"
+  "lastUpdate": "2026-02-08T18:37:45.045Z"
 };
