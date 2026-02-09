9ee8e06435b5b660cf42972a64296796f4bbf8c4
GitHub Action <actions@github.com>
2026-02-09 22:38:48 +0000

    data: update news and rates at Mon Feb  9 22:38:48 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "458,88"
     },
     "binance": {
-      "usdt": "569,50"
+      "usdt": "570,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "La familia de Juan Pablo Guanipa exigió una fe de vida tras su secuestro por parte de un grupo armado chavista en Caracas",
-      "link": "https://www.infobae.com/venezuela/2026/02/09/la-familia-de-juan-pablo-guanipa-exigio-una-fe-de-vida-tras-su-secuestro-por-parte-de-un-grupo-armado-chavista-en-caracas/",
-      "image": "https://www.infobae.com/resizer/v2/LFV7IT7IEBCWLHUVKX7LJ54JWU.JPG?auth=ad54bce1b2f5b8bdcc5664299d1fbbdb78c993b9a7f51d5b9b2eca24c0dad7f1&smart=true&width=350&height=197&quality=85"
+      "title": "“Creen que pueden hacer lo que les da la gana”: la amenaza de Diosdado Cabello tras el arresto arbitrario de Juan Pablo Guanipa",
+      "link": "https://www.infobae.com/venezuela/2026/02/09/creen-que-pueden-hacer-lo-que-les-da-la-gana-la-amenaza-de-diosdado-cabello-tras-el-arresto-arbitrario-de-juan-pablo-guanipa/",
+      "image": "https://www.infobae.com/resizer/v2/DFEPQ3RKNNB6HCPW3OGBV5VCQ4.JPG?auth=e695242a5cccfa2003897e85519e8b1401eb645dab8c124ff19ae971191f1ea1&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -26,27 +26,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Ghislaine Maxwell promete limpiar el nombre de Trump si le concede clemencia",
-      "link": "https://cnnespanol.cnn.com/2026/02/09/eeuu/ghislaine-maxwell-epstein-quinta-enmienda-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/maxwell-g.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "Sheinbaum defiende la soberanía de México frente a EE.UU., pero hace concesiones clave",
+      "link": "https://cnnespanol.cnn.com/2026/02/09/mexico/sheinbaum-soberania-concesiones-eeuu-estrategia-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2251753640-20260109154040689.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Examen de inglés para obtener ciudadanía en Estados Unidos: conoce quiénes pueden quedar exentos",
-      "link": "https://caraotadigital.net/eeuu/examen-de-ingles-para-obtener-ciudadania-en-estados-unidos-conoce-quienes-pueden-quedar-exentos/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2024/09/CIUDADANIA-DE-EEUU-1.jpg"
-    },
-    {
-      "source": "Noticiero Digital",
-      "title": "AN SUSPENDIÓ SESIÓN PARLAMENTARIA DEL MARTES 10 DE FEBRERO",
-      "link": "https://noticierodigital.com/2026/02/an-suspendio-sesion-parlamentaria-del-martes-10-de-febrero/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2024/04/asamblea-nacional.jpg"
+      "title": "Ghislaine Maxwell se negó a responder preguntas ante el Congreso estadounidense, invocó la V Enmienda",
+      "link": "https://caraotadigital.net/eeuu/ghislaine-maxwell-se-nego-a-responder-preguntas-ante-el-congreso-estadounidense-invoco-la-v-enmienda/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2025/10/Ghislaine-Maxwell-2.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "\"Resisto porque mi padre resiste\": el relato del hijo del opositor venezolano Juan Pablo Guanipa",
-      "link": "https://www.noticiascaracol.com/mundo/resisto-porque-mi-padre-resiste-el-relato-del-hijo-del-opositor-venezolano-juan-pablo-guanipa-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/a0e653e/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F9a%2Ff8%2F1ee83e9e4334bb6e3dc0a43ab9c1%2Fjuan-pablo-guanipa-1.jpg"
+      "title": "Violento hurto en calle 100 de Bogotá: ladrones golpearon a mujer y le dispararon con arma de fogueo",
+      "link": "https://www.noticiascaracol.com/colombia/bogota/violento-hurto-en-calle-100-de-bogota-ladrones-golpearon-a-mujer-y-le-dispararon-con-arma-de-fogueo-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/f376e79/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fae%2Fca%2F0c2ab8b7419f943b2a96ba25415c%2Ftamano-imagenes-45.png"
     },
     {
       "source": "La Verdad",
@@ -58,8 +52,8 @@ window.DASHBOARD_DATA = {
       "source": "Diario Versión Final",
       "title": "Maestra de McKinney critica a EE.UU. tras captura de su esposo venezolano por ICE",
       "link": "https://diarioversionfinal.com/politica-dinero/maestra-de-mckinney-critica-a-trump-tras-captura-de-su-esposo-venezolano-por-ice/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/apV4MgVJ-image.jpg"
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/gtdf-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-02-09T21:39:30.680Z"
+  "lastUpdate": "2026-02-09T22:37:11.787Z"
 };
