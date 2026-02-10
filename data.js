6fdb5ed3e87d9406987dc3553410b51a7767233f
GitHub Action <actions@github.com>
2026-02-10 18:04:42 +0000

    data: update news and rates at Tue Feb 10 18:04:42 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "458,88"
     },
     "binance": {
-      "usdt": "580,66"
+      "usdt": "576,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,46 +14,46 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "La Oficina de Derechos Humanos de la ONU confirmó contactos con Venezuela para reanudar su presencia en el país",
-      "link": "https://www.infobae.com/venezuela/2026/02/10/la-oficina-de-derechos-humanos-de-la-onu-confirmo-contactos-con-venezuela-para-reanudar-su-presencia-en-el-pais/",
-      "image": "https://www.infobae.com/resizer/v2/ECEDZGQQ7ZABRGH6B2YYZRLJOA.jpg?auth=6efb7d7d49d97c79cf83fb06710d3caceb3a4d612a68cda6ad9e4026fc817924&smart=true&width=350&height=197&quality=85"
+      "title": "Activistas políticos exigieron justicia por Juan Pablo Guanipa",
+      "link": "https://www.infobae.com/venezuela/2026/02/10/activistas-politicos-exigieron-justicia-por-juan-pablo-guanipa/",
+      "image": "https://www.infobae.com/resizer/v2/WJWR3DUNXRCWFDZ2RUHRUKXMW4.jpg?auth=b4de27a6c36c2584b59064040ea1e15f77048da7598e02eb1c4fd76e389ba901&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Juan Pablo Guanipa ya está en Maracaibo para cumplir el régimen de casa por cárcel\nPOLÍTICA",
-      "link": "https://www.elnacional.com/2026/02/juan-pablo-guanipa-ya-esta-en-maracaibo-preso-en-su-casa/",
+      "title": "\"No más anuncios incumplidos\": familiares de presos políticos protestaron frente a la Asamblea Nacional\nPOLÍTICA",
+      "link": "https://www.elnacional.com/2026/02/familiares-de-presos-politicos-protestaron-en-la-asamblea-nacional/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
       "title": "\"Son tiempos muy difíciles\": el plan de Cuba para sobrevivir a una de sus peores crisis",
       "link": "https://cnnespanol.cnn.com/2026/02/10/latinoamerica/cuba-medidas-crisis-energetica-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/videothumbnails/79533364-39050564-generated-thumbnail.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/filas-cuba.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Liberan a trabajador humanitario colombiano que llevaba 17 meses encarcelado en el país",
-      "link": "https://caraotadigital.net/venezuela/venezuela-libera-a-trabajador-humanitario-colombiano-que-llevaba-17-meses-encarcelado/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/TIQUE2026-02-10-at-11.10.20-AM.jpg"
+      "title": "Asalto estilo película: Simularon un punto de control policial en Italia y «volaron» la puerta de un camión blindado",
+      "link": "https://caraotadigital.net/mundo/asalto-estilo-pelicula-simularon-un-punto-de-control-policial-en-italia-y-volaron-la-puerta-de-un-camion-blindado/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/Italia-2.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Esto respondieron precandidatos de La Gran Consulta por Colombia durante debate en Noticias Caracol",
-      "link": "https://www.noticiascaracol.com/politica/elecciones-colombia/siga-aqui-el-segundo-debate-de-precandidatos-de-la-gran-consulta-por-colombia-en-noticias-caracol",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/5543b04/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F69%2Fc8%2F9942f8fb4e89a3d0fec56b7ce458%2Fdebate-segunda-parte-gran-consulta-por-colombia.jpg"
+      "title": "Ojo con su vuelo: ¿en qué consiste el ‘plan tortuga’ de trabajadores de Migración Colombia?",
+      "link": "https://www.noticiascaracol.com/colombia/bogota/ojo-con-su-vuelo-en-que-consiste-el-plan-tortuga-de-trabajadores-de-migracion-colombia-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/b1ad0c0/2147483647/strip/true/crop/1280x722+0+2/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fa9%2F14%2F1a6e28f044a78854f9b15bd49fe9%2Fmigracion-colombia-en-aerepuerto-el-dorado.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "UE reitera que el cambio de “régimen” en Venezuela debe venir desde dentro",
-      "link": "https://laverdad.com/ue-reitera-que-el-cambio-de-regimen-en-venezuela-debe-venir-desde-dentro/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/02/Esquiador-venezolano-Nicolas-Claveau-debuta-en-los-Juegos-Olimpicos-de-Invierno.jpg"
+      "title": "Liberan al activista colombiano Manuel Tique tras 17 meses encarcelado",
+      "link": "https://laverdad.com/liberan-al-activista-colombiano-manuel-tique-tras-17-meses-encarcelado/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/02/Yvan-Martinez-1.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Ramón Guanipa confirma que su padre se encuentra en Maracaibo",
-      "link": "https://diarioversionfinal.com/politica-dinero/ramon-guanipa-confirma-que-su-padre-se-encuentra-en-maracaibo/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-10-at-12.18.59-PM-750x430.jpeg"
+      "title": "Diputados atienden reclamos de familiares de presos políticos en el Parlamento",
+      "link": "https://diarioversionfinal.com/politica-dinero/diputados-atienden-reclamos-de-familiares-de-presos-politicos-en-el-parlamento/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/conviasa-inaugura-ruta-aerea-puerto-ordaz-manaos-124027-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-02-10T16:13:07.781Z"
+  "lastUpdate": "2026-02-10T18:03:00.271Z"
 };
