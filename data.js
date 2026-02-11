33ae4b36542f512692880b2f275e8fc5977aec4f
GitHub Action <actions@github.com>
2026-02-11 17:58:06 +0000

    data: update news and rates at Wed Feb 11 17:58:06 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "462,78"
     },
     "binance": {
-      "usdt": "550,00"
+      "usdt": "575,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -20,40 +20,40 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "El Nacional",
-      "title": "Secretario de Energía de EE UU llega este miércoles a Venezuela para evaluar sector petrolero\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/02/secretario-de-energia-de-ee-uu-llega-este-miercoles-a-venezuela/",
+      "title": "Secretario de Energía de EE UU fue recibido por Laura Dogu tras aterrizar en Caracas\nVENEZUELA",
+      "link": "https://www.elnacional.com/2026/02/llego-a-venezuela-el-secretario-de-energia-de-ee-uu/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Drones de un cartel mexicano violaron el espacio aéreo de EE.UU.",
-      "link": "https://cnnespanol.cnn.com/2026/02/11/eeuu/drones-cartel-mexicano-violaron-espacio-aereo-el-paso-texas-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/drones-espacio-aereo-usa.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "Secretario de Energía de EE.UU. visita Venezuela",
+      "link": "https://cnnespanol.cnn.com/venezuela/live-news/secretario-energia-eeuu-visita-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/wright-delcy-venezuela.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Caraota Digital",
-      "title": "Secretario de Energía de EEUU arribó a Maiquetía: Estos serían sus planes en el país",
-      "link": "https://caraotadigital.net/venezuela/secretario-de-energia-de-eeuu-arribo-a-maiquetia-estos-serian-sus-planes-en-el-pais/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/secretarioenergia181823.jpg"
+      "title": "Los nuevos detalles que debes conocer sobre el final de Stranger Things y el destino de Eleven, según los creadores de la famosa serie",
+      "link": "https://caraotadigital.net/caraota-show/los-nuevos-detalles-que-debes-conocer-sobre-el-final-de-stranger-things-y-el-destino-de-eleven-segun-los-creadores-de-la-famosa-serie/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/Stranger-Things.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "En medio de emergencia en Córdoba, hallan cuerpo de joven arrastrado por corriente en San Pelayo",
-      "link": "https://www.noticiascaracol.com/colombia/en-medio-de-emergencia-en-cordoba-hallan-cuerpo-de-joven-arrastrado-por-corriente-en-san-pelayo-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/75190a5/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fb5%2F73%2Fb7b734f841919f55b8f6791db9eb%2Finundaciones-cordoba-1.jpg"
+      "title": "Petro denunció complot desde la Policía: habló de supuesto intento de sabotear reunión con Trump",
+      "link": "https://www.noticiascaracol.com/politica/petro-denuncio-complot-desde-la-policia-hablo-de-supuesto-intento-de-sabotear-reunion-con-trump-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/b610615/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fb7%2Ff6%2Fc7637c2e4e28acdb640c6d1627e9%2Fgustavo-petro-5.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Detienen a 6 miembros de una familia tras riña en un “baby shower”",
-      "link": "https://laverdad.com/detienen-a-6-miembros-de-una-familia-tras-rina-en-un-baby-shower/",
+      "title": "Dos migrantes venezolanas mueren en naufragio de lancha en el Caribe panameño",
+      "link": "https://laverdad.com/dos-migrantes-venezolanas-mueren-en-naufragio-de-lancha-en-el-caribe-panameno/",
       "image": "https://laverdad.com/wp-content/uploads/2026/02/Britney-Spears-2.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Secretario de Energía de EE. UU. aterriza en Maiquetía en visita oficial de alto nivel",
-      "link": "https://diarioversionfinal.com/politica-dinero/secretario-de-energia-de-ee-uu-aterriza-en-maiquetia-en-visita-oficial-de-alto-nivel/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-11-at-12.42.41-PM-750x430.jpeg"
+      "title": "Venezolana que trabaja con gobernadora de Massachusetts demanda a Trump por retraso de su visa",
+      "link": "https://diarioversionfinal.com/xvf-portada/vf-extra/venezolana-que-trabaja-con-gobernadora-de-massachusetts-demanda-a-trump-por-retraso-de-su-visa/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/gold-reserve-venezuela-scaled-1-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-02-11T16:02:41.672Z"
+  "lastUpdate": "2026-02-11T17:56:30.995Z"
 };
