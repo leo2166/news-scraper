0ebbbe1cd9ec70c0208ad79819add4173452ee6c
GitHub Action <actions@github.com>
2026-02-13 15:43:43 +0000

    data: update news and rates at Fri Feb 13 15:43:43 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "467,33"
     },
     "binance": {
-      "usdt": "563,50"
+      "usdt": "564,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -32,28 +32,28 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Caraota Digital",
-      "title": "«Estoy soñando»: Así fue el espectacular debut de la Miss Venezuela en la Semana de la Moda de Nueva York",
-      "link": "https://caraotadigital.net/caraota-show/estoy-sonando-asi-fue-el-espectacular-debut-de-la-miss-venezuela-en-la-semana-de-la-moda-de-nueva-york/",
-      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/SaveClip.App_634084629_17924928831232765_5338134928811590346_n-1.jpg"
+      "title": "OFICIAL: Shakira ofrecerá en mayo un concierto gratuito en la playa brasileña de Copacabana",
+      "link": "https://caraotadigital.net/caraota-show/oficial-shakira-ofrecera-en-mayo-un-concierto-gratuito-en-la-playa-brasilena-de-copacabana/",
+      "image": "https://caraotadigital.net/wp-content/uploads/2026/02/SHAKIRA18131.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Benedetti reacciona a suspensión provisional de decreto del salario mínimo 2026: \"Decisión política\"",
-      "link": "https://www.noticiascaracol.com/politica/benedetti-reacciona-a-suspension-provisional-de-decreto-del-salario-minimo-2026-decision-politica-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/43a2fdc/2147483647/strip/true/crop/1280x722+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fd1%2Fee%2F26aef2144050a6d682d6e9ebffae%2Farmando-benetti.jpg"
+      "title": "¿Qué pasará con el salario mínimo mientras Gobierno expide nuevo \"decreto transitorio\"?",
+      "link": "https://www.noticiascaracol.com/economia/que-pasara-con-el-salario-minimo-mientras-gobierno-expide-nuevo-decreto-transitorio-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/da17bc2/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F95%2F32%2F420659574ac794a7c15b6e169eb7%2Fdiseno-sin-titulo-2024-12-05t114327-256.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Familias de presos políticos se encadenan en Zona 7 por retraso en debate sobre amnistía",
-      "link": "https://laverdad.com/familias-de-presos-politicos-se-encadenan-en-zona-7-por-retraso-en-debate-sobre-amnistia/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/02/la-ultima-1.jpg"
+      "title": "Cámara de Comercio consolida liderazgo en el Directorio Itinerante de Fedecámaras",
+      "link": "https://laverdad.com/camara-de-comercio-consolida-liderazgo-en-el-directorio-itinerante-de-fedecamaras/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/02/Venezuela-9-1-Cuba-Serie-de-las-Americas-1.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Delcy Rodríguez asegura eleciones libres en Venezuela sin anunciar fecha",
+      "title": "Delcy Rodríguez garantiza \"elecciones libres\" en Venezuela sin anunciar fecha",
       "link": "https://diarioversionfinal.com/politica-dinero/delcy-rodriguez-asegura-eleciones-libres-en-venezuela-sin-anunciar-fecha/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/11/papa-leon-xiv-en-audiencia-general-del-13-de-agosto-01-750x430.jpg"
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/WhatsApp-Image-2026-02-13-at-11.12.03-AM-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-02-13T14:40:45.374Z"
+  "lastUpdate": "2026-02-13T15:42:13.907Z"
 };
