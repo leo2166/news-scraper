1efcd8f1eb2e132d61b8503d446120370d55de21
GitHub Action <actions@github.com>
2026-02-19 20:37:57 +0000

    data: update news and rates at Thu Feb 19 20:37:57 UTC 2026

--- data.js
+++ data.js
@@ -20,21 +20,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "El Nacional",
-      "title": "María Corina Machado coordina con Trump la hoja de ruta para su regreso\nPOLÍTICA",
+      "title": "María Corina Machado coordina con Trump la hoja de ruta para su regreso a Venezuela\nPOLÍTICA",
       "link": "https://www.elnacional.com/2026/02/maria-corina-machado-coordina-con-trump-la-hoja-de-ruta-para-su-regreso/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Tras el arresto de su hermano, el rey Carlos dice que \"la ley debe seguir su curso\"",
+      "title": "Liberan al expríncipe Andrés y seguirá \"bajo investigación\"",
       "link": "https://cnnespanol.cnn.com/mundo/live-news/arrestan-andrew-mountbatten-windsor-mala-conducta-vivo",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2025-09-16t163716z-1214613565-rc23tga9x7cg-rtrmadp-3-britain-royals-kent-20260219124609344.jpeg?c=16x9&q=h_720,w_1280,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1245815868-20260219130518321.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Dr. Ramón Escovar León: \"La Ley de Amnistía requiere voluntad política y una redacción sin ambigüedades\"",
-      "link": "https://noticiasvenevision.com/noticias/politica/dr-ramon-escovar-leon-la-ley-de-amnistia-requiere-voluntad-politica-y-una-redaccion-sin-ambiguedades",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-02-19-at-5.55.58-am-716da6.jpeg"
+      "title": "Nora Bracho propone Comisión Especial para garantizar el cumplimiento de la Ley de Amnistía",
+      "link": "https://noticiasvenevision.com/noticias/politica/nora-bracho-propone-comision-especial-para-garantizar-el-cumplimiento-de-la-ley-de-amnistia",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/nora-bracho-1068x743-9c68b9.jpg"
     },
     {
       "source": "Caracol Noticias",
@@ -44,16 +44,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "Cuatro fallecidos y 17 heridos en Chile tras explosión de camión con gas",
-      "link": "https://laverdad.com/cuatro-fallecidos-y-17-heridos-en-chile-tras-explosion-de-camion-con-gas/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/02/Camion-de-gas-exploto-en-Chile.jpg"
-    },
-    {
-      "source": "Diario Versión Final",
-      "title": "Mark Zuckerberg se defiende durante juicio histórico por adicción de jóvenes a redes sociales",
-      "link": "https://diarioversionfinal.com/mundo/mark-zuckerberg-se-defiende-durante-juicio-historico-por-adiccion-de-jovenes-a-redes-sociales/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/dbb256f2f1f0cfe2e221493359577b87-750x430.webp"
+      "title": "El rey Carlos III reacciona a la detención de su hermano Andrés: “La ley debe seguir su curso”",
+      "link": "https://laverdad.com/el-rey-carlos-iii-reacciona-a-la-detencion-de-su-hermano-andres-la-ley-debe-seguir-su-curso/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/02/Carlos-III.jpg"
     }
   ],
-  "lastUpdate": "2026-02-19T19:39:16.900Z"
+  "lastUpdate": "2026-02-19T20:34:45.506Z"
 };
