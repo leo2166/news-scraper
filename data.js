9a68447c6f9f2be7380789ea063d1aa89616d6e8
GitHub Action <actions@github.com>
2026-02-20 04:48:50 +0000

    data: update news and rates at Fri Feb 20 04:48:50 UTC 2026

--- data.js
+++ data.js
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Venezuela reclama una negociación directa con Guyana por el Esequibo y rechaza la jurisdicción de La Haya",
-      "link": "https://www.infobae.com/venezuela/2026/02/20/venezuela-reclama-una-negociacion-directa-con-guyana-por-el-esequibo-y-rechaza-la-jurisdiccion-de-la-haya/",
-      "image": "https://www.infobae.com/resizer/v2/BSVAMQXHRJB2NC2MTDDRJVALK4.jpg?auth=2f25b32c4166e79732b4788a8fe462e9b690ff077d97b365d17f17a4ca58383f&smart=true&width=350&height=197&quality=85"
+      "title": "Delcy Rodríguez promulgó la ley de amnistía en Venezuela: “Hay que saber pedir perdón”",
+      "link": "https://www.infobae.com/venezuela/2026/02/20/delcy-rodriguez-promulgo-la-ley-de-amnistia-en-venezuela-hay-que-saber-pedir-perdon/",
+      "image": "https://www.infobae.com/resizer/v2/PG5LYP3VBVFEPCNGACHFNHQTWU.jpg?auth=c2eabcf280e17286c9ae41873131f0abe20f50e4feebb8f1d945abf2e7090cee&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -26,21 +26,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Venezuela aprueba la ley de amnistía para los presos políticos",
+      "title": "Venezuela aprueba histórica ley de amnistía para los presos políticos",
       "link": "https://cnnespanol.cnn.com/2026/02/19/venezuela/asamblea-nacional-aprueba-ley-amnistia-presos-politicos-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/119da574-01b5-493b-86d5-5d0dcac9ab5c.png?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/119da574-01b5-493b-86d5-5d0dcac9ab5c.png?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Aprobada Ley de Amnistía para la Convivencia Democrática",
-      "link": "https://noticiasvenevision.com/noticias/politica/aprobada-ley-de-amnistia-para-la-convivencia-democratica",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/hbj2vloxcaalzvw-e0da79.jpg"
+      "title": "Hijo de Juan Pablo Guanipa informa liberación de su padre",
+      "link": "https://noticiasvenevision.com/noticias/politica/hijo-de-juan-pablo-guanipa-informa-liberacion-de-su-padre",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/l5gqbya7hpexayfihopuvforsy-fad5b2.avif"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Ley de Amnistía es aprobada en Venezuela, pero excluye a condenados por promover acciones armadas",
-      "link": "https://www.noticiascaracol.com/mundo/aprueban-ley-de-amnistia-en-venezuela-que-contempla-cb20",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/67d6b9a/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F60%2F27%2F3d4508b44e6fa5276992aca3d43a%2Fley-amnistia-1.jpg"
+      "title": "Delcy Rodríguez firma Ley de Amnistía y pide \"máxima celeridad\" para que se aplique en Venezuela",
+      "link": "https://www.noticiascaracol.com/mundo/delcy-rodriguez-firma-ley-de-amnistia-y-pide-maxima-celeridad-para-que-se-aplique-en-venezuela-cb20",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/5250520/2147483647/strip/true/crop/1276x720+3+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fb3%2F16%2Fd4f103b14d2db62feff50b32e7cd%2Fley-de-amnistia-firma.jpg"
     },
     {
       "source": "La Verdad",
@@ -55,5 +55,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/IMG_0091-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-02-20T02:07:11.592Z"
+  "lastUpdate": "2026-02-20T04:47:15.157Z"
 };
