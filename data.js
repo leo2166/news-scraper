dff19e953c05b0b70a07ae92ba6ea8f490e79fbf
GitHub Action <actions@github.com>
2026-03-07 04:07:38 +0000

    data: update news and rates at Sat Mar  7 04:07:38 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "501,73"
     },
     "binance": {
-      "usdt": "647,10"
+      "usdt": "647,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -26,7 +26,7 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Israel lanza una nueva oleada de ataques a Teherán",
+      "title": "Una nueva ola de ataques a Irán alcanza uno de los aeropuertos de Teherán",
       "link": "https://cnnespanol.cnn.com/mundo/live-news/guerra-eeuu-israel-iran-medio-oriente-en-vivo-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/renamethishorizontal-00-00-32-20-still001.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
@@ -37,16 +37,22 @@ window.DASHBOARD_DATA = {
       "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/img_5799-2048x1638-83bc52.jpg"
     },
     {
+      "source": "Noticiero Digital",
+      "title": "DEPARTAMENTO DE JUSTICIA DE EE. UU. ABRIRÍA INVESTIGACIONES CONTRA FUNCIONARIOS CUBANOS",
+      "link": "https://noticierodigital.com/2026/03/departamento-de-justicia-de-ee-uu-abriria-investigaciones-contra-funcionarios-cubanos/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/la-habana-cuba-627x376.jpg"
+    },
+    {
       "source": "Caracol Noticias",
-      "title": "¿Habrá lluvias en Colombia este fin de semana electoral? Esto dice Ideam",
-      "link": "https://www.noticiascaracol.com/colombia/habra-lluvias-en-colombia-este-fin-de-semana-electoral-esto-dice-ideam-so35",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/9b48fea/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F99%2F12%2Ffa1e426941558d3bdfa163121a1a%2Fideam.jpg"
+      "title": "Habrá ley seca este fin de semana: ojo a las multas para quien consuma o venda alcohol en Bogotá",
+      "link": "https://www.noticiascaracol.com/politica/elecciones-colombia/habra-ley-seca-este-fin-de-semana-ojo-a-las-multas-para-quien-consuma-o-venda-alcohol-en-bogota-so35",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/983c3f2/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fad%2F3b%2F9ae0bdda494eba27dcfbddd42782%2Fley-seca-marzo-2026-cual-es-la-multa-para-quien-consuma-o-venda-alcohol.jpg"
     },
     {
       "source": "La Verdad",
       "title": "El monito Punch se entristece luego que le quitan a su “madre peluche” Lupe",
       "link": "https://laverdad.com/el-monito-punch-se-entristece-luego-que-le-quitan-a-su-madre-peluche-lupe/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/03/Monito.jpg"
+      "image": "https://laverdad.com/pagespeed_static/1.JiBnMqyl6S.gif"
     },
     {
       "source": "Diario Versión Final",
@@ -55,5 +61,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/WhatsApp-Image-2026-03-06-at-9.51.13-PM-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-03-07T01:59:28.282Z"
+  "lastUpdate": "2026-03-07T04:06:08.557Z"
 };
