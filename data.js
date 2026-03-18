94004f728331ba50fd5fe417710c0a70c3be0cf7
GitHub Action <actions@github.com>
2026-03-18 20:39:22 +0000

    data: update news and rates at Wed Mar 18 20:39:22 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "520,64"
     },
     "binance": {
-      "usdt": "696,50"
+      "usdt": "699,99"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,7 +14,7 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Quién es Gustavo González López, el general que Delcy Rodríguez nombró al frente del Ministerio de Defensa",
+      "title": "Quién es Gustavo González López, el general al que Delcy Rodríguez nombró al frente del Ministerio de Defensa",
       "link": "https://www.infobae.com/venezuela/2026/03/18/quien-es-gustavo-gonzalez-lopez-el-general-que-delcy-rodriguez-nombro-al-frente-del-ministerio-de-defensa/",
       "image": "https://www.infobae.com/resizer/v2/4XNJFNOYXRH3XIAOB5BO6NKBIE.jpg?auth=0d29945352615f8722c76f4beb9fc47eef341353a2d8cb66662a7625fac2afa8&smart=true&width=350&height=197&quality=85"
     },
@@ -32,34 +32,34 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Ernesto Villegas solicita suspensión de su militancia en el PSUV y Movimiento Futuro para aspirar a la Defensoría",
-      "link": "https://noticiasvenevision.com/noticias/politica/ernesto-villegas-solicita-suspension-de-su-militancia-en-el-psuv-y-movimiento-futuro-para-aspirar-a-la-defensoria",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/ernesto-1-56177a.jpg"
+      "title": "Presidenta (E) Delcy Rodríguez designa a Gustavo González López como nuevo Ministro de la Defensa",
+      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-e-delcy-rodriguez-designa-a-gustavo-gonzalez-lopez-como-nuevo-ministro-de-la-defensa",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/dwazbccwoaeuqvz-ed31be.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EL PITAZO: DENUNCIAN QUE PNB ACRIBILLÓ A ADOLESCENTE TRAS TORTURARLO EN SU PROPIA CASA",
-      "link": "https://noticierodigital.com/2026/03/el-pitazo-denuncian-que-pnb-acribillo-a-adolescente-tras-torturarlo-en-su-propia-casa/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/ElPitazo30deabril_PNB-627x376.jpg"
+      "title": "CONINDUSTRIA Y MIN. INDUSTRIA INSTALARÁN MESAS DE TRABAJO PARA IMPULSAR PRODUCCIÓN NACIONAL",
+      "link": "https://noticierodigital.com/2026/03/conindustria-y-min-industria-instalaran-mesas-de-trabajo-para-impulsar-produccion-nacional/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/imagen_2026-03-18_155642628-627x376.png"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Señalada en crimen de Jaime Esteban Moreno se convierte en la más buscada por homicidio en Bogotá",
-      "link": "https://www.noticiascaracol.com/judicial/paola-fernandez-senalada-en-crimen-de-jaime-esteban-moreno-se-convierte-en-la-mas-buscada-por-homicidio-en-bogota-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/189569a/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fff%2Fef%2F334e492c407c876f13e9184aa59f%2Fdiseno-1.jpg"
+      "title": "Vladimir Padrino deja de ser el ministro de Defensa de Venezuela: este funcionario será su reemplazo",
+      "link": "https://www.noticiascaracol.com/mundo/vladimir-padrino-deja-de-ser-el-ministro-de-venezuela-este-funcionario-sera-su-reemplazo-cb20",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/e4ebbac/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F00%2F76%2F90ec6fab48569d5948ec61e1bc48%2Fpadrino-delcy.jpg"
     },
     {
       "source": "La Verdad",
       "title": "Colombia confirma 14 muertos en frontera con Ecuador pero sin vínculo aparente con bombas",
       "link": "https://laverdad.com/colombia-confirma-14-muertos-en-frontera-con-ecuador-pero-sin-vinculo-aparente-con-bombas/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/03/Ministro-defensa-colombia.jpg"
+      "image": "https://laverdad.com/pagespeed_static/1.JiBnMqyl6S.gif"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Kast inicia construcción de zanja en la frontera con Perú para frenar imigracion ilegal",
+      "title": "Kast inicia construcción de zanja en la frontera con Perú para frenar inmigración ilegal",
       "link": "https://diarioversionfinal.com/mundo/kast-inicia-construccion-de-zanja-en-la-frontera-con-peru-para-frenar-imigracion-ilegal/",
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/Jose-Antonio-Kast-2-15-12-2025-750x430.png"
     }
   ],
-  "lastUpdate": "2026-03-18T19:52:20.070Z"
+  "lastUpdate": "2026-03-18T20:37:52.410Z"
 };
