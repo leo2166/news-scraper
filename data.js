5fc5b66235c2e3814755bed89c28c0e5c7cd0065
GitHub Action <actions@github.com>
2026-03-20 02:09:19 +0000

    data: update news and rates at Fri Mar 20 02:09:19 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "524,51"
     },
     "binance": {
-      "usdt": "694,49"
+      "usdt": "692,68"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,52 +14,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "El partido de María Corina Machado saludó que Volker Türk pida aclarar el alcance de la amnistía en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/03/19/el-partido-de-maria-corina-machado-saludo-que-volker-turk-pida-aclarar-el-alcance-de-la-amnistia-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/OFDUCSQHEBE75LLFO5JWDCTHWU.jpg?auth=9c0eba458950f7d2f65c8eaa5e5a53e093613309d6bff21d7b2de9fba0dc748c&smart=true&width=350&height=197&quality=85"
+      "title": "Acceso a la Justicia cuestionó la aplicación de la ley de Amnistía en Venezuela y advirtió sobre la falta de garantías judiciales",
+      "link": "https://www.infobae.com/venezuela/2026/03/20/acceso-a-la-justicia-cuestiono-la-aplicacion-de-la-ley-de-amnistia-en-venezuela-y-advirtio-sobre-la-falta-de-garantias-judiciales/",
+      "image": "https://www.infobae.com/resizer/v2/XKBZA4T4SJBCFD25I6KQQB3UL4.jpg?auth=4dd4e6762433021b6e66851d4b4f3de8888c1826a88a4af701fe3c1476e3fcd9&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Rick Scott se reúne con María Corina Machado y destaca su compromiso con democracia venezolana\nMUNDO",
+      "title": "Rick Scott se reúne con María Corina Machado y reafirma su compromiso con la causa democrática venezolana\nMUNDO",
       "link": "https://www.elnacional.com/2026/03/rick-scott-se-reune-con-maria-corina-machado-y-destaca-su-compromiso-con-democracia-venezolana/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump afirma haberle dicho a Netanyahu que detuviera los ataques contra instalaciones energéticas",
-      "link": "https://cnnespanol.cnn.com/mundo/live-news/guerra-ee-uu-israel-iran-ataques-medio-oriente-vivo-2-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/whatsapp-image-2026-03-19-at-16-18-42-1.jpeg?c=16x9&q=h_144,w_256,c_fill"
+      "title": "Dos petroleros con banderas de Rusia y Hong Kong estarían camino a Cuba",
+      "link": "https://cnnespanol.cnn.com/2026/03/19/latinoamerica/buques-rusia-china-petroleo-cuba-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/whatsapp-image-2026-03-19-at-16-18-42-1.jpeg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Delcy Rodríguez juramenta al nuevo Gabinete Ejecutivo",
-      "link": "https://noticiasvenevision.com/noticias/politica/delcy-rodriguez-juramenta-al-nuevo-gabinete-ejecutivo",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/5035510561273219995-49ac6b.jpg"
+      "title": "Presidenta encargada anuncia y juramenta al nuevo alto mando militar",
+      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-encargada-anuncia-al-nuevo-alto-mando-militar",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-03-19-at-9.57.10-pm-fe711c.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "AN DIFIRIÓ LA SEGUNDA DISCUSIÓN DEL PROYECTO DE LEY DE MINAS",
-      "link": "https://noticierodigital.com/2026/03/an-difirio-la-segunda-discusion-del-proyecto-de-ley-de-minas/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2019/12/an_asamblea_nacional_16dic2019.jpg"
+      "title": "CASETEL PLANTEÓ LA NECESIDAD DE REVISAR LAS TARIFAS DE TELECOMUNICACIONES",
+      "link": "https://noticierodigital.com/2026/03/casetel-planteo-la-necesidad-de-revisar-las-tarifas-de-telecomunicaciones/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/telecomunicaciones-venezuela-casetel-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Policía frustra atentado terrorista en el Huila: hicieron detonación controlada de artefactos",
-      "link": "https://www.noticiascaracol.com/colombia/policia-frustra-atentado-terrorista-en-el-departamento-del-huila-hicieron-detonacion-controlada-de-artefactos-explosivos-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/86fa21e/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fa1%2Faf%2Fa3807208456abff852880e096ca0%2Fdiseno.jpg"
+      "title": "Gobierno rechaza amenaza de disidencias de prohibir acceso de misiones humanitarias en territorios",
+      "link": "https://www.noticiascaracol.com/colombia/gobierno-rechaza-amenaza-de-disidencias-de-prohibir-acceso-de-misiones-humanitarias-en-territorios-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/681198f/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fe0%2F69%2F22f7dc3f4eedb71a7481f56d5633%2Fmaduro-31.jpg"
     },
     {
       "source": "La Verdad",
       "title": "Detienen a 2 venezolanos en Ecuador vinculados con el “Tren de Aragua”",
       "link": "https://laverdad.com/detienen-a-2-venezolanos-en-ecuador-vinculados-con-el-tren-de-aragua/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/03/Detenidos-dos-venezolanos-en-Ecuador-a-los-que-el-Gobierno-vincula-al-Tren-de-Aragua.jpg"
+      "image": "https://laverdad.com/pagespeed_static/1.JiBnMqyl6S.gif"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Irán ahorca a un campeón de lucha de 19 años por protestar contra el régimen",
-      "link": "https://diarioversionfinal.com/mundo/iran-ahorca-a-un-campeon-de-lucha-de-19-anos-por-protestar-contra-el-regimen/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/IMG_1455-750x430.webp"
+      "title": "Finlandia lidera el Informe Mundial sobre la Felicidad por noveno año seguido",
+      "link": "https://diarioversionfinal.com/mundo/finlandia-lidera-el-informe-mundial-sobre-la-felicidad-por-noveno-ano-seguido/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Helsinki-19-03-2026-750x430.png"
     }
   ],
-  "lastUpdate": "2026-03-19T23:30:34.602Z"
+  "lastUpdate": "2026-03-20T02:07:48.600Z"
 };
