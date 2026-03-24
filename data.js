dd53adfe17f7bb4021087aa667eab80b039cb85f
GitHub Action <actions@github.com>
2026-03-24 20:43:38 +0000

    data: update news and rates at Tue Mar 24 20:43:38 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "533,46"
     },
     "binance": {
-      "usdt": "689,50"
+      "usdt": "697,90"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,9 +14,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Trabajadores venezolanos marcharon por tercera vez en el año en reclamo de salarios dignos",
-      "link": "https://www.infobae.com/venezuela/2026/03/24/trabajadores-venezolanos-marcharon-por-tercera-vez-en-el-ano-en-reclamo-de-salarios-dignos/",
-      "image": "https://www.infobae.com/resizer/v2/IZ44YWFP6VA75EDQWGOEUHBNZM.jpg?auth=d1351d629f93bbb7ab6c7865fdca6cc3cfbc27bfad4427aefd2b4da2848d4089&smart=true&width=350&height=197&quality=85"
+      "title": "Una delegación venezolana viajará a Washington para avanzar en el restablecimiento de relaciones diplomáticas con Estados Unidos",
+      "link": "https://www.infobae.com/venezuela/2026/03/24/una-delegacion-venezolana-viajara-a-washington-para-avanzar-en-el-restablecimiento-de-relaciones-diplomaticas-con-estados-unidos/",
+      "image": "https://www.infobae.com/resizer/v2/QDOM6Q7ADFHWNELJ6DBRZPOTWA.JPG?auth=3c42c560604898ad844253c41655f70c4d0ade3f06d147258bd9589053c93265&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
@@ -37,12 +37,6 @@ window.DASHBOARD_DATA = {
       "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/gettyimages-2261136143-59e8fa.jpg"
     },
     {
-      "source": "Noticiero Digital",
-      "title": "DEPARTAMENTO DE ESTADO DE EE. UU. AFIRMA QUE AUMENTA CIRCULACIÓN DE DÓLARES EN VENEZUELA ",
-      "link": "https://noticierodigital.com/2026/03/departamento-de-estado-de-ee-uu-afirma-que-aumenta-circulacion-de-dolares-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/06/dolares-venezuela-referencial-627x376.jpg"
-    },
-    {
       "source": "Caracol Noticias",
       "title": "Estos son los nombres y rostros de las víctimas del accidente aéreo que ocurrió en Putumayo",
       "link": "https://www.noticiascaracol.com/colombia/estos-son-los-nombres-y-rostros-de-las-victimas-del-accidente-aereo-que-ocurrio-en-putumayo-rg10",
@@ -56,10 +50,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Marco Rubio testifica en el juicio contra David Rivera por presunto lobby venezolano",
-      "link": "https://diarioversionfinal.com/mundo/marco-rubio-testifica-en-el-juicio-contra-david-rivera-por-presunto-lobby-venezolano/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/Marco-Rubio-2-28-01-2026-750x430.jpg"
+      "title": "Trump emite licencia para reaperturar la embajada de Venezuela en EE. UU.",
+      "link": "https://diarioversionfinal.com/mundo/trump-emite-licencia-para-reaperturar-la-embajada-de-venezuela-en-ee-uu/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Embassy_of_Venezuela_-_Washington_D.C.jpg"
     }
   ],
-  "lastUpdate": "2026-03-24T19:07:53.572Z"
+  "lastUpdate": "2026-03-24T20:41:56.386Z"
 };
