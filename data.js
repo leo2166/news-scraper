e5d9ff69c3f85022b237cf869300cb1c2e5aabfa
GitHub Action <actions@github.com>
2026-03-24 06:01:33 +0000

    data: update news and rates at Tue Mar 24 06:01:33 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "533,46"
     },
     "binance": {
-      "usdt": "692,69"
+      "usdt": "661,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -26,9 +26,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "Accidente aéreo en Colombia: hay más de 60 muertos",
-      "link": "https://cnnespanol.cnn.com/2026/03/23/colombia/avion-fuerza-aerea-accidente-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/trumpescalerilla.jpg?c=16x9&q=h_144,w_256,c_fill"
+      "title": "Trump pospone durante cinco días los ataques contra las centrales eléctricas de Irán",
+      "link": "https://cnnespanol.cnn.com/mundo/live-news/guerra-eeuu-israel-iran-ataques-medio-oriente-vivo-243trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2267977139-20260323151433743.jpeg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -61,5 +61,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/16993664-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-03-24T04:53:52.335Z"
+  "lastUpdate": "2026-03-24T06:00:01.490Z"
 };
