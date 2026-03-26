af0e436a55d618bb9dd711e33a49e862e2f07044
GitHub Action <actions@github.com>
2026-03-26 15:06:22 +0000

    data: update news and rates at Thu Mar 26 15:06:22 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "540,17"
     },
     "binance": {
-      "usdt": "707,50"
+      "usdt": "702,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -20,27 +20,33 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "El Nacional",
-      "title": "La disputa sobre quién debe pagar la defensa de Maduro en EE.UU. (y qué ha pasado en otros casos contra expresidentes extranjeros)\nNOTICIAS",
-      "link": "https://www.elnacional.com/2026/03/la-disputa-sobre-quien-debe-pagar-la-defensa-de-maduro-en-ee-uu-y-que-ha-pasado-en-otros-casos-contra-expresidentes-extranjeros/",
+      "title": "Así están los alrededores de la corte de Nueva York por la audiencia de Maduro y Cilia Flores\nVENEZUELA",
+      "link": "https://www.elnacional.com/2026/03/asi-estan-los-alrededores-de-corte-de-nueva-york-por-la-audiencia-de-maduro-y-cilia-flores/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
       "title": "Maduro vuelve a los tribunales en EE.UU.",
       "link": "https://cnnespanol.cnn.com/2026/03/26/eeuu/live-news/ultima-hora-audiencia-nicolas-maduro-venezuela-noticias-reacciones-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2026-03-26t114740z-230840741-rc29ckaftu58-rtrmadp-3-usa-venezuela-maduro-new-york.JPG?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/still-22483658-15942-still.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Venezuela y la Santa Sede fortalecen diálogo sobre paz y reconciliación",
-      "link": "https://noticiasvenevision.com/noticias/politica/venezuela-y-la-santa-sede-fortalecen-dialogo-sobre-paz-y-reconciliacion",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/saveclip.app_656278240_18578131930014700_6125993128024232639_n-1-08fc9f.png"
+      "title": "Relatora de la ONU pide transparencia en nombramientos del Fiscal General y Defensor del Pueblo",
+      "link": "https://noticiasvenevision.com/noticias/politica/relatora-de-la-onu-pide-transparencia-en-nombramientos-del-fiscal-general-y-defensor-del-pueblo",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/097066cb-17dc-4b94-b17b-03a354563adb-045aa5.jpg"
+    },
+    {
+      "source": "Noticiero Digital",
+      "title": "«ULTRADERECHA OFRECE EN SUBASTA EL PETRÓLEO VENEZOLANO», CONDENÓ PSUV",
+      "link": "https://noticierodigital.com/2026/03/ultraderecha-ofrece-en-subasta-el-petroleo-venezolano-condeno-psuv/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/01/psuv-diosdado-cabello-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Revelan nombres y alias de los presuntos responsables del crimen del profesor Neill Felipe Cubides",
-      "link": "https://www.noticiascaracol.com/judicial/crimen-del-profesor-neill-felipe-cubides-revelan-nombres-y-alias-de-cuatro-de-los-presuntos-responsables-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/1c62b35/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F1b%2Fb7%2Fdbb4f4444f48ab31c9568e1d4f55%2Fdiseno-1.jpg"
+      "title": "Ella es Noelia Castillo, joven que recibirá eutanasia tras ser víctima de abuso y quedar parapléjica",
+      "link": "https://www.noticiascaracol.com/mundo/ella-es-noelia-castillo-joven-que-recibira-eutanasia-tras-ser-victima-de-abuso-y-quedar-paraplejica-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/a522c2a/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fcd%2Ff4%2Fd8efd273493d9b79c4d04d4245ac%2Fsenado-61.jpg"
     },
     {
       "source": "La Verdad",
@@ -50,10 +56,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Donald Trump lanza un ultimátum a Irán para negociar “antes de que sea demasiado tarde”",
-      "link": "https://diarioversionfinal.com/mundo/donald-trump-lanza-un-ultimatum-a-iran-para-negociar-antes-de-que-sea-demasiado-tarde/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Trump-20-03-2026.png"
+      "title": "Audiencia de Maduro y Cilia en NY genera protestas de ambos bandos en el Tribunal Federal",
+      "link": "https://diarioversionfinal.com/mundo/audiencia-de-maduro-y-cilia-en-ny-genera-protestas-de-ambos-bandos-en-el-tribunal-federal/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/2ad65b276edd8a497c5cbaca1c09d2b2a84102b5w-1536x1024-1-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-03-26T13:17:18.208Z"
+  "lastUpdate": "2026-03-26T15:04:48.321Z"
 };
