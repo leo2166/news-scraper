730eec7206e6d6663ab51393479a31bd914cdb6e
GitHub Action <actions@github.com>
2026-03-31 15:04:11 +0000

    data: update news and rates at Tue Mar 31 15:04:11 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "542,64"
     },
     "binance": {
-      "usdt": "690,70"
+      "usdt": "688,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,52 +14,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "La ONG Laboratorio de Paz consideró que la presencia de la embajada de EEUU en Venezuela permitirá recuperar la democracia",
-      "link": "https://www.infobae.com/venezuela/2026/03/31/la-ong-laboratorio-de-paz-considero-que-la-presencia-de-la-embajada-de-eeuu-en-venezuela-permitira-recuperar-la-democracia/",
-      "image": "https://www.infobae.com/resizer/v2/IFPCSRVP5ZDJRP2CBSVHJ6ZFDI.PNG?auth=a16156d8df10a2b00d873b1ce2a90bca13acce5ce4912e4af1b602debede6592&smart=true&width=350&height=197&quality=85"
+      "title": "La embajada de EEUU en Venezuela dijo que buscará vincular a empresarios de ambos países",
+      "link": "https://www.infobae.com/venezuela/2026/03/31/la-embajada-de-eeuu-en-venezuela-dijo-que-buscara-vincular-a-empresarios-de-ambos-paises/",
+      "image": "https://www.infobae.com/resizer/v2/YXXJLWNS6NB3VFIHLANJOY3OL4.jpg?auth=0fd9a0cfd0bf75ee50c8d8045eccded48186d8f69806bd199236c6f332746c23&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Marco Rubio se reunirá en privado con María Corina Machado este martes\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/03/marco-rubio-se-reunira-en-privado-con-maria-corina-machado-este-martes/",
+      "title": "EE UU ubica a Venezuela dentro de la Gran Norteamérica, su mapa estratégico de seguridad\nVENEZUELA",
+      "link": "https://www.elnacional.com/2026/03/ee-uu-ubica-a-venezuela-dentro-de-la-gran-norteamerica-su-mapa-estrategico-de-seguridad/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
       "source": "CNN en Español",
-      "title": "La gasolina alcanza los US$ 4 por galón en EE.UU. por primera vez desde 2022",
+      "title": "Hegseth dice que el \"esfuerzo primordial\" de EE.UU. es para lograr un acuerdo con Irán",
       "link": "https://cnnespanol.cnn.com/2026/03/31/mundo/live-news/guerra-ee-uu-israel-iran-ataques-medio-oriente-vivo-9-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/20260331-precios-gasolina-eeuu-hp.png?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/hegseth-345.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Alcalde de Baruta celebra retorno de la Embajada de EE. UU.",
-      "link": "https://noticiasvenevision.com/noticias/politica/alcalde-de-baruta-celebra-retorno-de-la-embajada-de-ee-uu",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/alcalde-de-baruta-darwin-gonzalez-la-alameda-354038-a77c38.jpg"
+      "title": "Plataforma Unitaria considera clave la presencia de EE.UU. para la transición en Venezuela",
+      "link": "https://noticiasvenevision.com/noticias/politica/plataforma-unitaria-considera-clave-la-presencia-de-eeuu-para-la-transicion-en-venezuela",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/ipzgx3tmwvft7dshbfxesirm4u-88a42d.avif"
     },
     {
       "source": "Noticiero Digital",
-      "title": "MINISTRO DE DEFENSA SE REUNIÓ CON COMANDANTES DE LA FANB POR LINEAMIENTOS DE SEMANA SANTA",
-      "link": "https://noticierodigital.com/2026/03/ministro-de-defensa-se-reunio-con-comandantes-de-la-fanb-por-lineamientos-de-semana-santa/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/gustavo-gonzalez-lopez-delcy-rodriguez-627x376.jpg"
+      "title": "ASOBANCA: CARTERA DE CRÉDITO REPRESENTA 3 % DEL PIB",
+      "link": "https://noticierodigital.com/2026/03/asobanca-cartera-de-credito-representa-3-del-pib/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2024/09/pedropacheco-unionradio.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Así fue la llamada extorsiva que recibieron los parientes de familia desaparecida en Bogotá",
-      "link": "https://www.noticiascaracol.com/colombia/bogota/asi-fue-la-llamada-extorsiva-que-recibieron-los-parientes-de-familia-desaparecida-en-bogota-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/540b01b/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fe2%2F2c%2F9056f2154ab58213ab55554ceb28%2Fplantilla-fotos-2026-03-30t194751-663.jpg"
+      "title": "Giros en el caso de María Camila Díaz: las transacciones millonarias y la captura que da esperanzas",
+      "link": "https://www.noticiascaracol.com/judicial/giros-en-el-caso-de-maria-camila-diaz-las-transacciones-millonarias-y-la-captura-que-da-esperanzas-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/cff923e/2147483647/strip/true/crop/1276x720+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F5a%2Fd7%2F203d15d9404090cfa4555b2976c2%2Ffotos-discover-2026-03-31t085727-883.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Vance sigue siendo favorito de los republicanos para suceder a Trump: Rubio recorta distancias",
-      "link": "https://laverdad.com/vance-sigue-siendo-favorito-de-los-republicanos-para-suceder-a-trump-rubio-recorta-distancias/",
-      "image": "https://laverdad.com/wp-content/uploads/2025/10/JD-Vance-Marco-Rubio.jpg"
+      "title": "EE. UU. incluye a Venezuela en su nuevo perímetro de seguridad “Gran América del Norte”",
+      "link": "https://laverdad.com/ee-uu-incluye-a-venezuela-en-su-nuevo-perimetro-de-seguridad-gran-america-del-norte/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/03/Pete-Hegseth.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Nieto de Fidel Castro asegura que “la mayoría de los cubanos quieren capitalismo, no comunismo”",
-      "link": "https://diarioversionfinal.com/mundo/nieto-de-fidel-castro-asegura-que-la-mayoria-de-los-cubanos-quieren-capitalismo-no-comunismo/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/hytg-750x389.jpg"
+      "title": "Elliott Abrams califica a Rodríguez Zapatero como una persona \"sin sentido moral\" en el caso venezolano",
+      "link": "https://diarioversionfinal.com/mundo/elliott-abrams-califica-a-rodriguez-zapatero-como-una-persona-sin-sentido-moral-en-el-caso-venezolano/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/10/Elliott-Abrams-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-03-31T13:20:17.731Z"
+  "lastUpdate": "2026-03-31T15:02:42.553Z"
 };
