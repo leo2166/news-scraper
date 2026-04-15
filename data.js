c245997194c57fb9da9a1ce21eddfc6c22665ac3
GitHub Action <actions@github.com>
2026-04-15 14:46:40 +0000

    data: update news and rates at Wed Apr 15 14:46:40 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "564,98"
     },
     "binance": {
-      "usdt": "627,00"
+      "usdt": "625,88"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,14 +14,14 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Citgo, atrapada entre una tregua judicial y un vacío político que amenaza el activo más importante de Venezuela en el exterior",
-      "link": "https://www.infobae.com/venezuela/2026/04/15/citgo-atrapada-entre-una-tregua-judicial-y-un-vacio-politico-que-amenaza-el-activo-mas-importante-de-venezuela-en-el-exterior/",
-      "image": "https://www.infobae.com/resizer/v2/45UJNZSVTVCPZO27VNADZW6RVM.jpg?auth=e62a5ad214c41430347da0d56740009f2ce89ae1cf18d10b65f20cd0898c85f1&smart=true&width=350&height=197&quality=85"
+      "title": "Estados Unidos tendrá un nuevo encargado de negocios en Venezuela: John Barrett reemplazará a Laura Dogu",
+      "link": "https://www.infobae.com/venezuela/2026/04/15/estados-unidos-tendra-un-nuevo-encargado-de-negocios-en-venezuela-john-barrett-reemplazara-a-laura-dogu/",
+      "image": "https://www.infobae.com/resizer/v2/EEHGUAA6SZCI7NL47RKOULADZE.PNG?auth=dfd0379c267b29d09bcb291812a7936986c448196b4d6e7466ed65302ce875bf&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Machado no se reunirá con Pedro Sánchez: \"En ciertos momentos no convienen ciertas reuniones\"\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/04/machado-no-se-reunira-con-pedro-sanchez-en-ciertos-momentos-no-convienen-ciertas-reuniones/",
+      "title": "Laura Dogu finaliza su misión en Venezuela: John Barrett asumirá como encargado de Negocios\nVENEZUELA",
+      "link": "https://www.elnacional.com/2026/04/laura-dogu-anuncia-que-dejara-su-cargo-como-encargada-de-negocios-de-ee-uu-para-venezuela/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -32,34 +32,34 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Comando Sur realiza nuevo ataque contra supuesta narcolancha en el Pacífico",
-      "link": "https://noticiasvenevision.com/noticias/politica/comando-sur-realiza-nuevo-ataque-contra-supuesta-narcolancha-en-el-pacifico",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/ataque-eua-narcolancha-b52bc6.webp"
+      "title": "John Barrett es el nuevo encargado de negocios de Estados Unidos en Venezuela",
+      "link": "https://noticiasvenevision.com/noticias/politica/john-barrett-es-el-nuevo-encargado-de-negocios-de-estados-unidos-en-venezuela",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/hf41mplasaixzdj-cb3b7a.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "CAPOZZOLO RESPALDÓ MESA DE DIÁLOGO PARA CONSTRUIR NUEVOS CONSENSOS ECONÓMICOS",
-      "link": "https://noticierodigital.com/2026/04/capozzolo-respaldo-mesa-de-dialogo-para-construir-nuevos-consensos-economicos/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/07/Capozzolo_presidenteFedecamaras-627x376.jpg"
+      "title": "CALECA: «ES OBLIGATORIO DESIGNAR UN DIRECTORIO DEL BANCO DE VENEZUELA»",
+      "link": "https://noticierodigital.com/2026/04/caleca-es-obligatorio-designar-un-directorio-del-banco-de-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2024/07/caleca-cne-fraude.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Videos de seguridad revelan cómo ocurrió el violento atraco en Oxxo de la localidad de Teusaquillo",
-      "link": "https://www.noticiascaracol.com/colombia/bogota/nuevos-videos-de-camaras-revelan-como-ocurrio-el-violento-atraco-en-oxxo-de-la-localidad-de-teusaquillo-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/db190e8/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F11%2F80%2Fc87ad30d434ca6d75775ceb053ef%2Fplantilla-fotos-53.jpg"
+      "title": "Nuevos detalles sobre robo a guardas de empresa de valores dentro de Oxxo en Teusaquillo, Bogotá",
+      "link": "https://www.noticiascaracol.com/colombia/bogota/nuevos-detalles-sobre-robo-a-guardas-de-empresa-de-valores-dentro-de-oxxo-en-teusaquillo-bogota-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/848d691/2147483647/strip/true/crop/1280x722+0+8/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F7b%2F70%2Fdf7f8a7f47b9880c967cdd730b50%2Foxxo.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Petro señala a Shakira: “Me sacó la chispa cuando dijo ‘las mujeres facturan”",
-      "link": "https://laverdad.com/petro-senala-a-shakira-me-saco-la-chispa-cuando-dijo-las-mujeres-facturan/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/04/image-2026-04-14T154931.942.jpg"
+      "title": "Trump critica de nuevo al Papa: “¿Le pueden decir que Irán ha matado a 42.000 inocentes?”",
+      "link": "https://laverdad.com/trump-critica-de-nuevo-al-papa-le-pueden-decir-que-iran-ha-matado-a-42-000-inocentes/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/04/Donald-Trump-Leon-XIV.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "EEUU celebra que Venezuela vuelva, de la mano Delcy Rodríguez, al Fondo Monetario Internacional, el demonio según Chávez",
-      "link": "https://diarioversionfinal.com/mundo/eeuu-celebra-que-venezuela-vuelva-de-la-mano-delcy-rodriguez-al-fondo-monetario-internacional-el-demonio-segun-chavez/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/04/3sRv84tX-image-750x430.jpg"
+      "title": "Trump vuelve a criticar al papa: \"Le pueden decir que Irán ha matado a 42.000 inocentes\"",
+      "link": "https://diarioversionfinal.com/mundo/trump-vuelve-a-criticar-al-papa-le-pueden-decir-que-iran-ha-matado-a-42-000-inocentes/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Trump-y-el-papa-Leon-XIV-31-03-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-04-15T12:02:44.750Z"
+  "lastUpdate": "2026-04-15T14:45:13.124Z"
 };
