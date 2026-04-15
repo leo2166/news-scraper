3a5fd7a5fcf6319c2380aaab4986ccdce624a115
GitHub Action <actions@github.com>
2026-04-15 16:07:14 +0000

    data: update news and rates at Wed Apr 15 16:07:14 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "564,98"
     },
     "binance": {
-      "usdt": "625,88"
+      "usdt": "652,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,14 +14,14 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Estados Unidos tendrá un nuevo encargado de negocios en Venezuela: John Barrett reemplazará a Laura Dogu",
-      "link": "https://www.infobae.com/venezuela/2026/04/15/estados-unidos-tendra-un-nuevo-encargado-de-negocios-en-venezuela-john-barrett-reemplazara-a-laura-dogu/",
-      "image": "https://www.infobae.com/resizer/v2/EEHGUAA6SZCI7NL47RKOULADZE.PNG?auth=dfd0379c267b29d09bcb291812a7936986c448196b4d6e7466ed65302ce875bf&smart=true&width=350&height=197&quality=85"
+      "title": "Delcy Rodríguez confirmó que mantuvo encuentros con ConocoPhillips y ExxonMobil en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/04/15/delcy-rodriguez-confirmo-que-mantuvo-encuentros-con-conocophillips-y-exxonmobil-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/AZU3VMLEIBEYZINVFDINJG2EL4.JPG?auth=1dfbfcaa13cfeacce6a14f49f0bd5891db386e625b3a062b216aec15ceea410a&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "El Nacional",
-      "title": "Laura Dogu finaliza su misión en Venezuela: John Barrett asumirá como encargado de Negocios\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/04/laura-dogu-anuncia-que-dejara-su-cargo-como-encargada-de-negocios-de-ee-uu-para-venezuela/",
+      "title": "¿Quién es John Barrett, el nuevo diplomático designado por EE UU para Venezuela?\nVENEZUELA",
+      "link": "https://www.elnacional.com/2026/04/john-barrett-el-diplomatico-designado-por-ee-uu-para-venezuela/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -38,28 +38,28 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "CALECA: «ES OBLIGATORIO DESIGNAR UN DIRECTORIO DEL BANCO DE VENEZUELA»",
-      "link": "https://noticierodigital.com/2026/04/caleca-es-obligatorio-designar-un-directorio-del-banco-de-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2024/07/caleca-cne-fraude.jpg"
+      "title": "GONZÁLEZ LOBATO: «UNA DEFENSORÍA TIENE QUE SER CERCANA Y ACCESIBLE PARA EL CIUDADANO»",
+      "link": "https://noticierodigital.com/2026/04/gonzalez-lobato-una-defensoria-tiene-que-ser-cercana-y-accesible-para-el-ciudadano/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/04/1776264684304-627x376.png"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Nuevos detalles sobre robo a guardas de empresa de valores dentro de Oxxo en Teusaquillo, Bogotá",
-      "link": "https://www.noticiascaracol.com/colombia/bogota/nuevos-detalles-sobre-robo-a-guardas-de-empresa-de-valores-dentro-de-oxxo-en-teusaquillo-bogota-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/848d691/2147483647/strip/true/crop/1280x722+0+8/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F7b%2F70%2Fdf7f8a7f47b9880c967cdd730b50%2Foxxo.jpg"
+      "title": "En Bogotá, 12 personas fueron sorprendidas delinquiendo y con brazalete del Inpec: ¿quién controla?",
+      "link": "https://www.noticiascaracol.com/colombia/bogota/en-bogota-12-personas-fueron-sorprendidas-delinquiendo-y-con-brazalete-del-inpec-quien-controla-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/9f3a1e9/2147483647/strip/true/crop/1280x722+0+40/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fb8%2Fae%2F79c3e5994d6a91d90dc329048b6c%2Fbrazalete-inpec.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Trump critica de nuevo al Papa: “¿Le pueden decir que Irán ha matado a 42.000 inocentes?”",
-      "link": "https://laverdad.com/trump-critica-de-nuevo-al-papa-le-pueden-decir-que-iran-ha-matado-a-42-000-inocentes/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/04/Donald-Trump-Leon-XIV.jpg"
+      "title": "León XIV destaca que se puede vivir juntos en paz a pesar de diferencias tras visita a Argelia",
+      "link": "https://laverdad.com/leon-xiv-destaca-que-se-puede-vivir-juntos-en-paz-a-pesar-de-diferencias-tras-visita-a-argelia/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/04/Leon-XIV-Camerun.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Trump vuelve a criticar al papa: \"Le pueden decir que Irán ha matado a 42.000 inocentes\"",
-      "link": "https://diarioversionfinal.com/mundo/trump-vuelve-a-criticar-al-papa-le-pueden-decir-que-iran-ha-matado-a-42-000-inocentes/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Trump-y-el-papa-Leon-XIV-31-03-2026-750x430.jpg"
+      "title": "Trump asegura que está reabriendo el Estrecho de Ormuz “para siempre”",
+      "link": "https://diarioversionfinal.com/mundo/trump-asegura-que-esta-reabriendo-el-estrecho-de-ormuz-para-siempre/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Iran-EstrechoOrmuz-Mapa-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-04-15T14:45:13.124Z"
+  "lastUpdate": "2026-04-15T16:05:44.171Z"
 };
