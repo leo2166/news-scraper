862d98bdcd85da0d7801f844360d4ba98086afb1
GitHub Action <actions@github.com>
2026-04-18 18:54:25 +0000

    data: update news and rates at Sat Apr 18 18:54:25 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "568,52"
     },
     "binance": {
-      "usdt": "650,61"
+      "usdt": "653,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -20,8 +20,8 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "El Nacional",
-      "title": "Regreso de Venezuela a la comunidad financiera internacional afianza la fase de estabilización trazada por EE UU\nVENEZUELA",
-      "link": "https://www.elnacional.com/2026/04/regreso-de-venezuela-a-la-comunidad-financiera-internacional-afianza-la-fase-de-estabilizacion-trazada-por-ee-uu/",
+      "title": "María Corina Machado recibe la Medalla de Oro de Madrid: \"Desde aquí comenzamos a preparar esta nueva fase\"\nMUNDO",
+      "link": "https://www.elnacional.com/2026/04/maria-corina-machado-recibe-la-medalla-de-oro-de-madrid-desde-aqui-comenzamos-a-preparar-esta-nueva-fase/",
       "image": "https://elnacional-uploads.nyc3.digitaloceanspaces.com/wp-content/uploads/2025/05/12084433/elnacional-site-image-1200x675-1-1.png"
     },
     {
@@ -38,13 +38,13 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "CONDENADAS A 25 AÑOS POR TRÁFICO DE DROGAS EXALCALDESA Y EXDIPUTADA DEL PSUV",
-      "link": "https://noticierodigital.com/2026/04/condenadas-a-25-anos-por-trafico-de-drogas-exalcaldesa-y-exdiputada-del-psuv/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/04/WhatsApp-Image-2026-04-18-at-12.56.04-PM-627x376.jpeg"
+      "title": "TRUMP CRITICA «LO MAL QUE LE VA A ESPAÑA» EN SU ECONOMÍA",
+      "link": "https://noticierodigital.com/2026/04/trump-critica-lo-mal-que-le-va-a-espana-en-su-economia/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/donald-trump-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "\"Le quitaron sus sueños\": el dolor de la madre del estudiante de Uniminuto asesinado en TransMilenio",
+      "title": "\"Solo quiero justicia\": el dolor de la madre del estudiante de Uniminuto asesinado en TransMilenio",
       "link": "https://www.noticiascaracol.com/colombia/bogota/le-quitaron-sus-suenos-el-dolor-de-la-madre-del-estudiante-de-uniminuto-asesinado-en-transmilenio-rg10",
       "image": "https://caracoltv.brightspotcdn.com/dims4/default/484d271/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F1f%2F20%2Ffe6c1460428ab09ad9f36333e606%2Fasesinato-de-fredy-santiago-guzman-en-transmilenio-familia-denuncia-falta-de-seguridad.jpg"
     },
@@ -56,10 +56,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "MCM se reúne con la comunidad venezolana en España: “Hoy comienza el regreso a casa”",
-      "link": "https://diarioversionfinal.com/mundo/mcm-se-reune-con-la-comunidad-venezolana-en-espana-hoy-comienza-el-regreso-a-casa/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/04/WhatsApp-Image-2026-04-18-at-10.24.04-AM-750x430.jpeg"
+      "title": "EE.UU. enjuiciará a narco colombiano, protector de Alex Saab, que se entregó en Maracaibo",
+      "link": "https://diarioversionfinal.com/mundo/ee-uu-enjuiciara-a-narco-colombiano-protector-de-alex-saab-que-se-entrego-en-maracaibo/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/04/Pollo-Caravajal-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-04-18T17:36:53.568Z"
+  "lastUpdate": "2026-04-18T18:52:56.411Z"
 };
