45439f9c73eb7b2f2ea0babae5a1ca9ba2c970ca
GitHub Action <actions@github.com>
2026-04-30 19:23:51 +0000

    data: update news and rates at Thu Apr 30 19:23:51 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "569,76"
     },
     "binance": {
-      "usdt": "662,00"
+      "usdt": "662,20"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,46 +14,46 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "El Departamento de Transporte de EEUU celebró el restablecimiento de los vuelos comerciales con Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/04/30/el-departamento-de-transporte-de-eeuu-celebro-el-restablecimiento-de-los-vuelos-comerciales-con-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/GZJ73AXKC5FV7MJDH5X7R66LLE.JPG?auth=7e97614a18c7401142e5902db8dae1e72ad1c05d35bdecb8900aec581e4d5830&smart=true&width=350&height=197&quality=85"
+      "title": "Presos políticos en Venezuela: el Foro Penal reportó 454 detenidos, en medio de reclamos internacionales y familiares",
+      "link": "https://www.infobae.com/venezuela/2026/04/30/presos-politicos-en-venezuela-el-foro-penal-reporto-454-detenidos-en-medio-de-reclamos-internacionales-y-familiares/",
+      "image": "https://www.infobae.com/resizer/v2/SGBTUFQOIZB3VLZDYEX5PUFIPY.jpg?auth=f1b8c68a2498f5039b29f9f263bf299234adc20053e49aac5b0f75224a6cefc6&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
       "title": "Tras la acusación de EE.UU. contra el gobernador de Sinaloa, Sheinbaum exigió pruebas",
       "link": "https://cnnespanol.cnn.com/2026/04/30/mexico/sheinbaum-acusacion-gobernador-sinaloa-eeuu-pruebas-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2273242958.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/mercados-20260430162643379.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Director del Consejo Nacional de Dominio Energético de EE.UU. visita Venezuela este jueves",
-      "link": "https://noticiasvenevision.com/noticias/politica/director-del-consejo-nacional-de-dominio-energetico-de-eeuu-visita-venezuela-este-jueves",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/jarrod_agen_usg00ga-50ce41.jpg"
+      "title": "Parlamento Europeo espera que Venezuela tenga avances significativos para lograr una transición democrática",
+      "link": "https://noticiasvenevision.com/noticias/politica/parlamento-europeo-espera-que-venezuela-tenga-avances-significativos-para-lograr-una-transicion-democratica",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/ursula-von-der-leyen-en-el-parlamento-europeo-1-800x500-acea85.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "RAFAEL UZCÁTEGUI: «PRESIDENCIALES EN 2030 NO ES TRANSICIÓN»",
-      "link": "https://noticierodigital.com/2026/04/rafael-uzcategui-presidenciales-en-2030-no-es-transicion/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2020/09/rafael_uzcategui_provea.jpg"
+      "title": "REPSOL PLANEA AUMENTAR PRODUCCIÓN DE QUEROSENO Y RECIBIRÁ UN CARGAMENTO DE CRUDO DE VENEZUELA",
+      "link": "https://noticierodigital.com/2026/04/repsol-planea-aumentar-produccion-de-queroseno-y-recibira-un-cargamento-de-crudo-de-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/03/repsol-sede-5jun2022-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Ellos son los tres colombianos detenidos por Israel en Flotilla Global Sumud, que se dirigía a Gaza",
-      "link": "https://www.noticiascaracol.com/mundo/ellos-son-los-tres-colombianos-detenidos-por-israel-en-flotilla-global-sumud-que-se-dirigia-a-gaza-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/a756bfa/2147483647/strip/true/crop/1276x720+4+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fdd%2F47%2F1d550cc9416ab4613d31ed08e3e2%2Fexcentricidades-44.jpg"
+      "title": "Tribunal confirma medida de aseguramiento contra el exministro Velasco por escándalo de la UNGRD",
+      "link": "https://www.noticiascaracol.com/judicial/tribunal-confirma-medida-de-aseguramiento-contra-el-exministro-velasco-por-escandalo-ungrd-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/a8b6e00/2147483647/strip/true/crop/1915x1080+5+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F98%2F3c%2F692e75ba4d5ea6f16337b761e405%2Fmujer-bus-24.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Despega de Miami el primer vuelo comercial directo entre EE. UU. y Venezuela en siete años",
-      "link": "https://laverdad.com/despega-de-miami-el-primer-vuelo-comercial-directo-entre-ee-uu-y-venezuela-en-siete-anos/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/04/American-Airlines-Miami-Caracas.png"
+      "title": "Gobierno de Trump celebra reinicio de vuelos comerciales entre EE. UU. y Venezuela",
+      "link": "https://laverdad.com/gobierno-de-trump-celebra-reinicio-de-vuelos-comerciales-entre-ee-uu-y-venezuela/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/04/American-Airlines-Miami-Caracas-1.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Gobierno de Trump estima en 25.000 millones el costo de su operación militar en Irán",
-      "link": "https://diarioversionfinal.com/mundo/gobierno-de-trump-estima-en-25-000-millones-el-costo-de-su-operacion-militar-en-iran/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/04/yymmdd-topic-horizontal-rename-this-00-00-31-29-still001-750x430.jpg"
+      "title": "Operativo en Perú deja 25 arrestos ligados al Tren de Aragua",
+      "link": "https://diarioversionfinal.com/mundo/operativo-en-peru-deja-25-arrestos-ligados-al-tren-de-aragua/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/04/peru-tren-aragua-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-04-30T17:21:53.378Z"
+  "lastUpdate": "2026-04-30T19:22:14.726Z"
 };
