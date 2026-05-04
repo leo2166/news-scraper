4f0c09756cb01d7baf6c62caadac15fda70cdbf4
GitHub Action <actions@github.com>
2026-05-04 18:16:38 +0000

    data: update news and rates at Mon May  4 18:16:38 UTC 2026

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ window.DASHBOARD_DATA = {
       "eur": "574,19"
     },
     "binance": {
-      "usdt": "677,00"
+      "usdt": "677,83"
     },
     "kontigo": {
       "usd": "N/A"
@@ -14,13 +14,13 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Nuevo momento, viejo bloqueo: Delcy Rodríguez mantiene la censura contra los medios digitales en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/05/04/nuevo-momento-viejo-bloqueo-delcy-rodriguez-mantiene-la-censura-contra-los-medios-digitales-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/NH45CEHUUNBGNHT6GDGOHCOUTE.jpg?auth=03d8f88e4d915fcc93d95d209d8ab1f5167aa0873776470f9d6a8bdb2e22b983&smart=true&width=350&height=197&quality=85"
+      "title": "Venezuela enfrenta el riesgo de perder el Esequibo en la Corte Internacional de Justicia",
+      "link": "https://www.infobae.com/venezuela/2026/05/04/venezuela-enfrenta-el-riesgo-de-perder-el-esequibo-en-la-corte-internacional-de-justicia/",
+      "image": "https://www.infobae.com/resizer/v2/XCCNBY755NCPBHNG6YFFQT2A2Q.jpg?auth=d6d0697ca9d3dc065b67c3cd29fea065732a341e947c9d93ad8e5cad249c7369&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "Reportan que dos buques con bandera de EE.UU. atravesaron el estrecho de Ormuz",
+      "title": "Estados Unidos e Irán intercambiaron disparos en el estrecho de Ormuz",
       "link": "https://cnnespanol.cnn.com/2026/05/04/mundo/live-news/guerra-ee-uu-israel-iran-ataques-4-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/ilustracion-vladimir-putin-cnn.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
@@ -31,10 +31,16 @@ window.DASHBOARD_DATA = {
       "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/eur-erco-peace-palace-international-court-of-justice-of-the-united-nations-the-hague-image-1-2-da77be.jpg"
     },
     {
+      "source": "Noticiero Digital",
+      "title": "SEPA CUÁNTO CUESTA LA ENTRADA A LA EXPO BELLEZA Y BIENESTAR EN EL CCCT",
+      "link": "https://noticierodigital.com/2026/05/sepa-cuanto-cuesta-la-entrada-a-la-expo-belleza-y-bienestar-en-el-ccct/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/Diseno-sin-titulo-6-627x376.png"
+    },
+    {
       "source": "Caracol Noticias",
-      "title": "Atraco en Medellín terminó en tiroteo y la muerte de un joven de 17 años: autoridades dan detalles",
-      "link": "https://www.noticiascaracol.com/colombia/atraco-en-medellin-termino-en-tiroteo-y-la-muerte-de-un-joven-de-17-anos-autoridades-dan-detalles-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/a339791/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F8b%2Ff7%2F29642a7b4e06a567543ece229f1d%2Fyondo.jpg"
+      "title": "Video | Avión se estrelló contra edificio en Brasil y reportan dos muertos: esto se sabe",
+      "link": "https://www.noticiascaracol.com/mundo/video-avion-se-estrello-contra-edificio-en-brasil-y-reportan-dos-muertos-esto-se-sabe-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/fe338c9/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F6a%2F32%2F3d9f42b848cb9598a216771d462f%2Fbre-b-23.jpg"
     },
     {
       "source": "La Verdad",
@@ -44,10 +50,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "“Proyecto Libertad” de Trump escolta dos primeros barcos y desmiente ataque a una de sus embarcaciones",
-      "link": "https://diarioversionfinal.com/mundo/proyecto-libertad-de-trump-escolta-dos-primeros-barcos-y-desmiente-ataque-a-una-de-sus-embarcaciones/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/Centcom-04-05-2026-750x430.png"
+      "title": "Directiva de la CEV regala al Papa León XIV la camiseta de los campeones del Clásico Mundial",
+      "link": "https://diarioversionfinal.com/mundo/directiva-de-la-cev-regala-al-papa-leon-xiv-la-camiseta-de-los-campeones-del-clasico-mundial/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/WhatsApp-Image-2026-05-04-at-1.00.19-PM-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-05-04T16:45:56.810Z"
+  "lastUpdate": "2026-05-04T18:15:01.677Z"
 };
