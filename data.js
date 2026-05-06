5d46af724828f19b5509c3dba715875cf13f2385
leo2166 <lucidio2010@gmail.com>
2026-05-06 18:34:27 -0400

    Actualizar tasas para manana jueves

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "493,38",
-      "eur": "577,52",
-      "fechaValor": "Miércoles, 06 Mayo 2026"
+      "usd": "496,83",
+      "eur": "583,87",
+      "fechaValor": "Jueves, 07 Mayo 2026"
     },
     "binance": {
-      "usdt": "N/A"
+      "usdt": "686,58"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,40 +15,46 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "La ONG Transparencia Venezuela acusa al gobierno de Delcy Rodríguez de ocultar información clave sobre fondos públicos",
-      "link": "https://www.infobae.com/venezuela/2026/05/05/la-ong-transparencia-venezuela-acusa-al-gobierno-de-delcy-rodriguez-de-ocultar-informacion-clave-sobre-fondos-publicos/",
-      "image": "https://www.infobae.com/resizer/v2/4XKXXQ2BWZH7DC7LOWJYJSEPAA.JPG?auth=5a2daa1e8b8ab16569e73c61e73683aae7142e667b8ed53d8beb8bc5345360e0&smart=true&width=350&height=197&quality=85"
+      "title": "El hotel en Caracas que se convirtió en centro de poder tras la caída de Maduro",
+      "link": "https://www.infobae.com/venezuela/2026/05/06/el-hotel-en-caracas-que-se-convirtio-en-centro-de-poder-tras-la-caida-de-maduro-donde-se-reunen-diplomaticos-de-estados-unidos/",
+      "image": "https://www.infobae.com/resizer/v2/HYG2AZ4WUJB6ZEJMR6LOUXYFSM.PNG?auth=0a2d7522a6e88bea1bcf2366408786733436e2f8984535caa1896bd37dd74ff1&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "¿Por qué EE.UU. permitió que Venezuela pague la defensa de Maduro?",
-      "link": "https://cnnespanol.cnn.com/2026/05/05/venezuela/defensa-maduro-victoria-complica-poder-caracas-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2026-01-05t124551z-1320417056-rc20via4lz84-rtrmadp-3-usa-venezuela-maduro-new-york-20260423224613291.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Muere Ted Turner, fundador de CNN, a los 87 años",
+      "link": "https://cnnespanol.cnn.com/2026/05/06/eeuu/muere-ted-turner-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ted-turner-2011-gettyimages-112544025.jpg?c=2x3&q=h_384,w_256,c_fill"
+    },
+    {
+      "source": "Noticias Venevisión",
+      "title": "Pedro Infante anuncia avances en la conformación del Comité de Postulaciones",
+      "link": "https://noticiasvenevision.com/noticias/politica/pedro-infante-anuncia-avances-en-la-conformacion-del-comite-de-postulaciones",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/diputado-pedro-infante-anuncia-nacimiento-de-la-liga-parlamentaria-mundial-antifacista-20241107210405-002db6.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "JUAN PABLO GUANIPA DESDE MARACAIBO: «MARÍA CORINA VA A VENIR PRONTO A VENEZUELA»",
-      "link": "https://noticierodigital.com/2026/05/juan-pablo-guanipa-desde-maracaibo-maria-corina-va-a-venir-pronto-a-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/juan-pablo-guanipa-627x376.jpg"
+      "title": "PROVEA EXIGIÓ INDEPENDENCIA JUDICIAL EN EVENTUAL REFORMA DEL TSJ",
+      "link": "https://noticierodigital.com/2026/05/provea-exigio-independencia-judicial-en-eventual-reforma-del-tsj/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/04/marino-alvarado-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Se conoce qué país recibirá el crucero varado por brote de hantavirus en Cabo Verde",
-      "link": "https://www.noticiascaracol.com/mundo/se-conoce-que-pais-recibira-el-crucero-varado-por-brote-de-hantavirus-en-cabo-verde-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/ce91d1c/2147483647/strip/true/crop/1280x722+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F80%2Feb%2Fae8c5c664c13b038190a08a78979%2Fhantavirus.jpg"
+      "title": "Esta es la rara variante del hantavirus: es la única confirmada que se transmite entre humanos",
+      "link": "https://www.noticiascaracol.com/salud/esta-es-la-rara-del-hantavirus-la-unica-confirmada-que-se-transmite-entre-humanos-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/d356646/2147483647/strip/true/crop/1276x720+2+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2F76%2F5f%2Fec6aefa645d88a8471148b4787c9%2Fplantilla-fotos-2026-05-06t170814-178.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Los tres infectados por hantavirus en el crucero serán evacuados a Países Bajos",
-      "link": "https://laverdad.com/los-tres-infectados-por-hantavirus-en-el-crucero-seran-evacuados-a-paises-bajos/",
-      "image": "https://laverdad.com/pagespeed_static/1.JiBnMqyl6S.gif"
+      "title": "Trump a León XIV: “Le guste o no, Irán no puede tener un arma nuclear”",
+      "link": "https://laverdad.com/trump-a-leon-xiv-le-guste-o-no-iran-no-puede-tener-un-arma-nuclear/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/05/Donald-Trump.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Director de Chevron advierte escasez de petróleo a nivel mundial por cierre de Ormuz",
-      "link": "https://diarioversionfinal.com/mundo/director-de-chevron-advierte-escasez-de-petroleo-a-nivel-mundial-por-cierre-de-ormuz/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/01/tsize_600x400_Petroleo-e1738703305946.jpg"
+      "title": "EE.UU. inmoviliza petrolero iraní en el golfo de Omán en medio del bloqueo naval",
+      "link": "https://diarioversionfinal.com/mundo/ee-uu-inmoviliza-petrolero-irani-en-el-golfo-de-oman-en-medio-del-bloqueo-naval/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/AA21Seez.jpg"
     }
   ],
-  "lastUpdate": "2026-05-05T23:14:28.062Z"
+  "lastUpdate": "2026-05-06T22:31:46.083Z"
 };
