34a91fbe9cbb73f664264b5a4fdd5889eddab348
GitHub Action <actions@github.com>
2026-05-13 03:30:33 +0000

    data: update news and rates at Wed May 13 03:30:33 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Miércoles, 13 Mayo 2026"
     },
     "binance": {
-      "usdt": "702,00"
+      "usdt": "670,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,33 +15,33 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Infobae",
-      "title": "Científicos venezolanos consideran un “duro golpe” a la investigación el retiro del uranio enriquecido",
-      "link": "https://www.infobae.com/venezuela/2026/05/12/cientificos-venezolanos-consideran-un-duro-golpe-a-la-investigacion-el-retiro-del-uranio-enriquecido/",
-      "image": "https://www.infobae.com/resizer/v2/7437FL7HLZB4TKKXYTDASYFR2I.jpg?auth=80616b5a19057ebe4b1ad057eb7f45dcae0027787e85fc260e914bc842b5df50&smart=true&width=350&height=197&quality=85"
+      "title": "El Parlamento de Venezuela aprobó una reforma que amplía a 32 el número de magistrados del Tribunal Supremo de Justicia",
+      "link": "https://www.infobae.com/venezuela/2026/05/13/el-parlamento-de-venezuela-aprobo-una-reforma-que-amplia-a-32-el-numero-de-magistrados-del-tribunal-supremo-de-justicia/",
+      "image": "https://www.infobae.com/resizer/v2/TLKN2G6HNNFE3NTAMV3B6WPNC4.jpg?auth=1cca0d62705411d9e685fd23e5603dac22e720fac0a44523bb4a692a18eb8e40&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
       "title": "La CIA intensifica guerra secreta contra los carteles en México",
       "link": "https://cnnespanol.cnn.com/2026/05/12/eeuu/cia-operaciones-carteles-narcotrafico-mexico-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/cnne-1764293-president-bush-tours-cia-headquarters.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/cia-mexico-tease-horizontal-20260512235305594.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Donald Trump garantiza la liberación de todos los presos políticos en el país",
-      "link": "https://noticiasvenevision.com/noticias/politica/donald-trump-garantiza-la-liberacion-de-todos-los-presos-politicos-en-el-pais",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_5772927025470443315_y-387f4f.jpg"
+      "title": "Asamblea Nacional instala Grupo Parlamentario de Amistad Venezuela–Estados Unidos",
+      "link": "https://noticiasvenevision.com/noticias/politica/asamblea-nacional-instala-grupo-parlamentario-de-amistad-venezuelaestados-unidos",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-05-12-at-6.06.50-pm-1-c7639a.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "DOUGLAS RICO REVELÓ CÓMO CAPTURARON EN VENEZUELA A MEXICANA ACUSADA DE FEMICIDIO",
-      "link": "https://noticierodigital.com/2026/05/douglas-rico-revelo-como-capturaron-en-venezuela-a-mexicana-acusada-de-femicidio/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/erika-herrera-asesinato-carolina-flores-627x376.jpg"
+      "title": "Gobierno ratificó meta de sembrar 10 millones de árboles",
+      "link": "https://noticierodigital.com/2026/05/gobierno-ratifico-meta-de-sembrar-10-millones-de-arboles/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/freddy-nanez-venezuela-627x376.jpg"
     },
     {
       "source": "Caracol Noticias",
-      "title": "Detalles del tiroteo en Soacha: video muestra a ladrones disfrazados como empleados de gasolinera",
-      "link": "https://www.noticiascaracol.com/colombia/detalles-del-tiroteo-en-soacha-video-muestra-a-ladrones-disfrazados-como-empleados-de-gasolinera-rg10",
-      "image": "https://caracoltv.brightspotcdn.com/dims4/default/0eb9988/2147483647/strip/true/crop/1280x722+0+1/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fd0%2Fb5%2F8f06c9d24de084549d385da6c7d9%2Fbalacera-soacha.jpg"
+      "title": "Torturada, abusada y obligada a abortar: mamá y padrastro habrían sometido a niña en Ibagué 10 años",
+      "link": "https://www.noticiascaracol.com/colombia/torturada-abusada-y-obligada-a-abortar-mama-y-padrastro-habrian-sometido-a-nina-en-ibague-10-anos-rg10",
+      "image": "https://caracoltv.brightspotcdn.com/dims4/default/f0495f1/2147483647/strip/true/crop/1280x722+0+0/resize/390x220!/quality/75/?url=https%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fce%2F92%2F8e413424461190ae98e895dda344%2Fabuso-ibague.jpg"
     },
     {
       "source": "La Verdad",
@@ -51,10 +51,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "EE. UU. presenta cargos contra más de 25 presuntos miembros del Tren de Aragua",
-      "link": "https://diarioversionfinal.com/mundo/ee-uu-presenta-cargos-contra-mas-de-25-presuntos-miembros-del-tren-de-aragua/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/IMG_4161.webp"
+      "title": "Trump publica imagen del mapa de Venezuela pintado con bandera de EE. UU. como el “estado 51”",
+      "link": "https://diarioversionfinal.com/mundo/trump-publica-imagen-del-mapa-de-venezuela-pintado-con-bandera-de-ee-uu-como-el-estado-51/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/Estado-51-12-05-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-05-12T23:02:15.176Z"
+  "lastUpdate": "2026-05-13T03:28:56.647Z"
 };
