6d94005d6cce6d3296cc4bff215dc144fdd80ecb
GitHub Action <actions@github.com>
2026-05-19 23:03:37 +0000

    data: update news and rates at Tue May 19 23:03:37 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Miércoles, 20 Mayo 2026"
     },
     "binance": {
-      "usdt": "743,57"
+      "usdt": "734,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,27 +21,27 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "La canasta alimentaria de las familias venezolanas aumentó un 5,5% en abril",
-      "link": "https://www.infobae.com/venezuela/2026/05/19/la-canasta-alimentaria-de-una-familia-venezolana-aumento-un-55-en-abril/",
-      "image": "https://www.infobae.com/resizer/v2/GMVPTJ7UGLNFFX5BCNL75DLSP4.jpg?auth=8928b0f10bf90fea0de76c1095e8790c87e373f6cef40db82c0b33a0e804f144&smart=true&width=350&height=197&quality=85"
+      "title": "Reclamaron justicia en el velatorio de Carmen Navas, madre de un preso político fallecido bajo custodia del Estado en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/05/19/reclamaron-justicia-en-el-velatorio-de-carmen-navas-madre-de-un-preso-politico-fallecido-bajo-custodia-del-estado-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/GVU4SYVH6RHWNAR6IVM3TJWZSA.JPG?auth=bb822eadd6e1e6e64a796dcb28bac5d42c85f7239ebee9977a77978e633f21fa&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump dice que pospondrá el ataque a Irán \"quizás hasta la próxima semana\"",
-      "link": "https://cnnespanol.cnn.com/2026/05/19/mundo/trump-limite-ataques-iran-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2026-05-09t192706z-1821657245-rc2u5lami6gi-rtrmadp-3-ww2-anniversary-putin-20260519053548856.jpg?c=16x9&q=h_144,w_256,c_fill"
+      "title": "EE.UU. prepara anuncio en medio de esfuerzos para imputar a Raúl Castro",
+      "link": "https://cnnespanol.cnn.com/2026/05/19/eeuu/departamento-justicia-anuncio-presiones-cuba-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2269679630a-20260409234939789.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Merys Torres de Sequea, de 71 años, es excarcelada tras ocho meses de reclusión en el INOF",
-      "link": "https://noticiasvenevision.com/noticias/politica/merys-torres-de-sequea-de-71-anos-es-excarcelada-tras-ocho-meses-de-reclusion-en-el-inof",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/descarga-1-fb3f8f.jpg"
+      "title": "Presidente de la Asamblea Nacional anuncia que 300 personas serán liberadas esta semana",
+      "link": "https://noticiasvenevision.com/noticias/politica/presidente-de-la-asamblea-nacional-anuncia-que-300-personas-seran-liberadas-esta-semana",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/diputado-jorge-rodriguez-senala-relacion-de-alex-saab-con-agencias-estadounidenses-20260519212826-07c952.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "JORGE RODRÍGUEZ ASEGURÓ QUE ALEX SAAB TENÍA RELACIÓN CON AGENCIAS DE EE. UU.",
-      "link": "https://noticierodigital.com/2026/05/jorge-rodriguez-aseguro-que-alex-saab-tenia-relacion-con-agencias-de-ee-uu/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/jorge-rodriguez-an-1-627x376.jpg"
+      "title": "PUD SE REUNIRÁ CON MARÍA CORINA MACHADO EN PANAMÁ",
+      "link": "https://noticierodigital.com/2026/05/pud-se-reunira-con-maria-corina-machado-en-panama/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/02/plataforma-unitaria-p-1-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -56,5 +56,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/WhatsApp-Image-2026-05-19-at-4.02.54-PM-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-05-19T21:38:10.279Z"
+  "lastUpdate": "2026-05-19T23:01:48.007Z"
 };
