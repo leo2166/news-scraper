84ffe50120ff8db3f0153f41900fc4d81d988967
GitHub Action <actions@github.com>
2026-05-27 16:39:35 +0000

    data: update news and rates at Wed May 27 16:39:35 UTC 2026

--- data.js
+++ data.js
@@ -15,46 +15,46 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Fedeunep advierte exclusión de sindicatos en propuesta de reforma de la Ley del Trabajo",
-      "link": "https://www.bancaynegocios.com/fedeunep-advierte-exclusion-de-sindicatos-en-propuesta-de-reforma-de-la-ley-del-trabajo/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/05/mintrabajo2-e1779840339628.jpg"
+      "title": "#Análisis: ¿Cuánto se requiere invertir para estabilizar el sistema hídrico de Venezuela?",
+      "link": "https://www.bancaynegocios.com/analisis-cuanto-se-requiere-invertir-para-estabilizar-el-sistema-hidrico-de-venezuela/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2023/06/grifo-agua-1-e1696026289992.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Venezuela liberó a ocho militares condenados por “instigación” contra el régimen de Nicolás Maduro",
-      "link": "https://www.infobae.com/venezuela/2026/05/27/venezuela-libero-a-ocho-militares-condenados-por-instigacion-contra-el-regimen-de-nicolas-maduro/",
-      "image": "https://www.infobae.com/resizer/v2/WLNTEMULWBFPFBZK4SK7CLF2T4.jpg?auth=d345ed967fc621376123b5e086898da691c697c93149fc1615209ca327cca966&smart=true&width=350&height=197&quality=85"
+      "title": "La activista Rocío San Miguel regresó a Venezuela tras permanecer más de cinco meses en España",
+      "link": "https://www.infobae.com/venezuela/2026/05/27/la-activista-rocio-san-miguel-regreso-a-venezuela-tras-permanecer-mas-de-cinco-meses-en-espana/",
+      "image": "https://www.infobae.com/resizer/v2/6SNPKLM4HFAC5OOZKQ4T2XYSJI.jpg?auth=40883c1d506fc3eefe1639a0f0646b47de1b4413efae75a7d809f7ec06061d5e&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "Por qué poner fin a la guerra con Irán podría ser muy difícil para Trump",
-      "link": "https://cnnespanol.cnn.com/2026/05/27/mundo/analisis-trump-buena-salida-guerra-iran-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2276616960-20260526233617274-20260527042831580.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "La Casa Blanca dice que el borrador difundido por medios iraníes es una “invención”",
+      "link": "https://cnnespanol.cnn.com/2026/05/27/mundo/live-news/guerra-iran-ee-uu-gabinete-represalias-vivo-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/8940082-20260527103256922.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "TSJ coordinó acciones para la Consulta de Reforma Judicial",
-      "link": "https://noticiasvenevision.com/noticias/politica/tsj-coordino-acciones-para-la-consulta-de-reforma-judicial",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_4949746901960035376_x-dd9a8a.jpg"
+      "title": "Juan Carlos Apitz: \"La reforma de la Ley del TSJ no garantiza independencia ni autonomía\"",
+      "link": "https://noticiasvenevision.com/noticias/politica/juan-carlos-apitz-la-reforma-de-la-ley-del-tsj-no-garantiza-independencia-ni-autonomia",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/az9_25721-36e4c7.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "JORGE ARREAZA PIDIÓ RESPALDAR AL GOBIERNO DE DELCY RODRÍGUEZ",
-      "link": "https://noticierodigital.com/2026/05/jorge-arreaza-pidio-respaldar-al-gobierno-de-delcy-rodriguez/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/jorge-arreaza-627x376.jpg"
+      "title": "ICE DETUVO AL MÚSICO ZULIANO GIANLORETT MATOS EN TEXAS",
+      "link": "https://noticierodigital.com/2026/05/ice-detuvo-al-musico-zuliano-gianlorett-matos-en-texas/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/ICEmusicozuliano-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "ICE pone en la mira a abogados de inmigración que presenten solicitudes falsas de asilo",
-      "link": "https://laverdad.com/ice-pone-en-la-mira-a-abogados-de-inmigracion-que-presenten-solicitudes-falsas-de-asilo/",
-      "image": "https://laverdad.com/wp-content/uploads/2025/04/Asilo-EE.-UU.jpg"
+      "title": "Al menos 12 soldados colombianos resultan heridos en ataque del ELN a 4 días de las elecciones",
+      "link": "https://laverdad.com/al-menos-12-soldados-colombianos-resultan-heridos-en-ataque-del-eln-a-4-dias-de-las-elecciones/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/05/imagen_2026-05-27_104247031.png"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Comparece ante la fiscalía de México gobernador de Sinaloa pedido en extradición por EE. UU.",
-      "link": "https://diarioversionfinal.com/mundo/comparece-ante-la-fiscalia-de-mexico-gobernador-de-sinaloa-pedido-en-extradicion-por-ee-uu/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/Ruben-Rocha-26-05-2026-750x430.jpg"
+      "title": "Hugo Carvajal estaría en conversaciones con la Fiscalía de EE. UU. y ofrece información sobre Zapatero",
+      "link": "https://diarioversionfinal.com/mundo/hugo-carvajal-estaria-en-conversaciones-con-la-fiscalia-de-ee-uu-y-ofrece-informacion-sobre-zapatero/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/07/cnne-1087535-hugo-pollo-carvajal-quedo-al-borde-de-la-extradicion-a-estados-unidos-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-05-27T09:50:18.950Z"
+  "lastUpdate": "2026-05-27T16:37:43.081Z"
 };
