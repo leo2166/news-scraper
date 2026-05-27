01ae42c830b9e425d4a921df27288bb3a6c0288f
GitHub Action <actions@github.com>
2026-05-27 19:27:11 +0000

    data: update news and rates at Wed May 27 19:27:11 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Miércoles, 27 Mayo 2026"
     },
     "binance": {
-      "usdt": "769,99"
+      "usdt": "773,10"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,13 +21,13 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "La activista Rocío San Miguel regresó a Venezuela tras permanecer más de cinco meses en España",
-      "link": "https://www.infobae.com/venezuela/2026/05/27/la-activista-rocio-san-miguel-regreso-a-venezuela-tras-permanecer-mas-de-cinco-meses-en-espana/",
-      "image": "https://www.infobae.com/resizer/v2/6SNPKLM4HFAC5OOZKQ4T2XYSJI.jpg?auth=40883c1d506fc3eefe1639a0f0646b47de1b4413efae75a7d809f7ec06061d5e&smart=true&width=350&height=197&quality=85"
+      "title": "Una madre contra el silencio del Estado: la búsqueda interminable del fotógrafo que desapareció en la frontera tras las elecciones",
+      "link": "https://www.infobae.com/venezuela/2026/05/27/una-madre-contra-el-silencio-del-estado-la-busqueda-interminable-del-fotografo-que-desaparecio-en-la-frontera-tras-las-elecciones/",
+      "image": "https://www.infobae.com/resizer/v2/3VP7AVRKE5DJTECXZJUUOGDXXQ.jpeg?auth=2a06e17255d80078473a2013ea2719ac3c1f1f36b30ad291ee4c3eb4f81d63b4&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "La Casa Blanca dice que el borrador difundido por medios iraníes es una “invención”",
+      "title": "Trump sobre acuerdo con Irán: “No me importan las elecciones de mitad de mandato”",
       "link": "https://cnnespanol.cnn.com/2026/05/27/mundo/live-news/guerra-iran-ee-uu-gabinete-represalias-vivo-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/8940082-20260527103256922.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
@@ -39,22 +39,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "ICE DETUVO AL MÚSICO ZULIANO GIANLORETT MATOS EN TEXAS",
-      "link": "https://noticierodigital.com/2026/05/ice-detuvo-al-musico-zuliano-gianlorett-matos-en-texas/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/ICEmusicozuliano-627x376.jpg"
+      "title": "HENRY CAVILL Y JAKE GYLLENHAAL LIDERAN UNA MISIÓN DE ALTO RIESGO «EN LA ZONA GRIS»",
+      "link": "https://noticierodigital.com/2026/05/henry-cavill-y-jake-gyllenhaal-lideran-una-mision-de-alto-riesgo-en-la-zona-gris/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/Diseno-sin-titulo-29-627x376.png"
     },
     {
       "source": "La Verdad",
-      "title": "Al menos 12 soldados colombianos resultan heridos en ataque del ELN a 4 días de las elecciones",
-      "link": "https://laverdad.com/al-menos-12-soldados-colombianos-resultan-heridos-en-ataque-del-eln-a-4-dias-de-las-elecciones/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/05/imagen_2026-05-27_104247031.png"
+      "title": "León XIV se sienta al volante del primer Ferrari eléctrico",
+      "link": "https://laverdad.com/leon-xiv-se-sienta-al-volante-del-primer-ferrari-electrico/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/05/Papa-Ferrari-electrico.png"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Hugo Carvajal estaría en conversaciones con la Fiscalía de EE. UU. y ofrece información sobre Zapatero",
-      "link": "https://diarioversionfinal.com/mundo/hugo-carvajal-estaria-en-conversaciones-con-la-fiscalia-de-ee-uu-y-ofrece-informacion-sobre-zapatero/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/07/cnne-1087535-hugo-pollo-carvajal-quedo-al-borde-de-la-extradicion-a-estados-unidos-750x430.jpeg"
+      "title": "Muere en Chichiriviche Xavier Arruti, etarra al que Maduro incorporó en Pdvsa",
+      "link": "https://diarioversionfinal.com/mundo/muere-en-chichiriviche-xavier-arruti-etarra-al-que-maduro-incorporo-en-pdvsa/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/arruti-1-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-05-27T16:37:43.081Z"
+  "lastUpdate": "2026-05-27T19:25:19.868Z"
 };
