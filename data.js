396f4e1dd2cdc49962c881aa930c745b271341ad
GitHub Action <actions@github.com>
2026-05-29 15:34:37 +0000

    data: update news and rates at Fri May 29 15:34:37 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Viernes, 29 Mayo 2026"
     },
     "binance": {
-      "usdt": "773,95"
+      "usdt": "775,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,40 +21,40 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "La oposición liderada por María Corina Machado propuso negociar una transición “seria y responsable” en Venezuela con Delcy Rodríguez y EEUU",
-      "link": "https://www.infobae.com/venezuela/2026/05/29/la-oposicion-liderada-por-maria-corina-machado-propuso-negociar-una-transicion-seria-y-responsable-en-venezuela-con-delcy-rodriguez-y-eeuu/",
-      "image": "https://www.infobae.com/resizer/v2/H2GGIOHRZBEB5AJFALXS4KJOFY.JPG?auth=65b09d58d0c30920dff6787bf3cf3c50c8f1e563aab225f35d6787dfdeee4fd7&smart=true&width=350&height=197&quality=85"
+      "title": "“Mi hijo es inocente”: piden amnistía para preso acusado de intento de magnicidio contra Nicolás Maduro",
+      "link": "https://www.infobae.com/venezuela/2026/05/29/mi-hijo-es-inocente-piden-amnistia-para-preso-acusado-de-intento-de-magnicidio-contra-nicolas-maduro/",
+      "image": "https://www.infobae.com/resizer/v2/DZKRR3KTKRDZ5P35ANSBEKRKVY.jpg?auth=37633ea47e2b6a5907103314cb39c13519b01d1fcbec5c714cf4feed470378eb&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "EE.UU. e Irán alcanzan un acuerdo preliminar, pero Trump debe dar su visto bueno",
-      "link": "https://cnnespanol.cnn.com/2026/05/29/mundo/live-news/guerra-iran-ee-uu-israel-nuevos-ataques-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ed561bec-2ac6-4103-860a-4ef16324fe77.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "El primer sobreviviente logra salir de la cueva en Laos",
+      "link": "https://cnnespanol.cnn.com/2026/05/29/mundo/live-news/laos-cueva-mision-rescate-en-vivo-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2026-05-12t085351z-804505313-rc2j7lawllbw-rtrmadp-3-iran-crisis-daily-life-20260529123225912.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Parlamento reformará ley contra robo o hurto de ganado",
-      "link": "https://noticiasvenevision.com/noticias/politica/parlamento-reformara-ley-contra-robo-o-hurto-de-ganado",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_4956600720576678962_y-e22bfd.jpg"
+      "title": "María Corina, la PUD y Con Vzla firman el “Manifiesto de Panamá” por la transición",
+      "link": "https://noticiasvenevision.com/noticias/politica/maria-corina-la-pud-y-con-vzla-firman-el-manifiesto-de-panama-por-la-transicion",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/5ebc5c0184afbd44729c7fe5df90cf85b8ace688-860x574-f8cc2e.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "IRIS VARELA DESCARTÓ ELECCIONES PRESIDENCIALES EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/05/iris-varela-descarto-elecciones-presidenciales-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2020/12/iris-varela-4dic2020.jpg"
+      "title": "ESTOS SON LOS 3 PRINCIPALES CANDIDATOS PARA PRESIDIR COLOMBIA: ELECCIONES DEL 31M",
+      "link": "https://noticierodigital.com/2026/05/estos-son-los-3-principales-candidatos-para-presidir-colombia-elecciones-del-31m/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/Colombia_elecciones-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Gobierno de EE. UU. presiona para que se impriman billetes de $250 con la cara de Trump",
-      "link": "https://laverdad.com/gobierno-de-ee-uu-presiona-para-que-se-impriman-billetes-de-250-con-la-cara-de-trump/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/05/g-kj-v-weaa2ill-20260528174726913-1.jpg"
+      "title": "Colombia captura en la frontera a “Caraota”, presunto miembro del “Tren de Aragua”",
+      "link": "https://laverdad.com/colombia-captura-en-la-frontera-a-caraota-presunto-miembro-del-tren-de-aragua/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/05/image-2026-05-29T110208.397.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "EE. UU. presenta portal “aliens” con datos de detenciones de inmigrantes",
-      "link": "https://diarioversionfinal.com/mundo/ee-uu-presenta-portal-aliens-con-datos-de-detenciones-de-inmigrantes/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/X5QFHOPZIRB67CLU42DSHMZJW4-750x430.jpg"
+      "title": "Álvaro Uribe advierte a Iván Cepeda que \"el chavismo local destruye la empresa privada y la pública\"",
+      "link": "https://diarioversionfinal.com/mundo/alvaro-uribe-advierte-a-ivan-cepeda-que-el-chavismo-local-destruye-la-empresa-privada-y-la-publica/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2022/01/alvaro-uribe-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-05-29T11:31:52.441Z"
+  "lastUpdate": "2026-05-29T15:32:40.384Z"
 };
