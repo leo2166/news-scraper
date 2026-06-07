f96f031952ff1ed12fa6a9dc0faf30f336ad76ea
GitHub Action <actions@github.com>
2026-06-07 22:06:18 +0000

    data: update news and rates at Sun Jun  7 22:06:18 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 09 Junio 2026"
     },
     "binance": {
-      "usdt": "797,89"
+      "usdt": "799,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,27 +15,27 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Todos los sectores reportaron inflación de un dígito en mayo, pero el INPC ha subido 101,97% en 2026",
-      "link": "https://www.bancaynegocios.com/todos-los-sectores-reportaron-inflacion-de-un-digito-en-mayo-pero-el-inpc-ha-subido-10197-en-2026/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/10/INFLACION-NUEVA.jpg"
+      "title": "Plantean excenciones arancelarias para importación de insumos destinados a la producción de camarones",
+      "link": "https://www.bancaynegocios.com/plantean-excenciones-arancelarias-para-importacion-de-insumos-destinados-a-la-produccion-de-camarones/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/camarones.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Cinco años después, persisten las dudas sobre un soldado desaparecido en la frontera venezolana: “Teníamos miedo de denunciar”",
-      "link": "https://www.infobae.com/venezuela/2026/06/07/cinco-anos-despues-persisten-las-dudas-sobre-un-soldado-desaparecido-en-la-frontera-venezolana-teniamos-miedo-de-denunciar/",
-      "image": "https://www.infobae.com/resizer/v2/AN62F4FERJGV3LYXYTX7FS37ZY.jpg?auth=4011839c6a9b60111e23f306298a94ba2b2c3623faf634db74d4c9217984d67a&smart=true&width=350&height=197&quality=85"
+      "title": "Delcy Rodríguez llegó a Turquía tras su paso por la India para consolidar a Venezuela como proveedor clave de crudo",
+      "link": "https://www.infobae.com/venezuela/2026/06/07/delcy-rodriguez-llego-a-turquia-tras-su-paso-por-la-india-para-consolidar-a-venezuela-como-proveedor-clave-de-crudo/",
+      "image": "https://www.infobae.com/resizer/v2/7KHIRMIWONCZZLLM5LRBC2YWOE.png?auth=9cbd01c15b993e1ca3b21c5dfd16109c4834f4dc2cb564e74723cd40aa1940c1&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "Detienen a dos personeros por presunta manipulación de votos",
+      "title": "Ha sido una jornada electoral tranquila en Perú, según la OEA",
       "link": "https://cnnespanol.cnn.com/2026/06/07/latinoamerica/live-news/peru-elecciones-presidenciales-keiko-fujimori-sanchez-segunda-vuelta-orix",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2278788134.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Presidenta (e) Delcy Rodríguez culmina visita a la India",
-      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-e-delcy-rodriguez-culmina-visita-a-la-india",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/30_zc__2235_p-1536x1024-1-f40bd2.jpg"
+      "title": "Presidenta (E) Delcy Rodríguez arriba a Turquía para continuar agenda de trabajo internacional",
+      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-e-delcy-rodriguez-arriba-a-turquia-para-continuar-agenda-de-trabajo-internacional",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_4985624305676782783_x-eca71c.jpg"
     },
     {
       "source": "Noticiero Digital",
@@ -45,16 +45,16 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "La Verdad",
-      "title": "Papa León XIV insta desde Cibeles a abandonar el egoísmo y actuar por el bien común",
-      "link": "https://laverdad.com/papa-leon-xiv-insta-desde-cibeles-a-abandonar-el-egoismo-y-actuar-por-el-bien-comun/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/06/9a8dbfa4b494670313710f71954062b7771316fb-scaled.jpg"
+      "title": "Matan a periodista colombiano amenazado por su labor en la frontera con Venezuela",
+      "link": "https://laverdad.com/matan-a-periodista-colombiano-amenazado-por-su-labor-en-la-frontera-con-venezuela/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/06/rss-efe87bf0c6081aa7e1672e46b2da02240bd78b55a0fw.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Iván Cepeda solicita debate a Abelardo de la Espriella antes de segunda vuelta en Colombia",
-      "link": "https://diarioversionfinal.com/mundo/ivan-cepeda-solicita-debate-a-abelardo-de-la-espriella-antes-de-segunda-vuelta-en-colombia/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/10/Ivan_Cepeda_Castro-750x430.webp"
+      "title": "Presidente interino de Perú llama a una transición pacífica y pide reconocer resultados electorales",
+      "link": "https://diarioversionfinal.com/mundo/presidente-interino-de-peru-llama-a-una-transicion-pacifica-y-pide-reconocer-resultados-electorales/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/rss-efe348f3ea71a8c112e462f9a3192a76e922d84250ew-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-06-07T20:12:18.968Z"
+  "lastUpdate": "2026-06-07T22:04:26.142Z"
 };
