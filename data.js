2c7224c9d01fcaa1de52e2411bf2ef85275d21fa
GitHub Action <actions@github.com>
2026-06-08 17:55:22 +0000

    data: update news and rates at Mon Jun  8 17:55:22 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 09 Junio 2026"
     },
     "binance": {
-      "usdt": "797,89"
+      "usdt": "799,65"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,40 +21,40 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Delcy Rodríguez llegó a Turquía tras su paso por la India para consolidar a Venezuela como proveedor clave de crudo",
-      "link": "https://www.infobae.com/venezuela/2026/06/07/delcy-rodriguez-llego-a-turquia-tras-su-paso-por-la-india-para-consolidar-a-venezuela-como-proveedor-clave-de-crudo/",
-      "image": "https://www.infobae.com/resizer/v2/7KHIRMIWONCZZLLM5LRBC2YWOE.png?auth=9cbd01c15b993e1ca3b21c5dfd16109c4834f4dc2cb564e74723cd40aa1940c1&smart=true&width=350&height=197&quality=85"
+      "title": "Denuncian que se pone en peligro la salud de los niños al racionar la aplicación de vacunas en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/06/08/denuncian-que-se-pone-en-peligro-la-salud-de-los-ninos-al-racionar-la-aplicacion-de-vacunas-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/AN55KKFJKLUIJ7TKERXCMDKKII.jpg?auth=325e672be6f94e45ac3d11efe8b0166859749f44a04874d9cb95b12c3ca04b5e&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
       "title": "Fujimori lidera por un estrecho margen el conteo de votos en Perú",
       "link": "https://cnnespanol.cnn.com/2026/06/07/latinoamerica/live-news/peru-elecciones-presidenciales-keiko-fujimori-sanchez-segunda-vuelta-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/20260607-keiko-sanchez-resultados-hp.jpg?c=2x3&q=h_384,w_256,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/videothumbnails/89650671-02520727-generated-thumbnail.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Presidenta (E) Delcy Rodríguez arriba a Turquía para continuar agenda de trabajo internacional",
-      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-e-delcy-rodriguez-arriba-a-turquia-para-continuar-agenda-de-trabajo-internacional",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_4985624305676782783_x-eca71c.jpg"
+      "title": "Presidenta encargada Delcy Rodríguez se reunió con el presidente turco Recep Tayyip Erdogan",
+      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-encargada-delcy-rodriguez-se-reunio-con-el-presidente-turco-recep-tayyip-erdogan",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/delcy--8bf4d1.png"
     },
     {
       "source": "Noticiero Digital",
-      "title": "AVIÓN EJECUTIVO DE EE. UU. SE ESTRELLÓ EN REPÚBLICA DOMINICANA DEJANDO DOS MUERTOS",
-      "link": "https://noticierodigital.com/2026/06/avion-ejecutivo-de-ee-uu-se-estrello-en-republica-dominicana-dejando-dos-muertos/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/Diseno-sin-titulo-1-2-627x376.png"
+      "title": "ELCY RODRÍGUEZ SE REUNIÓ CON MINISTRO DE ENERGÍA Y RECURSOS NATURALES DE ESTAMBUL",
+      "link": "https://noticierodigital.com/2026/06/elcy-rodriguez-se-reunio-con-ministro-de-energia-y-recursos-naturales-de-estambul/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/delcyrodriguez_Estambul-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Keiko Fujimori conserva 4,4 puntos de ventaja sobre Sánchez al 76 % del escrutinio en Perú",
-      "link": "https://laverdad.com/keiko-fujimori-conserva-44-puntos-de-ventaja-sobre-sanchez-al-76-del-escrutinio-en-peru/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/06/Keiko-Fujimori-scaled.jpg"
+      "title": "Bebé hijo de migrante zuliano recibe bendición del papa León XIV en Madrid",
+      "link": "https://laverdad.com/bebe-hijo-de-migrante-zuliano-recibe-bendicion-del-papa-leon-xiv-en-madrid/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/06/Papa-Leon-XIV-2.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Air Canada, Avianca y Gol avanzan hacia una alianza para fortalecer la conectividad en América",
-      "link": "https://diarioversionfinal.com/mundo/air-canada-avianca-y-gol-avanzan-hacia-una-alianza-para-fortalecer-la-conectividad-en-america/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2022/09/avianca-750x430.jpg"
+      "title": "Espacio Público contabiliza 13 violaciones a la libertad de expresión durante mayo",
+      "link": "https://diarioversionfinal.com/mundo/espacio-publico-contabiliza-13-violaciones-a-la-libertad-de-expresion-durante-mayo/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/libertad-de-expresion.jpg"
     }
   ],
-  "lastUpdate": "2026-06-08T14:49:11.858Z"
+  "lastUpdate": "2026-06-08T17:53:30.631Z"
 };
