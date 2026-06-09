776c07a0fe101d648c704ce54995ab887f492e3e
GitHub Action <actions@github.com>
2026-06-09 20:59:43 +0000

    data: update news and rates at Tue Jun  9 20:59:43 UTC 2026

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "567,68",
-      "eur": "655,38",
-      "fechaValor": "Martes, 09 Junio 2026"
+      "usd": "572,68",
+      "eur": "662,25",
+      "fechaValor": "Miércoles, 10 Junio 2026"
     },
     "binance": {
-      "usdt": "809,71"
+      "usdt": "812,07"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,21 +15,21 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Canasta Alimentaria de Maracaibo se ubicó en Bs. 339.095 en mayo: Incrementó más de 16% en bolívares",
-      "link": "https://www.bancaynegocios.com/canasta-alimentaria-de-maracaibo-se-ubico-en-bs-339095-en-mayo-incremento-mas-de-16-porciento-en-bolivares/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/SupermercadoAlimentosComida.jpg"
+      "title": "#Datos: Sector privado operó con 46,4% de capacidad ociosa y nóminas estables en el primer trimestre",
+      "link": "https://www.bancaynegocios.com/datos-sector-privado-opero-con-46coma4-porciento-de-capacidad-ociosa-y-nominas-estables-en-el-primer-trimestre/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/08/INDUSTRIA-INFORME.png"
     },
     {
       "source": "Infobae",
-      "title": "El Gobierno encargado de Venezuela indicó que no está planteada una negociación con María Corina Machado",
-      "link": "https://www.infobae.com/venezuela/2026/06/09/el-gobierno-encargado-de-venezuela-indico-que-no-esta-planteada-una-negociacion-con-maria-corina-machado/",
-      "image": "https://www.infobae.com/resizer/v2/YHKZJU732RHQLDKN7L4C5BHS54.JPG?auth=83793f63b5c0baa497cc6f576e23ae791725c9c05871a32540269b5438521728&smart=true&width=350&height=197&quality=85"
+      "title": "ONG señala que en el primer trimestre del año se registró un femicidio cada 64 horas en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/06/09/ong-senala-que-en-el-primer-trimestre-del-ano-se-registro-un-femicidio-cada-64-horas-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/FIDBRPYI2VB5DHMKZ7NX2RXIBY.png?auth=78db2cb1e75b1b4fc713114dfbc9ea75d4c07d8dce41911c2725b9147f2bf8c3&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "El boom del Mundial no termina de llegar a México",
-      "link": "https://cnnespanol.cnn.com/2026/06/09/mexico/reservas-mundial-hoteles-airbnb-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2224529392-20260529024748380.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Trump afirma que Irán derribó el helicóptero Apache y que EE.UU. debe \"responder\"",
+      "link": "https://cnnespanol.cnn.com/2026/06/09/mundo/live-news/guerra-eeuu-israel-iran-vivo-conversaciones-paz-3-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/2026-06-09t044126z-1483211279-rc23qlab4tva-rtrmadp-3-usa-trump-20260609165834761.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -39,22 +39,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "MINISTRA SANJUÁN: «EL BONO DE RESPONSABILIDAD ACADÉMICA NO ES EL ÚNICO, NI ES EL ÚLTIMO»",
-      "link": "https://noticierodigital.com/2026/06/ministra-sanjuan-el-bono-de-responsabilidad-academica-no-es-el-unico-ni-es-el-ultimo/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/ana-maria-sanjuan-627x376.jpg"
+      "title": "DRON IRANÍ DERRIBÓ HELICÓPTERO DEL EJÉRCITO ESTADOUNIDENSE, SEGÚN FUNCIONARIOS DE EE. UU.",
+      "link": "https://noticierodigital.com/2026/06/dron-irani-derribo-helicoptero-del-ejercito-estadounidense-segun-funcionarios-de-ee-uu/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/donald-trump-eeuu-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Barcelona recibe al Papa con gran expectación y emoción de los fieles",
-      "link": "https://laverdad.com/barcelona-recibe-al-papa-con-gran-expectacion-y-emocion-de-los-fieles/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/06/papa-barcelona-7-2.jpg"
+      "title": "La Nasa presenta tripulación de la misión Artemis III, otro paso hacia el regreso a la Luna",
+      "link": "https://laverdad.com/la-nasa-presenta-tripulacion-de-la-mision-artemis-iii-otro-paso-hacia-el-regreso-a-la-luna/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/06/Nasa.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Gobierno boliviano acusa a protestas contra Paz de tener vínculos con el narcotráfico",
-      "link": "https://diarioversionfinal.com/mundo/gobierno-boliviano-acusa-a-protestas-contra-paz-de-tener-vinculos-con-el-narcotrafico/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/DURDKRNZFFEJ7KZWVKSIYV6OYA-750x430.jpg"
+      "title": "Abinader oficializa designación de nuevo cónsul de República Dominicana en Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/abinader-oficializa-designacion-de-nuevo-consul-de-republica-dominicana-en-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/742CE821-47A8-4E1D-8FA0-624FFE5D30AF.webp"
     }
   ],
-  "lastUpdate": "2026-06-09T18:05:45.320Z"
+  "lastUpdate": "2026-06-09T20:57:50.688Z"
 };
