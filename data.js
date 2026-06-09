6636f46a5881a68348afa889d266b768ad43498e
GitHub Action <actions@github.com>
2026-06-09 15:57:03 +0000

    data: update news and rates at Tue Jun  9 15:57:03 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 09 Junio 2026"
     },
     "binance": {
-      "usdt": "794,32"
+      "usdt": "799,69"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,46 +15,46 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Venezuela y Turquía realizarán V Comisión Mixta de Cooperación en noviembre",
-      "link": "https://www.bancaynegocios.com/venezuela-y-turquia-realizaran-v-comision-mixta-de-cooperacion-en-noviembre/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/06/DELCY-RODRIGUEZ-ERDOGAN-080626.jpg"
+      "title": "Canasta Alimentaria de Maracaibo se ubicó en Bs. 339.095 en mayo: Incrementó más de 16% en bolívares",
+      "link": "https://www.bancaynegocios.com/canasta-alimentaria-de-maracaibo-se-ubico-en-bs-339095-en-mayo-incremento-mas-de-16-porciento-en-bolivares/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/SupermercadoAlimentosComida.jpg"
     },
     {
       "source": "Infobae",
-      "title": "EEUU dijo que las exportaciones petroleras de Venezuela alcanzaron su “nivel más alto” en siete años",
-      "link": "https://www.infobae.com/venezuela/2026/06/09/eeuu-dijo-que-las-exportaciones-petroleras-de-venezuela-alcanzaron-su-nivel-mas-alto-en-siete-anos/",
-      "image": "https://www.infobae.com/resizer/v2/ECJTYXNZX5DHZP5ANHU4RZDQ4Y.JPG?auth=27f907a71303422b09acd27b8f7836b910b80c6e1f7c33a13f2139470386b91f&smart=true&width=350&height=197&quality=85"
+      "title": "El Gobierno encargado de Venezuela indicó que no está planteada una negociación con María Corina Machado",
+      "link": "https://www.infobae.com/venezuela/2026/06/09/el-gobierno-encargado-de-venezuela-indico-que-no-esta-planteada-una-negociacion-con-maria-corina-machado/",
+      "image": "https://www.infobae.com/resizer/v2/YHKZJU732RHQLDKN7L4C5BHS54.JPG?auth=83793f63b5c0baa497cc6f576e23ae791725c9c05871a32540269b5438521728&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
       "title": "Perú, dividido: ¿a quién favorecerán los votos que faltan contar?",
       "link": "https://cnnespanol.cnn.com/2026/06/08/latinoamerica/peru-resultados-actas-pendientes-onpe-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ap26158651660975.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/20260607-keiko-sanchez-resultados-hp.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "República Dominicana designa cónsul general en Caracas",
-      "link": "https://noticiasvenevision.com/noticias/politica/republica-dominicana-designa-consul-general-en-caracas",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/vas-quww--4663f4.jpg"
+      "title": "Delcy Rodríguez emprende regreso al país tras gira por India y Turquía",
+      "link": "https://noticiasvenevision.com/noticias/politica/delcy-rodriguez-emprende-regreso-al-pais-tras-gira-por-india-y-turquia",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/diseno-sin-titulo-3-1068x601-d91506.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "SECTOR PETROLERO COLOMBIANO COMIENZA A VER EN VENEZUELA UNA OPCIÓN PARA CRECER",
-      "link": "https://noticierodigital.com/2026/06/sector-petrolero-colombiano-comienza-a-ver-en-venezuela-una-opcion-para-crecer/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/03/petroleo-caracas-venezuela-627x376.jpg"
+      "title": "CAPRILES: «HAY QUE DECIRLE A EE .UU. QUE NO HABRÁ RECUPERACIÓN SIN MEJORAS DEL INGRESO»",
+      "link": "https://noticierodigital.com/2026/06/capriles-hay-que-decirle-a-ee-uu-que-no-habra-recuperacion-sin-mejoras-del-ingreso/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/11/henrique-capriles-radonski-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Roberto Sánchez adelanta a Keiko Fujimori al llegar al 93,92 % del escrutinio en Perú",
-      "link": "https://laverdad.com/roberto-sanchez-adelanta-a-keiko-fujimori-al-llegar-al-9392-del-escrutinio-en-peru/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/06/Roberto-Sanchez-scaled.jpg"
+      "title": "CPI suspende al fiscal Karim Khan y emplaza a países miembros a votar sobre su destitución",
+      "link": "https://laverdad.com/cpi-suspende-al-fiscal-karim-khan-y-emplaza-a-paises-miembros-a-votar-sobre-su-destitucion/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/06/fiscal-de-la-corte-penal-internacional-cpi-karim-khan-22073-1.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Sube a 37 número de muertos por fuerte terremoto en Filipinas",
-      "link": "https://diarioversionfinal.com/mundo/sube-a-37-numero-de-muertos-por-fuerte-terremoto-en-filipinas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/Terremoto-Filipinas-08-06-2026-750x430.jpg"
+      "title": "Una segunda ejecutiva bancaria es imputada en investigación contra el Tren de Aragua en Chile",
+      "link": "https://diarioversionfinal.com/mundo/una-segunda-ejecutiva-bancaria-es-imputada-en-investigacion-contra-el-tren-de-aragua-en-chile/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/extorsionadora-y-empleada-bancaria_-revelan-rol-de-ejecutiva-de-bancoestado-ligada-al-tren-de-aragua-2-750x430.png"
     }
   ],
-  "lastUpdate": "2026-06-09T12:37:26.482Z"
+  "lastUpdate": "2026-06-09T15:55:06.147Z"
 };
