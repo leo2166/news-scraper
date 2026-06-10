3708aefa1dc1e6095f1d50f23035c1172077ae0b
GitHub Action <actions@github.com>
2026-06-10 22:52:48 +0000

    data: update news and rates at Wed Jun 10 22:52:48 UTC 2026

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "572,68",
-      "eur": "662,25",
-      "fechaValor": "Miércoles, 10 Junio 2026"
+      "usd": "577,55",
+      "eur": "667,37",
+      "fechaValor": "Jueves, 11 Junio 2026"
     },
     "binance": {
       "usdt": "845,03"
@@ -21,15 +21,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Fuerza Armada de Venezuela ataca a bandas criminales en zonas mineras tras la firma de acuerdos con Estados Unidos",
-      "link": "https://www.infobae.com/venezuela/2026/06/10/fuerza-armada-de-venezuela-ataca-a-bandas-criminales-en-zonas-mineras-tras-la-firma-de-acuerdos-con-estados-unidos/",
-      "image": "https://www.infobae.com/resizer/v2/MOXWIODU35BFPDKZOB62UUDWF4.jpg?auth=4ac2ed6ac8c823e433eb89ef3fc95af716dd6586792810cce11e6db5bb77324e&smart=true&width=350&height=197&quality=85"
+      "title": "La Corte Interamericana de Derechos Humanos ordenó el cierre de El Helicoide, el mayor centro de torturas del chavismo",
+      "link": "https://www.infobae.com/venezuela/2026/06/10/la-corte-interamericana-de-derechos-humanos-ordeno-el-cierre-de-el-helicoide-el-mayor-centro-de-torturas-del-chavismo/",
+      "image": "https://www.infobae.com/resizer/v2/7KSD2ASY6BBMPPQLYZYL52O7IU.jpg?auth=82759d0813d80d5a29e0fedfdc79c8e864614eedeb18f1f458fe02dbbb194fd0&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump promete atacar Irán y sigue la retórica belicista",
+      "title": "Estados Unidos lanza más ataques contra Irán",
       "link": "https://cnnespanol.cnn.com/2026/06/10/mundo/live-news/guerra-eeuu-israel-iran-vivo-conversaciones-paz-4-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2280241481.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/c-2026-06-09t063812z-1973947870-rc24qla81cqt-rtrmadp-3-usa-trump-20260609215452179.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -39,22 +39,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "TRUMP REITERA «RESPALDO TOTAL» A CANDIDATO COLOMBIANO ABELARDO DE LA ESPRIELLA",
-      "link": "https://noticierodigital.com/2026/06/trump-reitera-respaldo-total-a-candidato-colombiano-abelardo-de-la-espriella/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/05/donald-trump-eeuu-1-627x376.jpg"
+      "title": "VENEZUELA SELLÓ ALIANZA PETROLERA CON LA GIGANTE SCHLUMBERGER",
+      "link": "https://noticierodigital.com/2026/06/venezuela-sello-alianza-petrolera-con-la-gigante-schlumberger/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/delcy-rodriguez-venezuela-1-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Petro denuncia violación de la ley y apunta a proceso en la Corte Suprema por la suspensión",
-      "link": "https://laverdad.com/petro-denuncia-violacion-de-la-ley-y-apunta-a-proceso-en-la-corte-suprema-por-la-suspension/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/06/ONU-ASAMBLEA_GENERAL-COLOMBIA_91101-1.jpg"
+      "title": "León XIV ensalza la Sagrada Familia como “signo de unidad y concordia”",
+      "link": "https://laverdad.com/leon-xiv-ensalza-la-sagrada-familia-como-signo-de-unidad-y-concordia/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/06/3bfd879a34c721fb1dcb52dd2f06a111c7944953-1.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Netanyahu confirma que buscará un nuevo mandato en Israel",
-      "link": "https://diarioversionfinal.com/mundo/netanyahu-confirma-que-buscara-un-nuevo-mandato-en-israel/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/17676389533400-750x430.jpg"
+      "title": "Alto al fuego en vilo: EE. UU. lanza nueva ronda de ataques contra Irán",
+      "link": "https://diarioversionfinal.com/mundo/alto-al-fuego-en-vilo-ee-uu-lanza-nueva-ronda-de-ataques-contra-iran/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/EE.-UU-ataque-contra-Iran-10-06-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-06-10T20:30:48.619Z"
+  "lastUpdate": "2026-06-10T22:50:56.096Z"
 };
