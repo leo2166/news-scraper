901e9e16c77b71b014ac206daab7f853ec4b9a33
GitHub Action <actions@github.com>
2026-06-14 18:15:04 +0000

    data: update news and rates at Sun Jun 14 18:15:04 UTC 2026

--- data.js
+++ data.js
@@ -15,21 +15,15 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Qatar Airways iniciará en julio ruta que enlazará con Caracas y Bogotá",
-      "link": "https://www.bancaynegocios.com/qatar-airways-iniciara-en-julio-ruta-que-enlazara-con-caracas-y-bogota/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2024/12/qatar_airways.jpg"
+      "title": "Gobierno reorganiza en brigadas la actividad minera tras la operación militar contra el Tren de Aragua",
+      "link": "https://www.bancaynegocios.com/gobierno-reorganiza-en-brigadas-la-actividad-minera-tras-la-operacion-militar-contra-el-tren-de-aragua/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/06/ARCO-MINERO-140626.jpg"
     },
     {
       "source": "Infobae",
       "title": "Seis condenados y una acusación bajo sospecha: las grietas del expediente por el supuesto plan contra la nieta de Maduro",
       "link": "https://www.infobae.com/venezuela/2026/06/14/seis-condenados-y-una-acusacion-bajo-sospecha-las-grietas-del-expediente-por-el-supuesto-plan-contra-la-nieta-de-maduro/",
       "image": "https://www.infobae.com/resizer/v2/TTYMODSK2BEMFOO6HXLSEJJC5M.jpg?auth=60decc3dc69b376c28c149d02cfde7c2283c02b2b265967beaeec4e426dc9dc4&smart=true&width=350&height=197&quality=85"
-    },
-    {
-      "source": "Noticia al Día",
-      "title": "Detienen a un hombre en Maracaibo por presunta extorsión a una mujer con 5.000 dólares",
-      "link": "https://noticialdia.com/al-dia/capturado-en-maracaibo-presuntamente-por-intentar-extorsionar-a-una-mujer-con-cinco-mil-dolares/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/bfi_thumb/WhatsApp-Image-2026-06-14-at-6.09.33-AM-1000x520-2-24xb5vbybw487c7cxa2n9ivqftnc97y2g7k57mztevlg.jpeg?x40597"
     },
     {
       "source": "CNN en Español",
@@ -39,15 +33,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Andrés Caleca: \"Lo primero que se requiere para convocar unas elecciones en Venezuela es que haya voluntad política de todos para convocarla\"",
-      "link": "https://noticiasvenevision.com/noticias/politica/andres-caleca-lo-primero-que-se-requiere-para-convocar-unas-elecciones-en-venezuela-es-que-haya-voluntad-politica-de-todos-para-convocarla",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/hkxlmogwmaa9fnn-124d82.jpg"
+      "title": "John Magdaleno: \"El problema es si a partir de ahora podemos expresar libremente tenemos nuestra opinión sin temor a represalias\"",
+      "link": "https://noticiasvenevision.com/noticias/politica/john-magdaleno-el-problema-es-si-a-partir-de-ahora-podemos-expresar-libremente-tenemos-nuestra-opinion-sin-temor-a-represalias",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/img_6028.jpg-ba8198.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EMBAJADA DE EE. UU. SE SUMA AL AGRADECIMIENTO POR OPERACIÓN CONJUNTA EN LA CAÍDA DEL ‘NIÑO GUERRERO’",
-      "link": "https://noticierodigital.com/2026/06/embajada-de-ee-uu-se-suma-al-agradecimiento-por-operacion-conjunta-en-la-caida-del-nino-guerrero/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/10/embajada-de-eeuu-en-caracas-venezuela-627x376.jpg"
+      "title": "DEFENSORÍA HACE DESPLIEGUE INTERINSTITUCIONAL EN VARIOS ESTADOS POR REFORMA DE LA JUSTICIA PENAL",
+      "link": "https://noticierodigital.com/2026/06/defensoria-hace-despliegue-interinstitucional-en-varios-estados-por-reforma-de-la-justicia-penal/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/Diseno-sin-titulo-2-7-627x376.png"
     },
     {
       "source": "La Verdad",
@@ -62,5 +56,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/EuropaPress_7264306_january_2026_tehran_iran_esmaeil_baghaei_spokesperson_of_iranas_foreign-1024x683-1-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-06-14T16:21:55.952Z"
+  "lastUpdate": "2026-06-14T18:12:45.649Z"
 };
