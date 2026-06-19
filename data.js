b93215f8fe30ed356a6f6ba5708dffbd33934772
GitHub Action <actions@github.com>
2026-06-19 06:22:04 +0000

    data: update news and rates at Fri Jun 19 06:22:04 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Viernes, 19 Junio 2026"
     },
     "binance": {
-      "usdt": "837,60"
+      "usdt": "797,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,33 +21,33 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Estados Unidos respaldó el diálogo iniciado en Venezuela para reconstruir el sistema electoral y avanzar hacia una transición",
-      "link": "https://www.infobae.com/estados-unidos/2026/06/19/estados-unidos-respaldo-el-dialogo-iniciado-en-venezuela-para-reconstruir-el-sistema-electoral-y-avanzar-hacia-una-transicion/",
-      "image": "https://www.infobae.com/resizer/v2/ADLSNNYINBAMBNDGT2ST5TZLPY.jpg?auth=59ce4571157246e0d87ff46253911bad4745e0cb2a738e6ee6e66af22712b2dc&smart=true&width=350&height=197&quality=85"
+      "title": "Estados Unidos flexibilizó las sanciones a la aerolínea estatal venezolana Conviasa y autorizó nuevas operaciones vinculadas a PDVSA",
+      "link": "https://www.infobae.com/estados-unidos/2026/06/19/estados-unidos-flexibilizo-las-sanciones-a-la-aerolinea-estatal-venezolana-conviasa-y-autorizo-nuevas-operaciones-vinculadas-a-pdvsa/",
+      "image": "https://www.infobae.com/resizer/v2/7H6JSY33UVCMNHX33AFPS37NOU.JPG?auth=bc5b95219015f6954e0914793ecd9cf2238e537d4369a7a2d997ae39b4601fc0&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Reaparece alias “Naín” en redes sociales realizando control ilegal en La Guajira",
-      "link": "https://noticialdia.com/sucesos/reaparece-alias-nain-en-redes-sociales-realizando-control-ilegal-en-la-guajira/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/06/Alias-Naim-1-1.webp?x84139"
+      "title": "Consternación en Sabaneta de Palmas por la trágica muerte de Jean Carlos Paz Montero «Canito»",
+      "link": "https://noticialdia.com/sucesos/consternacion-en-sabaneta-de-palmas-por-la-tragica-muerte-de-jean-carlos-paz-montero-canito/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/06/ea719a6a-bbbc-42b2-a765-b3c656bf3dc3-1.webp?x84139"
     },
     {
       "source": "CNN en Español",
-      "title": "Propuestas secretas entre EE.UU. e Irán revelan la fragilidad del acuerdo",
-      "link": "https://cnnespanol.cnn.com/2026/06/18/eeuu/propuestas-eeuu-iran-fragilidad-acuerdo-nuclear-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/screenshot-2026-06-18-150413.png?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Se avecinan negociaciones difíciles entre EE.UU. e Irán",
+      "link": "https://cnnespanol.cnn.com/2026/06/18/mundo/live-news/guerra-ee-uu-israel-iran-conversaciones-paz-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ls-20260619061521484.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Alianza Bravo Pueblo en Monagas exige revisión de la “Causa 2020” y reestructuración del CNE",
-      "link": "https://noticiasvenevision.com/noticias/politica/alianza-bravo-pueblo-en-monagas-exige-revision-de-la-causa-2020-y-reestructuracion-del-cne",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/1767451706-venezuela-primer-paso-hacia-libertad-55f7dd.jpg"
+      "title": "Jorge Rodríguez y Dinorah Figuera instalan mesa técnica para el diálogo político en Venezuela",
+      "link": "https://noticiasvenevision.com/noticias/politica/jorge-rodriguez-y-dinora-figuera-instalan-mesa-tecnica-para-el-dialogo-politico-en-venezuela",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/7asamblea-530f30.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EE. UU. REPORTÓ TRES MUERTOS EN OPERATIVO MILITAR CONTRA EMBARCACIÓN EN EL PACÍFICO",
-      "link": "https://noticierodigital.com/2026/06/ee-uu-reporto-tres-muertos-en-operativo-militar-contra-embarcacion-en-el-pacifico/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/comando-sur-de-eeuu-ataque-narcolancha-627x376.jpg"
+      "title": "EMBAJADA DE EE. UU, DESTACÓ ACERCAMIENTO ENTRE DINORAH FIGUERA Y JORGE RODRÍGUEZ",
+      "link": "https://noticierodigital.com/2026/06/embajada-de-ee-uu-destaco-acercamiento-entre-dinorah-figuera-y-jorge-rodriguez/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/10/embajada-de-eeuu-en-caracas-venezuela-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Un centenar de guerrilleros entrega las armas en avance del proceso de paz en Colombia",
-      "link": "https://diarioversionfinal.com/mundo/un-centenar-de-guerrilleros-entrega-las-armas-en-avance-del-proceso-de-paz-en-colombia/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2022/09/guerrilleros-eln.-archivo-750x430.jpg"
+      "title": "Díaz-Canel rechaza injerencia estadounidense en nuevo paquete de reformas en Cuba",
+      "link": "https://diarioversionfinal.com/mundo/diaz-canel-rechaza-injerencia-estadounidense-en-nuevo-paquete-de-reformas-en-cuba/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/03/Diaz-Canel-18-03-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-06-19T00:26:54.317Z"
+  "lastUpdate": "2026-06-19T06:20:21.381Z"
 };
