59f408374b833d905dbeaae99893d61b74eda064
GitHub Action <actions@github.com>
2026-06-29 19:59:34 +0000

    data: update news and rates at Mon Jun 29 19:59:34 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 30 Junio 2026"
     },
     "binance": {
-      "usdt": "799,99"
+      "usdt": "785,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,25 +15,25 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "PNUD estima pérdidas de US$ 6.700 millones en viviendas y activos económicos por terremotos en Venezuela",
-      "link": "https://www.bancaynegocios.com/pnud-estima-perdidas-de-us-6700-millones-en-viviendas-y-activos-economicos-por-terremotos-en-venezuela/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/06/TerremotoVenezuelaEscombros.jpg"
+      "title": "Reconstrucción de zonas afectadas podría requerir inversiones de entre US$12.000 y US$15.000 millones",
+      "link": "https://www.bancaynegocios.com/reconstruccion-de-zonas-afectadas-podria-requerir-inversiones-de-entre-us12-000-y-us15-000-millones/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/06/TERREMOTO-VENEZUELA-AFP-290626.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Estados Unidos intensifica el despliegue de ayuda militar y humanitaria en Venezuela tras los terremotos",
-      "link": "https://www.infobae.com/estados-unidos/2026/06/29/estados-unidos-intensifica-el-despliegue-de-ayuda-militar-y-humanitaria-en-venezuela-tras-los-terremotos/",
-      "image": "https://www.infobae.com/resizer/v2/LEDVO45VZNGHFPCM45TIS732JI.JPG?auth=b085809c5b4be3be5deb92ec89a029cda51091cfb78a7cdbbf004685335e52e8&smart=true&width=350&height=197&quality=85"
+      "title": "Una réplica de 4,6 sacudió el norte de Venezuela mientras continúan las labores de rescate",
+      "link": "https://www.infobae.com/venezuela/2026/06/29/una-replica-de-46-sacudio-el-norte-de-venezuela-mientras-continuan-las-labores-de-rescate/",
+      "image": "https://www.infobae.com/resizer/v2/4TEBQ4IRCRBTVB7EJ6CFWXL2BU.JPG?auth=71cc3ae580ed3442778b6a4a7bcc451fc5fcb311e60b881464c40be3d261272b&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Cpbez activa 21 centros de acopio en el Zulia para asistir a víctimas del sismo",
-      "link": "https://noticialdia.com/sucesos/cpbez-activa-21-centros-de-acopio-en-el-zulia-para-asistir-a-victimas-del-sismo/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/06/Captura-39.webp?x74468"
+      "title": "Falleció el presidente del Metro de Caracas, Carlos Silva",
+      "link": "https://noticialdia.com/sucesos/fallecio-el-presidente-del-metro-de-caracas-carlos-silva/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/06/Metro-de-Caracas22.webp?x74468"
     },
     {
       "source": "CNN en Español",
-      "title": "Un sismo de magnitud 4,6 reaviva el temor en Venezuela",
+      "title": "Asciende a 1.719 el número de muertos por los terremotos en Venezuela",
       "link": "https://cnnespanol.cnn.com/2026/06/29/venezuela/live-news/terremotos-caracas-guaira-victimas-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/lucas-trejo-familia-terremoto-venezuela.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "SUPERMERCADOS INICIARON REABASTECIMIENTO DE AGUA POTABLE: ANSA NEGÓ DESABASTECIMIENTO",
-      "link": "https://noticierodigital.com/2026/06/supermercados-iniciaron-reabastecimiento-de-agua-potable-ansa-nego-desabastecimiento/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/04/1775565348724-627x376.png"
+      "title": "METRO DE CARACAS CONFIRMÓ FALLECIMIENTO DE SU PRESIDENTE, CARLOS SILVA, TRAS LOS TERREMOTOS",
+      "link": "https://noticierodigital.com/2026/06/metro-de-caracas-confirmo-fallecimiento-de-su-presidente-carlos-silva-tras-los-terremotos/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/Gemini_Generated_Image_a9ucnza9ucnza9uc-627x376.png"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "México enviará más ayuda a Venezuela tras la solicitud de Caracas",
-      "link": "https://diarioversionfinal.com/mundo/mexico-enviara-mas-ayuda-a-venezuela-tras-la-solicitud-de-caracas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/9A4WJmrA-88de7e5f2f06c5445dba0c91a11ae9c4bdf1a60ew-750x375-1.jpg"
+      "title": "Rick Scott advierte a autoridades venezolanas no interferir en labores de rescate",
+      "link": "https://diarioversionfinal.com/mundo/rick-scott-advierte-a-autoridades-venezolanas-no-interferir-en-labores-de-rescate/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2024/04/Rick-Scott-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-06-29T17:36:43.429Z"
+  "lastUpdate": "2026-06-29T19:57:48.352Z"
 };
