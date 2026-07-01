cf4157b387f72626cf32aed245e5ea51b9cf5a59
GitHub Action <actions@github.com>
2026-07-01 15:44:00 +0000

    data: update news and rates at Wed Jul  1 15:44:00 UTC 2026

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {},
     "binance": {
-      "usdt": "772,89"
+      "usdt": "770,25"
     },
     "kontigo": {
       "usd": "N/A"
@@ -11,27 +11,27 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Aumentó más de 14% en bolívares: Pagan segunda parte del \"Ingreso Integral de los Trabajadores\" de junio a jubilados",
-      "link": "https://www.bancaynegocios.com/aumento-mas-de-14-porciento-en-bolivares-pagan-segunda-parte-del-ingreso-integral-de-los-trabajadores-de-junio-a-jubilados/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2024/06/BolivaresDolaresCambio.jpg"
+      "title": "Estiman que más de 800 comercios se ven comprometidos por los terremotos en Venezuela",
+      "link": "https://www.bancaynegocios.com/estiman-que-mas-de-800-comercios-se-ven-comprometidos-por-los-terremotos-en-venezuela/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/ComerciosLaGuairaTerremotoCerrados.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Venezolanos obligaron a un policía a entregar dólares hallados entre los escombros que dejó el terremoto",
-      "link": "https://www.infobae.com/venezuela/2026/07/01/venezolanos-obligaron-a-un-policia-a-entregar-dolares-hallados-entre-los-escombros-que-dejo-el-terremoto/",
-      "image": "https://www.infobae.com/resizer/v2/KKO7V7A73ZGMBN5RCRUFNEPASY.jpg?auth=5c2dcf2c13d760fe7c415814d7a049dbe973aa2a949838fca1efe072df40c26f&smart=true&width=350&height=197&quality=85"
+      "title": "Diosdado Cabello: “Cero tolerancia” contra funcionarios que roben en zonas afectadas por el terremoto",
+      "link": "https://www.infobae.com/venezuela/2026/07/01/diosdado-cabello-cero-tolerancia-contra-funcionarios-que-roben-en-zonas-afectadas-por-el-terremoto/",
+      "image": "https://www.infobae.com/resizer/v2/ZQZMWJDFPJABDHJFYEAJWVUI3Q.JPG?auth=d22205de9183974c7b7fc5596cc97c04466c9f7c9ee85d70370d0130b49f16f7&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Zoe Antonella y Kloe Isabella, 2 milagros: Ahora reconstruyen su vida en San Francisco",
-      "link": "https://noticialdia.com/sucesos/zoe-antonella-y-kloe-isabella-2-milagros-ahora-reconstruyen-su-vida-en-san-francisco/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/06/WhatsApp-Image-2026-06-30-at-5.29.55-PM-convertido-a-1024x780-1.webp?x74468"
+      "title": "HAY ESPERANZA: Hallan con vida a dos personas tras siete días bajo los escombros del edificio Seyemar en La Guaira",
+      "link": "https://noticialdia.com/al-dia/hay-esperanza-hallan-con-vida-a-dos-personas-tras-siete-dias-bajo-los-escombros-del-edificio-seyemar-en-la-guaira/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/WhatsApp-Image-2026-07-01-at-11.00.30-AM-1.webp?x74468"
     },
     {
       "source": "CNN en Español",
-      "title": "Venezolanos limpian a mano escombros en una búsqueda desesperada",
-      "link": "https://cnnespanol.cnn.com/2026/07/01/venezuela/venezolanos-escombros-edificios-maquinaria-inactiva-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/whatsapp-image-2026-06-29-at-5-26-01-pm.jpeg?c=2x3&q=h_384,w_256,c_fill"
+      "title": "Una semana después de los terremotos, Venezuela enfrenta una crisis humanitaria",
+      "link": "https://cnnespanol.cnn.com/2026/07/01/venezuela/semana-terremotos-venezuela-refugios-rescates-crisis-humanitaria-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2283591433.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -41,9 +41,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "CARLOS JULIO ROJAS PIDIÓ ACELERAR LA INSPECCIÓN DE EDIFICIOS AFECTADOS EN CARACAS",
-      "link": "https://noticierodigital.com/2026/06/carlos-julio-rojas-pidio-acelerar-la-inspeccion-de-edificios-afectados-en-caracas/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/carlos-julio-rojas-627x376.jpg"
+      "title": "EE. UU. ENVIÓ A VENEZUELA VEHÍCULOS TODOTERRENO Y CAPACIDADES DE AMBULANCIAS MILITARES",
+      "link": "https://noticierodigital.com/2026/07/ee-uu-envio-a-venezuela-vehiculos-todoterreno-y-capacidades-de-ambulancias-militares/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/Diseno-sin-titulo-1-627x376.png"
     },
     {
       "source": "La Verdad",
@@ -53,10 +53,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Warren Buffett pone en pausa su donación a la Fundación Gates por caso Epstein",
-      "link": "https://diarioversionfinal.com/mundo/warren-buffett-pone-en-pausa-su-donacion-a-la-fundacion-gates-por-caso-epstein/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Warren-Buffett-30-06-2026-660x430.webp"
+      "title": "Ecuador repatria a seis ciudadanos afectados por los terremotos en Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/ecuador-repatria-a-seis-ciudadanos-afectados-por-los-terremotos-en-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/depositphotos_60335561-stock-photo-ecuadorian-flag.webp"
     }
   ],
-  "lastUpdate": "2026-07-01T12:07:47.165Z"
+  "lastUpdate": "2026-07-01T15:41:49.424Z"
 };
