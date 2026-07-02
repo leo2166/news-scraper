6a6c6e855f83dad1cf0d4c861d5e2dea4ccd75a9
GitHub Action <actions@github.com>
2026-07-02 17:49:00 +0000

    data: update news and rates at Thu Jul  2 17:49:00 UTC 2026

--- data.js
+++ data.js
@@ -15,52 +15,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Estiman que pérdidas económicas derivadas de los terremotos en Venezuela superan los US$ 10.000 millones",
-      "link": "https://www.bancaynegocios.com/estiman-que-perdidas-economicas-derivadas-de-los-terremotos-en-venezuela-superan-los-us-10000-millones/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2022/11/DivisasDolaresMonedaExtranjeraDenominacion.jpg"
+      "title": "#Dato: Peajes de todo el país se mantienen sin cobro por la contingencia tras terremotos",
+      "link": "https://www.bancaynegocios.com/dato-peajes-de-todo-el-pais-se-mantienen-sin-cobro-por-la-contingencia-tras-terremotos/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2022/11/PeajesVenezuela.jpg"
     },
     {
       "source": "Infobae",
-      "title": "“Uno al lado del otro”: hallaron muertos a la modelo Skarlent Rodríguez y a su pareja tras los terremotos en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/02/uno-al-lado-del-otro-hallaron-muertos-a-la-modelo-skarlent-rodriguez-y-a-su-pareja-tras-los-terremotos-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/4RK45A72QNFTLFVXJLCPQ2SOT4.jpg?auth=31b96a4510c4bc6b197e2bdedc030bdbbc90471f95f89713d99658045764c5bb&smart=true&width=350&height=197&quality=85"
+      "title": "El Salvador distribuyó más de 24 toneladas de medicamentos en hospitales venezolanos tras la emergencia por terremotos",
+      "link": "https://www.infobae.com/el-salvador/2026/07/02/el-salvador-distribuyo-mas-de-24-toneladas-de-medicamentos-en-hospitales-venezolanos-tras-la-emergencia-por-terremotos/",
+      "image": "https://www.infobae.com/resizer/v2/4C6OELX3XVH53M3D2L36E24TAI.jpg?auth=814c5b0680606f556ed65a3d83dd6a80fac11c9cbb0c856342d624da7eaf743d&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Balance de la explosión del pozo petrolero en La Victoria: Dos obreros muertos y ocho heridos evacuados por aire a Colombia",
-      "link": "https://noticialdia.com/sucesos/balance-de-la-explosion-del-pozo-petrolero-en-la-victoria-dos-obreros-muertos-y-ocho-heridos-evacuados-por-aire-a-colombia/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/imagen-traslado-1-convertido-a-1024x780-1.webp?x74468"
+      "title": "El SILENCIO: La clave para rescatar vidas bajo los escombros",
+      "link": "https://noticialdia.com/principal/el-silencio-la-clave-para-rescatar-vidas-bajo-los-escombros/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/TERRE.webp?x74468"
     },
     {
       "source": "CNN en Español",
       "title": "Un hombre que estuvo ocho días bajo los escombros fue sacado con vida en Venezuela",
       "link": "https://cnnespanol.cnn.com/2026/07/02/venezuela/live-news/terremotos-venezuela-en-vivo-noticias-la-guaira-caracas-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/c-gettyimages-2282638517-20260625220615611.jpg?c=2x3&q=h_384,w_256,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2283772311.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Comando sur informa apoyo espacial y unidades en tierra en zonas afectadas tras los terremotos en Venezuela",
-      "link": "https://noticiasvenevision.com/noticias/politica/comando-sur-informa-apoyo-espacial-y-unidades-en-tierra-en-zonas-afectadas-tras-los-terremotos-en-venezuela",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/saveclip.app_736420947_1506558124843229_8851563067069075432_n-e287be.jpg"
+      "title": "Ministra de Transporte restablece la vialidad en La Guaira y anuncia demolición de puente Caraballeda 2",
+      "link": "https://noticiasvenevision.com/noticias/politica/ministra-de-transporte-restablece-la-vialidad-en-la-guaira-y-anuncia-demolicion-de-puente-caraballeda-2",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/ministerio-de-transporte-inspecciones-3ee24a.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EL HATILLO: 179 INSPECCIONES Y 520 PACIENTES ATENDIDOS EN AMBULATORIO TRAS TERREMOTOS",
-      "link": "https://noticierodigital.com/2026/07/el-hatillo-179-inspecciones-y-520-pacientes-atendidos-en-ambulatorio-tras-terremotos/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/photo_2026-06-28_10-38-41-627x376.jpg"
+      "title": "RAFALLI: «LA AYUDA DESORGANIZADA PUEDE COLAPSAR A QUIENES TRABAJAN AL FILO DE LA VIDA»",
+      "link": "https://noticierodigital.com/2026/07/rafalli-la-ayuda-desorganizada-puede-colapsar-a-quienes-trabajan-al-filo-de-la-vida/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/20260702_122436-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Exilio venezolano pide a Trump revisar relación con Delcy Rodríguez tras terremotos",
-      "link": "https://laverdad.com/exilio-venezolano-pide-a-trump-revisar-relacion-con-delcy-rodriguez-tras-terremotos/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/07/exilio-venezolano-scaled.jpg"
+      "title": "OMS prevé que fallecidos por terremotos en el país aumenten de forma considerable",
+      "link": "https://laverdad.com/oms-preve-que-fallecidos-por-terremotos-en-el-pais-aumenten-de-forma-considerable/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/07/fallecidos-1-scaled.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "EE. UU. desmantela células del Tren de Aragua con el arresto de casi 350 integrantes",
-      "link": "https://diarioversionfinal.com/mundo/ee-uu-desmantela-celulas-del-tren-de-aragua-con-el-arresto-de-casi-350-integrantes/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/4K3GJXE_FBI_Director_Kash_Patel_Law_Enforcement_Attache_Office_Opening_Ceremony_at_U_S_Embassy_Wellington_31_July_2025_1_jpg_1-750x430.jpeg"
+      "title": "El BID aprueba donación de un millón de dólares para la asistencia humanitaria inmediata en Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/el-bid-aprueba-donacion-de-un-millon-de-dolares-para-la-asistencia-humanitaria-inmediata-en-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2018/05/banco-interamericano-de-desarrollo.jpg"
     }
   ],
-  "lastUpdate": "2026-07-02T15:23:40.598Z"
+  "lastUpdate": "2026-07-02T17:47:57.179Z"
 };
