6cb65d49dff8e65ccd2dae84ba864fa649c4fab3
GitHub Action <actions@github.com>
2026-07-03 15:13:05 +0000

    data: update news and rates at Fri Jul  3 15:13:05 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Viernes, 03 Julio 2026"
     },
     "binance": {
-      "usdt": "774,69"
+      "usdt": "771,25"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,21 +21,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "El parte médico de Hernán Gil, el sobreviviente del doble terremoto que fue rescatado tras ocho días en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/03/el-parte-medico-de-hernan-gil-el-sobreviviente-del-doble-terremoto-que-fue-rescatado-tras-ocho-dias-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/5HR3LESFDBAWZLDEHEQUVXLF3A.jpg?auth=657abfb3ad8d2c90be0d16df3e5aa9001121b9b94bde6a04dc112adaf9ba3078&smart=true&width=350&height=197&quality=85"
+      "title": "Las impactantes imágenes grabadas con drones que muestran los edificios de La Guaira arrasados ​​por los terremotos",
+      "link": "https://www.infobae.com/venezuela/2026/07/03/las-impactantes-imagenes-grabadas-con-drones-que-muestran-los-edificios-de-la-guaira-arrasados-por-los-terremotos/",
+      "image": "https://www.infobae.com/resizer/v2/5QB5I3MTEJH25P5XTANCS73I3E.jpg?auth=d0eae3521b71131b5199b5731500b8c103cf2cab08aed5482ad30776143d7805&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Murió un hombre tras desplomarse parte de la estructura en el antiguo Hospital Central de Maracaibo",
-      "link": "https://noticialdia.com/al-dia/murio-un-hombre-tras-desplomarse-parte-de-la-estructura-en-el-antiguo-hospital-central-de-maracaibo/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Hospital_Central_Dr._Urquinaona_I-1.webp?x74468"
+      "title": "Nuevo milagro en La Guaira: Rescatan con vida a septuagenaria tras pasar ocho días bajo los escombros",
+      "link": "https://noticialdia.com/sucesos/nuevo-milagro-en-la-guaira-rescatan-con-vida-a-septuagenaria-tras-pasar-ocho-dias-bajo-los-escombros/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/ppal22.webp?x74468"
     },
     {
       "source": "CNN en Español",
-      "title": "Irán envía un mensaje desafiante a Trump con el funeral para Jamenei",
-      "link": "https://cnnespanol.cnn.com/2026/07/03/mundo/iran-trump-funeral-lider-ali-jamenei-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ls-1-20260703104338173.jpg?c=2x3&q=h_384,w_256,c_fill"
+      "title": "Hospitales saturados y un sistema débil: Venezuela se acerca a una crisis sanitaria",
+      "link": "https://cnnespanol.cnn.com/2026/07/03/venezuela/terremotos-crisis-sanitaria-hospitales-saturados-sistema-debilitado-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/venezuela-gettyimages-2283800859.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "EL REY HA VUELTO: CONOR MCGREGOR ES NUEVO EMBAJADOR GLOBAL DE 1XBET",
-      "link": "https://noticierodigital.com/2026/07/el-rey-ha-vuelto-conor-mcgregor-es-nuevo-embajador-global-de-1xbet/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/conor-mcgregor-627x376.jpg"
+      "title": "SOCIEDAD ESPAÑOLA DE EPIDEMIOLOGÍA PIDE ATENDER CONSECUENCIAS PSICOLÓGICAS POR SISMOS EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/07/sociedad-espanola-de-epidemiologia-pide-atender-consecuencias-psicologicas-por-sismos-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/Diseno-sin-titulo-2-14-627x376.png"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Explosivo: Manuscrito de Nervis Villalobos vincula a Chávez, Zapatero y la empresa Duro Felguera en adjudicaciones de centrales termoeléctricas",
-      "link": "https://diarioversionfinal.com/xvf-portada/vf-home/explosivo-manuscrito-de-nervis-villalobos-vincula-a-chavez-zapatero-y-la-empresa-duro-felguera-en-adjudicaciones-de-centrales-termoelectricas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/jose-luis-rodriguez-zapatero-comision-caso-koldo-senado-marzo-2026_163.webp"
+      "title": "Comando Sur mantiene puente aéreo con aviones C-17 para enviar ayuda humanitaria a Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/comando-sur-mantiene-puente-aereo-con-aviones-c-17-para-enviar-ayuda-humanitaria-a-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/ee-uu-intensifica-el-apoyo-logistico-y-medico-en-venezuela-tras-los-recientes-terremotos-63614-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-03T12:55:38.820Z"
+  "lastUpdate": "2026-07-03T15:12:06.338Z"
 };
