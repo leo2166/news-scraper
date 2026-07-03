18d910b20838c135be06e54a730fc0a27564c16f
GitHub Action <actions@github.com>
2026-07-03 12:56:37 +0000

    data: update news and rates at Fri Jul  3 12:56:37 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Viernes, 03 Julio 2026"
     },
     "binance": {
-      "usdt": "775,50"
+      "usdt": "774,69"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,21 +21,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Venezuela informó que mantiene conversaciones con EEUU y el FMI para la reconstrucción de la infraestructura afectada por los terremotos",
-      "link": "https://www.infobae.com/venezuela/2026/07/03/venezuela-informo-que-mantiene-conversaciones-con-eeuu-y-el-fmi-para-la-reconstruccion-de-la-infraestructura-afectada-por-los-terremotos/",
-      "image": "https://www.infobae.com/resizer/v2/3XIEI6ZO5VGD7OHEVOFTSWT5DY.JPG?auth=04513f6fd95ba2c03ef8a55a41716257de3540d3a5338bc15acc0f32c1e9e02f&smart=true&width=350&height=197&quality=85"
+      "title": "El parte médico de Hernán Gil, el sobreviviente del doble terremoto que fue rescatado tras ocho días en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/07/03/el-parte-medico-de-hernan-gil-el-sobreviviente-del-doble-terremoto-que-fue-rescatado-tras-ocho-dias-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/5HR3LESFDBAWZLDEHEQUVXLF3A.jpg?auth=657abfb3ad8d2c90be0d16df3e5aa9001121b9b94bde6a04dc112adaf9ba3078&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
       "title": "Murió un hombre tras desplomarse parte de la estructura en el antiguo Hospital Central de Maracaibo",
       "link": "https://noticialdia.com/al-dia/murio-un-hombre-tras-desplomarse-parte-de-la-estructura-en-el-antiguo-hospital-central-de-maracaibo/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Hospital_Central_Dr._Urquinaona_I.webp?x74468"
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Hospital_Central_Dr._Urquinaona_I-1.webp?x74468"
     },
     {
       "source": "CNN en Español",
       "title": "Irán envía un mensaje desafiante a Trump con el funeral para Jamenei",
       "link": "https://cnnespanol.cnn.com/2026/07/03/mundo/iran-trump-funeral-lider-ali-jamenei-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2283561075-20260701220723765.jpg?c=16x9&q=h_144,w_256,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ls-1-20260703104338173.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Se retrasa el juicio a Maduro en EEUU",
-      "link": "https://diarioversionfinal.com/xvf-portada/vf-home/se-retrasa-el-juicio-a-maduro-en-eeuu/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/image-750x430.jpg"
+      "title": "Explosivo: Manuscrito de Nervis Villalobos vincula a Chávez, Zapatero y la empresa Duro Felguera en adjudicaciones de centrales termoeléctricas",
+      "link": "https://diarioversionfinal.com/xvf-portada/vf-home/explosivo-manuscrito-de-nervis-villalobos-vincula-a-chavez-zapatero-y-la-empresa-duro-felguera-en-adjudicaciones-de-centrales-termoelectricas/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/jose-luis-rodriguez-zapatero-comision-caso-koldo-senado-marzo-2026_163.webp"
     }
   ],
-  "lastUpdate": "2026-07-03T10:35:19.496Z"
+  "lastUpdate": "2026-07-03T12:55:38.820Z"
 };
