1c9c3d1b0df4e99cdaa304fd80ea8b4b95d933f1
GitHub Action <actions@github.com>
2026-07-04 19:14:21 +0000

    data: update news and rates at Sat Jul  4 19:14:21 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 06 Julio 2026"
     },
     "binance": {
-      "usdt": "792,25"
+      "usdt": "789,69"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,19 +21,19 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Testimonio desde el epicentro de los devastadores terremotos en Venezuela: “Las autoridades llegaron a las 72 horas”",
-      "link": "https://www.infobae.com/venezuela/2026/07/04/testimonio-desde-el-epicentro-de-los-devastadores-terremotos-en-venezuela-las-autoridades-llegaron-a-las-72-horas/",
-      "image": "https://www.infobae.com/resizer/v2/OYUY5CFLNNHHJLLVJASLHBLJNA.jpg?auth=3acf0e52815a0b522754103e4eef32e0f285de557d4da50d134ed0f58589eef1&smart=true&width=350&height=197&quality=85"
+      "title": "El gobierno de Venezuela elevó a 2.954 el número de muertos por los devastadores terremotos",
+      "link": "https://www.infobae.com/venezuela/2026/07/04/el-gobierno-de-venezuela-elevo-a-2954-el-numero-de-muertos-por-los-devastadores-terremotos/",
+      "image": "https://www.infobae.com/resizer/v2/OTTWHHDHTZEI5J6YDFIZWFGFCU.JPG?auth=796833d725bb198ca5bd77e0ce26ce8efedebc4848ee6220e20e874233176f19&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Lo delató el ruido: Lo capturaron con una bombona de 43 kilos que acababa de robar en Coro",
-      "link": "https://noticialdia.com/sucesos/lo-delato-el-ruido-lo-capturaron-con-una-bombona-de-43-kilos-que-acababa-de-robar-en-coro/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/%C2%A1Rodaba-una-bombona-hurtada-y-termino-tras-las-rejas-El-ruido-lo-delato-Segundo-arresto-de-l-convertido-a-1024x780-1.webp?x74468"
+      "title": "¡De película! Aparece un \"Batman\" real en México y amarra a presuntos ladrones de motos en plena vía pública",
+      "link": "https://noticialdia.com/sucesos/de-pelicula-aparece-un-batman-real-en-mexico-y-amarra-a-presuntos-ladrones-de-motos-en-plena-via-publica/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Mexico1-1-convertido-a-1024x780-1.webp?x74468"
     },
     {
       "source": "CNN en Español",
-      "title": "Estados Unidos celebra su 250° aniversario",
+      "title": "EE.UU. celebra 250 años de independencia en medio de una peligrosa ola de calor",
       "link": "https://cnnespanol.cnn.com/2026/07/04/eeuu/live-news/estados-unidos-250-aniversario-4-julio-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2229628233.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Venezolanos en EE.UU. piden frenar deportaciones por crisis tras terremotos",
-      "link": "https://diarioversionfinal.com/mundo/venezolanos-en-ee-uu-piden-frenar-deportaciones-por-crisis-tras-terremotos/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/images-2026-07-04T100351.761.jpg"
+      "title": "Irán nombra a Ali Ozmaei como nuevo jefe de la Armada de la Guardia Revolucionaria",
+      "link": "https://diarioversionfinal.com/mundo/iran-nombra-a-ali-ozmaei-como-nuevo-jefe-de-la-armada-de-la-guardia-revolucionaria/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/ca7jms0wwxsi6b6vyg5m6xmf4iukq8r0crigldw8.jpg"
     }
   ],
-  "lastUpdate": "2026-07-04T17:05:38.324Z"
+  "lastUpdate": "2026-07-04T19:13:18.304Z"
 };
