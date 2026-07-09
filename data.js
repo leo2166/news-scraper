0827ed6b4d552f1805e5aeb44da7e3eba24f43a4
GitHub Action <actions@github.com>
2026-07-09 17:56:58 +0000

    data: update news and rates at Thu Jul  9 17:56:58 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Jueves, 09 Julio 2026"
     },
     "binance": {
-      "usdt": "822,18"
+      "usdt": "866,49"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,21 +15,21 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "#Escenarios: La tragedia restará puntos al crecimiento económico, pero no habrá recesión",
-      "link": "https://www.bancaynegocios.com/escenarios-la-tragedia-restara-puntos-al-crecimiento-economico-pero-no-habra-recesion/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/LaGuairaComerciantesEFE.jpg"
+      "title": "Autoridades del FMI y Venezuela hablaron sobre el DEG, la situación económica y las necesidades humanitarias",
+      "link": "https://www.bancaynegocios.com/autoridades-del-fmi-y-venezuela-hablaron-sobre-el-deg-la-situacion-economica-y-las-necesidades-humanitarias-en-venezuela/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2024/10/DolaresDivisasRemesas.jpg"
     },
     {
       "source": "Infobae",
-      "title": "El Foro Penal confirmó la excarcelación del último adolescente que permanecía detenido por motivos políticos en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/09/el-foro-penal-confirmo-la-excarcelacion-del-ultimo-adolescente-que-permanecia-detenido-por-motivos-politicos-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/KKJWYOMX3ZHQDI23ATFZ723CZA.jpg?auth=4bb1f342905ee05e6676be5a612805812985962dd3787d64b3fdfbef7b921af9&smart=true&width=350&height=197&quality=85"
+      "title": "Aportes de la comunidad internacional tras el doble terremoto en Venezuela ascienden a 780 millones de dólares",
+      "link": "https://www.infobae.com/venezuela/2026/07/09/aportes-de-la-comunidad-internacional-tras-el-doble-terremoto-en-venezuela-ascienden-a-780-millones-de-dolares/",
+      "image": "https://www.infobae.com/resizer/v2/RJXOA5USLFCYPP7AAQQHPNE5OI.jpg?auth=f276f29ed10e0fe0cfd1848314d13639e36ba1fcaa2fd915ec6eaad6035748f8&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Intentó huir con una escopeta en el barrio 19 de Abril: Cpbez lo puso tras las rejas",
-      "link": "https://noticialdia.com/sucesos/intento-huir-con-una-escopeta-en-el-barrio-19-de-abril-cpbez-lo-puso-tras-las-rejas/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-convertido-a-577x403-1.webp?x33727"
+      "title": "Violentaron una vivienda para robar dinero en efectivo en el sur del lago",
+      "link": "https://noticialdia.com/sucesos/violentaron-una-vivienda-para-robar-dinero-en-efectivo-en-el-sur-del-lago/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-47.webp?x33727"
     },
     {
       "source": "CNN en Español",
@@ -39,15 +39,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Luke Lindberg y el equipo DART evalúan daños por sismos en La Guaira para optimizar ayuda humanitaria",
-      "link": "https://noticiasvenevision.com/noticias/politica/luke-lindberg-y-el-equipo-dart-evaluan-danos-por-sismos-en-la-guaira-para-optimizar-ayuda-humanitaria",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/saveclip.app_744251252_18400417906095971_1398117552797524122_n-baee84.jpg"
+      "title": "Embajada de EE. UU. en caracas  destacó que Starlink ha desplegado  más de 1.600 kits de conectividad satelital en zonas afectadas  en Venezuela",
+      "link": "https://noticiasvenevision.com/noticias/politica/embajada-de-ee-uu-en-caracas-destaco-que-starlink-ha-desplegado-mas-de-1600-kits-de-conectividad-satelital-en-zonas-afectadas-en-venezuela",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-07-09-at-12.46.01-pm-b87344.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "FORO PENAL REGISTRA 372 PRESOS POLÍTICOS EN VENEZUELA HASTA EL 6 DE JULIO",
-      "link": "https://noticierodigital.com/2026/07/foro-penal-registra-372-presos-politicos-en-venezuela-hasta-el-6-de-julio/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/09/presos-politicos-venezuela-627x376.jpg"
+      "title": "ESCUELAS MUNICIPALES DE BARUTA NO SUFRIERON DAÑOS ESTRUCTURALES",
+      "link": "https://noticierodigital.com/2026/07/escuelas-municipales-de-baruta-no-sufrieron-danos-estructurales/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/Diseno-sin-titulo-6-627x376.png"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Guardia Revolucionaria de Irán asegura que ataques de EE.UU. dificultan la reapertura del estrecho de Ormuz",
-      "link": "https://diarioversionfinal.com/mundo/guardia-revolucionaria-de-iran-asegura-que-ataques-de-ee-uu-dificultan-la-reapertura-del-estrecho-de-ormuz/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/05/Estrecho-Ormuz-03-05-2026-750x430.jpg"
+      "title": "Estados Unidos bombardea 170 objetivos en Irán en solo 48 horas",
+      "link": "https://diarioversionfinal.com/xvf-portada/vf-home/estados-unidos-bombardea-170-objetivos-en-iran-en-solo-48-horas/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/q5SXIOug-image-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-09T15:25:16.958Z"
+  "lastUpdate": "2026-07-09T17:55:55.591Z"
 };
