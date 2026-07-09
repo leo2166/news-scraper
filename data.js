048e546b55077e3a19fb6f97635cf1ba80c56b79
GitHub Action <actions@github.com>
2026-07-09 19:48:50 +0000

    data: update news and rates at Thu Jul  9 19:48:50 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Jueves, 09 Julio 2026"
     },
     "binance": {
-      "usdt": "866,49"
+      "usdt": "869,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,15 +21,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Aportes de la comunidad internacional tras el doble terremoto en Venezuela ascienden a 780 millones de dólares",
-      "link": "https://www.infobae.com/venezuela/2026/07/09/aportes-de-la-comunidad-internacional-tras-el-doble-terremoto-en-venezuela-ascienden-a-780-millones-de-dolares/",
-      "image": "https://www.infobae.com/resizer/v2/RJXOA5USLFCYPP7AAQQHPNE5OI.jpg?auth=f276f29ed10e0fe0cfd1848314d13639e36ba1fcaa2fd915ec6eaad6035748f8&smart=true&width=350&height=197&quality=85"
+      "title": "Venezuela y el FMI comenzaron a negociar el acceso rápido a fondos tras los terremotos",
+      "link": "https://www.infobae.com/venezuela/2026/07/09/venezuela-y-el-fmi-comenzaron-a-negociar-el-acceso-rapido-a-fondos-tras-los-terremotos/",
+      "image": "https://www.infobae.com/resizer/v2/ZRGREEZARVGRPN5Y4QO5JYZE4I.jpg?auth=0c093f67e7c53e9d609cfc64e34aade9f91c51545fcd602932d3d879db1e247f&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Violentaron una vivienda para robar dinero en efectivo en el sur del lago",
-      "link": "https://noticialdia.com/sucesos/violentaron-una-vivienda-para-robar-dinero-en-efectivo-en-el-sur-del-lago/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-47.webp?x33727"
+      "title": "Morgues colapsadas y errores en la identificación de víctimas del vuelo 164",
+      "link": "https://noticialdia.com/sucesos/morgues-colapsadas-y-errores-en-la-identificacion-de-victimas-del-vuelo-164/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/IMG-20260630-WA0025.webp?x33727"
     },
     {
       "source": "CNN en Español",
@@ -39,15 +39,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Embajada de EE. UU. en caracas  destacó que Starlink ha desplegado  más de 1.600 kits de conectividad satelital en zonas afectadas  en Venezuela",
+      "title": "Embajada de EE. UU. en Caracas  destacó que Starlink ha desplegado  más de 1.600 kits de conectividad satelital en zonas afectadas  en Venezuela",
       "link": "https://noticiasvenevision.com/noticias/politica/embajada-de-ee-uu-en-caracas-destaco-que-starlink-ha-desplegado-mas-de-1600-kits-de-conectividad-satelital-en-zonas-afectadas-en-venezuela",
       "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-07-09-at-12.46.01-pm-b87344.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "ESCUELAS MUNICIPALES DE BARUTA NO SUFRIERON DAÑOS ESTRUCTURALES",
-      "link": "https://noticierodigital.com/2026/07/escuelas-municipales-de-baruta-no-sufrieron-danos-estructurales/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/Diseno-sin-titulo-6-627x376.png"
+      "title": "OPS DESCARTÓ RIESGO DE BROTE DE CÓLERA TRAS TERREMOTOS EN VENEZUELA",
+      "link": "https://noticierodigital.com/2026/07/ops-descarto-riesgo-de-brote-de-colera-tras-terremotos-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/06/ninos-terremotos-venezuela-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Estados Unidos bombardea 170 objetivos en Irán en solo 48 horas",
-      "link": "https://diarioversionfinal.com/xvf-portada/vf-home/estados-unidos-bombardea-170-objetivos-en-iran-en-solo-48-horas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/q5SXIOug-image-750x430.jpg"
+      "title": "ING analiza posibles acuerdos de financiación para materias primas de Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/ing-analiza-posibles-acuerdos-de-financiacion-para-materias-primas-de-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/b6a82fb6a50beb729397e6b5145d5214c809801dw-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-09T17:55:55.591Z"
+  "lastUpdate": "2026-07-09T19:47:49.829Z"
 };
