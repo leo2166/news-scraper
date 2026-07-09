970131678cf24da2b18deb08bfccbaadc85681af
GitHub Action <actions@github.com>
2026-07-09 00:10:53 +0000

    data: update news and rates at Thu Jul  9 00:10:53 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Jueves, 09 Julio 2026"
     },
     "binance": {
-      "usdt": "852,68"
+      "usdt": "858,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,27 +15,27 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Bloomberg: Venezuela reduce su brecha cambiaria mientras enfrenta el impacto de los terremotos",
-      "link": "https://www.bancaynegocios.com/bloomberg-venezuela-reduce-su-brecha-cambiaria-mientras-enfrenta-el-impacto-de-los-terremotos/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/bolivares-dolares.webp"
+      "title": "Asocav: Establecen protocolo de emergencia para mitigar impacto económico en agentes aduanales de La Guaira",
+      "link": "https://www.bancaynegocios.com/asocav-establecen-protocolo-de-emergencia-para-mitigar-impacto-economico-en-agentes-aduanales-de-la-guaira/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/PuertoLaGuairaNuevaImagenVenezuela.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Venezuela reclama la liberación de USD 9.000 millones en activos congelados para financiar la reconstrucción tras el doble terremoto",
-      "link": "https://www.infobae.com/venezuela/2026/07/08/venezuela-reclama-la-liberacion-de-usd-9000-millones-en-activos-congelados-para-financiar-la-reconstruccion-tras-el-doble-terremoto/",
-      "image": "https://www.infobae.com/resizer/v2/QKZGVDVNVZHUJN33EYPZVZRCMU.JPG?auth=02348ef48780bd7c0ea4b04c06155de9e554971474e26c23843d6f036b915709&smart=true&width=350&height=197&quality=85"
+      "title": "“Solamente contamos con el pueblo”: refugiados por el terremoto exigen más apoyo del gobierno de Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/07/08/solamente-contamos-con-el-pueblo-refugiados-por-el-terremoto-exigen-mas-apoyo-del-gobierno-de-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/QFAIOSUJA5BNFMGL73KBLCOYS4.JPG?auth=3b430c2f9801d3f840d4a4402cfdd59c8017b565941f3716ec9174ecf4422ebe&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Confirman el fallecimiento del niño argentino Lucas Gámez tras quedar atrapado en los escombros por el doble terremoto",
-      "link": "https://noticialdia.com/principal/confirman-el-fallecimiento-del-nino-argentino-lucas-gamez-tras-quedar-atrapado-en-los-escombros-por-el-doble-terremoto/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/lucas-gamez-el-nino-desaparecido-en-TJ2TJJFPYRHR5G4HRBSNQ64TIY.avif?x16039"
+      "title": "El desgarrador mensaje de fe del papá de Lucas: \"Amen a sus hijos y que crean en Dios\"",
+      "link": "https://noticialdia.com/?p=637382",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/lucas-gamez-el-nino-desaparecido-en-TJ2TJJFPYRHR5G4HRBSNQ64TIY.avif"
     },
     {
       "source": "CNN en Español",
       "title": "EE.UU. vuelve a lanzar ataques aéreos contra Irán",
       "link": "https://cnnespanol.cnn.com/2026/07/07/mundo/live-news/cumbre-otan-trump-presion-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2285229521.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/c-2026-07-08t175151z-96181374-rc2m9ma4zyo9-rtrmadp-3-iran-crisis-usa-strikes-20260709000536467.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "CONFIRMARON LA MUERTE DE LUCAS GÁMEZ TRAS DÍAS DE BÚSQUEDA ENTRE LOS ESCOMBROS",
-      "link": "https://noticierodigital.com/2026/07/confirmaron-la-muerte-de-lucas-gamez-tras-dias-de-busqueda-entre-los-escombros/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/lucas-gamez-venezuela-terremotos-627x376.jpg"
+      "title": "DOS CENTROS COMERCIALES EN CARACAS ESPERAN INFORMES TÉCNICOS PARA RETOMAR OPERACIONES",
+      "link": "https://noticierodigital.com/2026/07/dos-centros-comerciales-en-caracas-esperan-informes-tecnicos-para-retomar-operaciones/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/centros-comerciales-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Estados Unidos destina más de 386 millones de dólares en ayuda humanitaria para Venezuela",
-      "link": "https://diarioversionfinal.com/mundo/estados-unidos-destina-mas-de-386-millones-de-dolares-en-ayuda-humanitaria-para-venezuela/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/d8fda0d2a8262feff6a6c6ce8dc9b2b66eb470bdw-750x375-1.jpg"
+      "title": "EE. UU. reanuda ataques contra Irán tras ruptura del alto al fuego anunciada por Trump",
+      "link": "https://diarioversionfinal.com/mundo/ee-uu-reanuda-ataques-contra-iran-tras-ruptura-del-alto-al-fuego-anunciada-por-trump/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Bombardeos-EEUU-en-Iran-08-07-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-08T22:05:14.775Z"
+  "lastUpdate": "2026-07-09T00:09:51.602Z"
 };
