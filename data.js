c5ac92c10f88ad6f622a85baf4e0ad4a9b9a8ddc
GitHub Action <actions@github.com>
2026-07-10 23:58:30 +0000

    data: update news and rates at Fri Jul 10 23:58:30 UTC 2026

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "709,69",
-      "eur": "811,45",
-      "fechaValor": "Viernes, 10 Julio 2026"
+      "usd": "721,35",
+      "eur": "823,94",
+      "fechaValor": "Lunes, 13 Julio 2026"
     },
     "binance": {
-      "usdt": "863,39"
+      "usdt": "866,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,33 +21,33 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "“Una aguja en un pajar”: un padre busca a su hijo en medio de la desinformación tras los terremotos en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/10/una-aguja-en-un-pajar-un-padre-busca-a-su-hijo-en-medio-de-la-desinformacion-tras-los-terremotos-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/6P2DYY5QUZH4FIPMKVCPUWSQUY.JPG?auth=f4be192ecbb15f17ff34b0506232fe655c9c457b6fe524982d3f14801668dc35&smart=true&width=350&height=197&quality=85"
+      "title": "El total de muertos por el doble terremoto en Venezuela superó los 4.000",
+      "link": "https://www.infobae.com/venezuela/2026/07/10/el-total-de-muertos-por-el-doble-terremoto-en-venezuela-supero-los-4000/",
+      "image": "https://www.infobae.com/resizer/v2/GJF4W63LG5F6TEPYWIVHBLSVSM.jpg?auth=80e113a7b5f5819864c9300615f5b167efd630b61ee8fa25bf13b1a942737ec0&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Detenido por presunta estafa mediante rifas ilícitas en Maracaibo",
-      "link": "https://noticialdia.com/sucesos/detenido-por-presunta-estafa-mediante-rifas-ilicitas-en-maracaibo/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-59.webp?x33727"
+      "title": "Asciende a 4 mil 118 la cifra de fallecidos por el doble terremoto: La cantidad de heridos se mantiene en 16 mil 740",
+      "link": "https://noticialdia.com/principal/asciende-a-4-mil-118-la-cifra-de-fallecidos-por-el-doble-terremoto-la-cantidad-de-heridos-se-mantiene-en-16-mil-740/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/fdfc129a5ee90c30ae89ac10f0446808a51424c4w.webp?x33727"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump da por terminado el alto el fuego, pero acuerda conversar con Irán",
-      "link": "https://cnnespanol.cnn.com/2026/07/10/mundo/live-news/nuevos-ataques-iran-eeuu-vivo-1-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/trump-declaraciones-20260710150524233.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "¿Qué sigue ahora que Trump dio por terminado el alto el fuego con Irán?",
+      "link": "https://cnnespanol.cnn.com/2026/07/10/eeuu/trump-alto-el-fuego-iran-termina-que-sigue-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/screenshot-2026-07-10-150754.png?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Viceministro Oliver blanco agradece apoyo de la embajada de Italia en zonas vulnerables tras terremotos del 24 de junio",
+      "title": "Viceministro Oliver Blanco agradece apoyo de la embajada de Italia en zonas vulnerables tras terremotos del 24 de junio",
       "link": "https://noticiasvenevision.com/noticias/politica/viceministro-oliver-blanco-agradece-apoyo-de-la-embajada-de-italia-en-zonas-vulnerables-tras-terremotos-del-24-de-junio",
       "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/saveclip.app_743157163_18607887697054795_1589657269246551869_n-393727.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "AEROPUERTOS ALTERNATIVOS MANTIENEN LA CONEXIÓN AÉREA DE VENEZUELA CON EL EXTERIOR",
-      "link": "https://noticierodigital.com/2026/07/aeropuertos-alternativos-mantienen-la-conexion-aerea-de-venezuela-con-el-exterior/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/aeropuerto-internacional-arturo-michelena-627x376.jpg"
+      "title": "EXCARCELARON A JACKSON VERA Y JECSON RICARDO CARIEL TRAS CASI CUATRO AÑOS DE PRISIÓN",
+      "link": "https://noticierodigital.com/2026/07/excarcelaron-a-jackson-vera-y-jecson-ricardo-cariel-tras-casi-cuatro-anos-de-prision/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/jackson-vera-jecson-ricardo-excarcelacion-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Tragedia en Almería: incendio forestal deja al menos 11 muertos y decenas de evacuados",
-      "link": "https://diarioversionfinal.com/mundo/tragedia-en-almeria-incendio-forestal-deja-al-menos-11-muertos-y-decenas-de-evacuados/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/7273877bb7dd-fuego-almeria1-750x430.webp"
+      "title": "Lancha de la Armada de EE.UU. llega a La Guaira para apoyar labores humanitarias tras los terremotos",
+      "link": "https://diarioversionfinal.com/mundo/lancha-de-la-armada-de-ee-uu-llega-a-la-guaira-para-apoyar-labores-humanitarias-tras-los-terremotos/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/HM5FvaUXQAAS7Kg-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-10T22:02:32.977Z"
+  "lastUpdate": "2026-07-10T23:57:26.175Z"
 };
