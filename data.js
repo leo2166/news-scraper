23d5a8789eb4e9cce6a2f237825ebccfc5679857
GitHub Action <actions@github.com>
2026-07-13 20:16:50 +0000

    data: update news and rates at Mon Jul 13 20:16:50 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 13 Julio 2026"
     },
     "binance": {
-      "usdt": "872,00"
+      "usdt": "875,39"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,15 +21,15 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "Una máquina por 11.500 dólares: familias alquilan equipos para rescates por el terremoto en Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/13/una-maquina-por-11500-dolares-familias-alquilan-equipos-para-rescates-por-el-terremoto-en-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/7WUMH65MGBAI5AIMPI3PWY4FAU.JPG?auth=74cfe40a5c64e97dcd1c0bcfd85eb2b35b9aa2433bed5df2f0e83cfb69e2654f&smart=true&width=350&height=197&quality=85"
+      "title": "Nuevo balance del doble terremoto en Venezuela: la cifra de muertos aumentó a 4.561",
+      "link": "https://www.infobae.com/venezuela/2026/07/13/nuevo-balance-del-doble-terremoto-en-venezuela-la-cifra-de-muertos-aumento-a-4561/",
+      "image": "https://www.infobae.com/resizer/v2/B2HV2ZODPFE3TGQ6FTPXBXXKG4.JPG?auth=40970faa887a220f408b0dd72fa88f0c1d82de3a622fd0a79b305e39ecbf8429&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Lo capturaron con más de 100 gramos de marihuana en Maracaibo: Intentó huir del Cpbez",
-      "link": "https://noticialdia.com/sucesos/capturaron-con-mas-de-100-gramos-de-marihuana-en-maracaibo-intento-huir-del-cpbez/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-85.webp?x33727"
+      "title": "Capturado alias “El Loco Loco” tras mantener como rehén a un niño de 5 años en el municipio Santa Rita",
+      "link": "https://noticialdia.com/sucesos/capturado-alias-el-loco-loco-tras-mantener-como-rehen-a-un-nino-de-5-anos-en-el-municipio-santa-rita/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Captura-88-768x651.webp?x33727"
     },
     {
       "source": "CNN en Español",
@@ -45,22 +45,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "STALIN GONZÁLEZ PIDE UNA «LEY DE EMERGENCIA» PARA RECONSTRUIR EL PAÍS TRAS LOS TERREMOTOS DEL 24J",
-      "link": "https://noticierodigital.com/2026/07/stalin-gonzalez-pide-una-ley-de-emergencia-para-reconstruir-el-pais-tras-los-terremotos-del-24j/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/02/stalin-gonzalez-627x376.jpg"
+      "title": "BROTE DE LEGIONELA EN NUEVA YORK DEJA 59 AFECTADOS Y 15 HOSPITALIZADOS",
+      "link": "https://noticierodigital.com/2026/07/brote-de-legionela-en-nueva-york-deja-59-afectados-y-15-hospitalizados/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/broteManhattan-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Aparece un volcán de lodo frente a Trinidad tras los sismos en Venezuela",
-      "link": "https://laverdad.com/aparece-un-volcan-de-lodo-frente-a-trinidad-tras-los-sismos-en-venezuela/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/07/425346736028a789e96a664d2536b44aa358645e-scaled.jpg"
+      "title": "Edad para jubilarse en México: requisitos según tu régimen de pensión",
+      "link": "https://laverdad.com/edad-para-jubilarse-en-mexico-requisitos-segun-tu-regimen-de-pension/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/07/Edad-para-jubilarse-en-Mexico-requisitos-segun-tu-regimen-de-pension.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "EE.UU. anuncia bloqueo naval contra puertos y terminales petroleras de Irán",
-      "link": "https://diarioversionfinal.com/mundo/ee-uu-anuncia-bloqueo-naval-contra-puertos-y-terminales-petroleras-de-iran/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/images-2026-07-13T144111.626.jpg"
+      "title": "Administración Trump reactiva antigua prisión en Colorado como centro de detención para 1.200 migrantes",
+      "link": "https://diarioversionfinal.com/mundo/administracion-trump-reactiva-antigua-prision-en-colorado-como-centro-de-detencion-para-1-200-migrantes/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2024/11/carcel13-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-13T18:43:44.117Z"
+  "lastUpdate": "2026-07-13T20:14:43.224Z"
 };
