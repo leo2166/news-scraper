664d03c78fc804da1a7120fcfaa90528b3ea0cae
GitHub Action <actions@github.com>
2026-07-20 06:46:27 +0000

    data: update news and rates at Mon Jul 20 06:46:27 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 20 Julio 2026"
     },
     "binance": {
-      "usdt": "894,00"
+      "usdt": "854,36"
     },
     "kontigo": {
       "usd": "N/A"
@@ -33,9 +33,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "CNN en Español",
-      "title": "España vence a una Argentina épica y gana el Mundial 2026",
-      "link": "https://cnnespanol.cnn.com/2026/07/19/deportes/live-news/espana-argentina-final-mundial-2026-en-vivo-resultado-goles-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2286813887.jpg?c=16x9&q=h_144,w_256,c_fill"
+      "title": "España ganó el Mundial fiel a su estilo",
+      "link": "https://cnnespanol.cnn.com/2026/07/19/deportes/espana-campeon-mundial-2026-fifa-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2286813887.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Trump pide incluir a Irán en sanciones contra Rusia",
-      "link": "https://diarioversionfinal.com/mundo/trump-pide-incluir-a-iran-en-sanciones-contra-rusia/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/UQNFtE0u-image-750x430.jpg"
+      "title": "Tragedia familiar en España en plena fiesta mundialista: Muere un niño en Salamanca tras el colapso de una fuente donde celebraban",
+      "link": "https://diarioversionfinal.com/mundo/tragedia-familiar-en-espana-en-plena-fiesta-mundialista-muere-un-nino-en-salamanca-tras-el-colapso-de-una-fuente-donde-celebraban/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/tragedia_salamaca_mundial-680x430.webp"
     }
   ],
-  "lastUpdate": "2026-07-20T03:33:15.993Z"
+  "lastUpdate": "2026-07-20T06:45:21.525Z"
 };
