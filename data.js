2da0ac92bfa7b8fde56ff15f3726d316ec7a77cb
GitHub Action <actions@github.com>
2026-07-21 13:50:45 +0000

    data: update news and rates at Tue Jul 21 13:50:45 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 21 Julio 2026"
     },
     "binance": {
-      "usdt": "907,66"
+      "usdt": "N/A"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,21 +21,21 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "El gobierno de Venezuela ascendió a 5.278 muertos el balance de víctimas por el doble terremoto",
-      "link": "https://www.infobae.com/venezuela/2026/07/21/el-gobierno-de-venezuela-ascendio-a-5278-muertos-el-balance-de-victimas-por-el-doble-terremoto/",
-      "image": "https://www.infobae.com/resizer/v2/NPML2UGF7FD3BMU6HGYLWOLYW4.JPG?auth=4cab92fbde7aba85d3dbac3ec0a35bbf9dca36e0f2e3f4f0ac2f42b1d3fb725a&smart=true&width=350&height=197&quality=85"
+      "title": "La opositora Dinorah Figuera dijo que se convocará a todos los sectores al diálogo con el chavismo",
+      "link": "https://www.infobae.com/venezuela/2026/07/21/la-opositora-dinorah-figuera-dijo-que-se-convocara-a-todos-los-sectores-al-dialogo-con-el-chavismo/",
+      "image": "https://www.infobae.com/resizer/v2/H7AWCLAKFM27JU6KGLVBWTNB6A.jpg?auth=a1cc63edc3297b73c91dd50cbb9d5d89627376a57161f4686e994613750b7afe&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Nuevo balance oficial: Asciende a 5 mil 278 la cifra de fallecidos por el doble terremoto",
-      "link": "https://noticialdia.com/principal/nuevo-balance-oficial-asciende-a-5-mil-278-la-cifra-de-fallecidos-por-el-doble-terremoto/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/6b3dee51db5d146b186216497ef6178cad2a0dd0miniw-e1784601786464.webp?x33727"
+      "title": "Detenidos tres sospechosos vinculados a la muerte de la \"Bruja Rosmary\" en el Sur del Lago",
+      "link": "https://noticialdia.com/sucesos/detenidos-tres-sospechosos-vinculados-a-la-muerte-de-la-bruja-rosmary-en-el-sur-del-lago/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-2-1.webp?x33727"
     },
     {
       "source": "CNN en Español",
       "title": "Casi 100 militares de EE.UU. han resultado heridos en las últimas dos semanas",
       "link": "https://cnnespanol.cnn.com/2026/07/21/mundo/live-news/guerra-ee-uu-israel-iran-en-vivo-ataques-noticias-3-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/buques-guerra-ee-uu-estrecho-ormuz.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/estrecho-bab-el-mandeb-es.jpg?c=2x3&q=h_384,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -45,22 +45,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "PROVEA EXIGIÓ A DELCY RODRÍGUEZ DESBLOQUEAR AL MENOS 92 MEDIOS DIGITALES EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/07/provea-exigio-a-delcy-rodriguez-desbloquear-al-menos-92-medios-digitales-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/bloqueo-de-medios-digitales-627x376.jpg"
+      "title": "DANIEL ORTEGA DICE QUE NO VOLVERÁ A HABER ELECCIONES EN NICARAGUA ",
+      "link": "https://noticierodigital.com/2026/07/daniel-ortega-dice-que-no-volvera-a-haber-elecciones-en-nicaragua/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2021/11/daniel-ortega-elecciones-7nov2021.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Ortega descarta la alternancia democrática en Nicaragua: “Aquí no volverá a haber elecciones”",
-      "link": "https://laverdad.com/ortega-descarta-la-alternancia-democratica-en-nicaragua-aqui-no-volvera-a-haber-elecciones/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/07/Ortega-descarta-la-alternancia-democratica-en-Nicaragua_-Aqui-no-volvera-a-haber-elecciones.jpg"
+      "title": "Senador de EE. UU. propone presidenciales venezolanas para el 24 de julio de 2027",
+      "link": "https://laverdad.com/senador-de-ee-uu-propone-presidenciales-venezolanas-para-el-24-de-julio-de-2027/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/07/Senador-de-EE.-UU.-propone-realizar-las-presidenciales-venezolanas-el-24-de-julio-de-2027.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Suman 27 los muertos por el naufragio de un ferry en Guyana",
-      "link": "https://diarioversionfinal.com/mundo/suman-27-los-muertos-por-el-naufragio-de-un-ferry-en-guyana/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Ferry-Guyana-20-07-2026-750x430.jpg"
+      "title": "Papa León XIV advierte que las armas atómicas “no generan paz” y pide desarme nuclear",
+      "link": "https://diarioversionfinal.com/mundo/papa-leon-xiv-advierte-que-las-armas-atomicas-no-generan-paz-y-pide-desarme-nuclear/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/06/Leon-XIV.jpg"
     }
   ],
-  "lastUpdate": "2026-07-21T11:08:13.755Z"
+  "lastUpdate": "2026-07-21T13:49:43.477Z"
 };
