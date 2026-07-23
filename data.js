1ce236a2fb6b867e8e42e1e2bb972d4306ad9ee6
GitHub Action <actions@github.com>
2026-07-23 16:00:07 +0000

    data: update news and rates at Thu Jul 23 16:00:07 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Jueves, 23 Julio 2026"
     },
     "binance": {
-      "usdt": "906,66"
+      "usdt": "N/A"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,39 +15,39 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Fedeagro: Cosechas de maíz y arroz enfrentan distorsiones estructurales que comprometen rentabilidad del campo",
-      "link": "https://www.bancaynegocios.com/fedeagro-cosechas-de-maiz-y-arroz-enfrentan-distorsiones-estructurales-que-comprometen-rentabilidad-del-campo/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2022/05/Maiz-Venezuela-e1655133230123.jpg"
+      "title": "Banco Mundial: Daños de infraestructura por terremotos en Venezuela podrían costar casi US$ 20.000 millones",
+      "link": "https://www.bancaynegocios.com/banco-mundial-danos-de-infraestructura-por-terremotos-en-venezuela-podrian-costar-casi-us-20000-millones/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/NuevaDivisasDolares.jpg"
     },
     {
       "source": "Infobae",
-      "title": "La nueva camada de ascensos a coroneles en la Guardia Nacional y sus conexiones de poder",
-      "link": "https://www.infobae.com/venezuela/2026/07/23/la-nueva-camada-de-ascensos-a-coroneles-en-la-guardia-nacional-y-sus-conexiones-de-poder/",
-      "image": "https://www.infobae.com/resizer/v2/VVS2SXFNP5HPRDJGZV6LLXOVFA.jpg?auth=64b28b0d69cced0d74eab65324a2ea7e2dbc9b1a9ccfb1fc63257b214bdd9581&smart=true&width=350&height=197&quality=85"
+      "title": "Un mes después del doble terremoto en Venezuela, solo el 39% de la respuesta humanitaria cuenta con financiación",
+      "link": "https://www.infobae.com/venezuela/2026/07/23/un-mes-despues-del-doble-terremoto-en-venezuela-solo-el-39-de-la-respuesta-humanitaria-cuenta-con-financiacion/",
+      "image": "https://www.infobae.com/resizer/v2/4DHJGCVGRBFTVCGSLIDWQ3ZG5Y.JPG?auth=53dd4213e5726000397f3f17eff2815e2ff6a0aecd0af0aded741e5e883ccdcb&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Cpbez saca de circulación dos motos con escapes modificados por contaminación sónica en Maracaibo y JEL",
-      "link": "https://noticialdia.com/sucesos/cpbez-saca-de-circulacion-dos-motos-con-escapes-modificados-por-contaminacion-sonica-en-maracaibo-y-jel/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-20.webp?x95844"
+      "title": "Polimaracaibo detiene a una mujer por agredir a su madre de 80 años en Manuel Dagnino",
+      "link": "https://noticialdia.com/al-dia/polimaracaibo-detiene-a-una-mujer-por-agredir-a-su-madre-de-80-anos-en-manuel-dagnino/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Diseno-sin-titulo-19-6.webp?x95844"
     },
     {
       "source": "CNN en Español",
       "title": "Trump revive tribunal secreto para deportar a presuntos \"terroristas extranjeros\"",
       "link": "https://cnnespanol.cnn.com/2026/07/23/eeuu/trump-deportacion-terroristas-extranjeros-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/trump-deportaciones-20260723131601010.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2285361563.jpg?c=16x9&q=h_144,w_256,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Donald Trump resaltó el envío de crudo venezolano hacia las refinerías de Houston y Luisiana",
-      "link": "https://noticiasvenevision.com/noticias/politica/donald-trump-resalto-el-envio-de-crudo-venezolano-hacia-las-refinerias-de-houston-y-luisiana",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/president-donald-trump-state-of-the-union-2026-ap-022526-edcc54.jpg"
+      "title": "Marco Rubio: \"La reconciliación en Venezuela debe incluir a todos los sectores de la sociedad\"",
+      "link": "https://noticiasvenevision.com/noticias/politica/marco-rubio-la-reconciliacion-en-venezuela-debe-incluir-a-todos-los-sectores-de-la-sociedad",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/descarga-8519c6.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EE. UU. AUMENTA COMPRAS DE CRUDO VENEZOLANO POR FUTURA IMPOSIBILIDAD DE CANADÁ",
-      "link": "https://noticierodigital.com/2026/07/ee-uu-aumenta-compras-de-crudo-venezolano-por-futura-imposibilidad-de-canada/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/03/petroleoTrump-627x376.jpg"
+      "title": "REPSOL SE PREPARA PARA RECIBIR CUATRO BARCOS CON CRUDO DE VENEZUELA HASTA FINAL DE AÑO",
+      "link": "https://noticierodigital.com/2026/07/repsol-se-prepara-para-recibir-cuatro-barcos-con-crudo-de-venezuela-hasta-final-de-ano/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2024/05/repsol.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Trump amenaza a Irán con castigo militar si continúan ataques hutíes contra buques",
-      "link": "https://diarioversionfinal.com/mundo/trump-amenaza-a-iran-con-castigo-militar-si-continuan-ataques-huties-contra-buques/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Trump-OTAN.jpg"
+      "title": "Banco Mundial estima daños por terremotos en Venezuela en unos 19 mil 600 millones de dólares",
+      "link": "https://diarioversionfinal.com/mundo/banco-mundial-estima-danos-por-terremotos-en-venezuela-en-unos-19-mil-600-millones-de-dolares/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/fdfb7dbaabb198f948fc59ec2845464634a2763ew-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-07-23T13:59:14.257Z"
+  "lastUpdate": "2026-07-23T15:59:04.423Z"
 };
