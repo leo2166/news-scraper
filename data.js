f661a66efb897a4d6ca6b6efc9c4e30f5414ecc0
GitHub Action <actions@github.com>
2026-07-27 16:58:00 +0000

    data: update news and rates at Mon Jul 27 16:58:00 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 27 Julio 2026"
     },
     "binance": {
-      "usdt": "859,00"
+      "usdt": "860,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,39 +15,39 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Delcy Rodríguez: Gobierno trabaja en \"plan maestro\" de reconstrucción de zonas afectadas",
-      "link": "https://www.bancaynegocios.com/delcy-rodriguez-gobierno-trabaja-en-plan-maestro-de-reconstruccion-de-zonas-afectadas/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/DELCY-RODRIGUEZ-PLAN-MAESTRO.jpg"
+      "title": "Dos de cada tres venezolanos creen que se puede mejorar la economía y reconstruir La Guaira tras los terremotos",
+      "link": "https://www.bancaynegocios.com/dos-de-cada-tres-venezolanos-creen-que-se-puede-mejorar-la-economia-y-reconstruir-la-guaira-tras-los-terremotos/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/EscombrosEdificiosLaGuairaTerremotosVenezuela.jpg"
     },
     {
       "source": "Infobae",
-      "title": "María Corina Machado dijo que no participará en los diálogos con el chavismo pero aclaró que no será un obstáculo",
-      "link": "https://www.infobae.com/venezuela/2026/07/26/maria-corina-machado-dijo-que-no-participara-en-los-dialogos-con-el-chavismo-pero-aclaro-que-no-sera-un-obstaculo/",
-      "image": "https://www.infobae.com/resizer/v2/TANHDDLS6BGFFBLITCHU3PJL7U.jpg?auth=626645c6940aa30cd6d3ea22d3f77856db5b30b0ddcbab6f2bba2fd58dd86519&smart=true&width=350&height=197&quality=85"
+      "title": "Jefe del PSUV a radicales chavistas: “No hay otro camino” que aceptar la propuesta de Estados Unidos",
+      "link": "https://www.infobae.com/venezuela/2026/07/27/jefe-del-psuv-a-radicales-chavistas-no-hay-otro-camino-que-aceptar-la-propuesta-de-estados-unidos/",
+      "image": "https://www.infobae.com/resizer/v2/XJHSN344WZCCPOPVM62JB4GBTI.JPG?auth=f8fcfcf8c6686b29810446ddd393e82c431b899cede45aebaa518d08f108bf9f&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Fallece mujer de 75 años que fue herida por su hija con un chopo",
-      "link": "https://noticialdia.com/sucesos/fallece-mujer-de-75-anos-que-fue-herida-por-su-hija-con-un-chopo/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-52.webp?x95844"
+      "title": "Tenia 5 años abusando de su sobrina de 14: Preso en Mara",
+      "link": "https://noticialdia.com/al-dia/tenia-cinco-anos-abusando-de-su-sobrina-de-14-anos-preso-en-mara/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-54.webp?x95844"
     },
     {
       "source": "CNN en Español",
-      "title": "La lucha por mantener el TPS tras el fallo de la Corte Suprema a favor de Trump",
-      "link": "https://cnnespanol.cnn.com/2026/07/27/eeuu/luchas-mantener-tps-corte-suprema-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/protect-tps-justice.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "Cómo las diásporas se volvieron clave en elecciones latinoamericanas",
+      "link": "https://cnnespanol.cnn.com/2026/07/27/latinoamerica/elecciones-votantes-extranjero-diaspora-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2282083496.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Departamento de Estado de EE.UU. reitera apoyo a una transición y estabilidad en Venezuela",
-      "link": "https://noticiasvenevision.com/noticias/politica/departamento-de-estado-de-eeuu-reitera-apoyo-a-una-transicion-y-estabilidad-en-venezuela",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/images-3-9c0425.jpg"
+      "title": "Un Nuevo Tiempo propone relanzar la alianza democrática para avanzar hacia la reconstrucción institucional",
+      "link": "https://noticiasvenevision.com/noticias/politica/un-nuevo-tiempo-propone-relanzar-la-alianza-democratica-para-avanzar-hacia-la-reconstruccion-institucional",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/avatars-000323531413-5wkxaa-t1080x1080-625f79-efbbd9.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "PDVSA DEMANDA A PETROJAM POR EXPROPIARLE EL 49% DE SU PARTICIPACIÓN EN UNA REFINERÍA",
-      "link": "https://noticierodigital.com/2026/07/pdvsa-demanda-a-petrojam-por-expropiarle-el-49-de-su-participacion-en-una-refineria/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/01/pdvsa-venezuela-627x376.jpg"
+      "title": "DIÁLOGO DEL 1 DE AGOSTO ES UNA CONSECUENCIA DIRECTA DEL 3 DE ENERO, DIJO DIPUTADO ALGARRA",
+      "link": "https://noticierodigital.com/2026/07/dialogo-del-1-de-agosto-es-una-consecuencia-directa-del-3-de-enero-dijo-diputado-algarra/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/diputadoAlgarra-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "La esposa de Pedro Sánchez irá a juicio con una petición de 13 años de cárcel",
-      "link": "https://diarioversionfinal.com/mundo/la-esposa-de-pedro-sanchez-ira-a-juicio-con-una-peticion-de-13-anos-de-carcel/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/b8ec9750-02d4-11ef-a99b-6969aee8f40b.jpg.webp"
+      "title": "Tiroteo en festival de Seattle deja \"múltiples víctimas\"",
+      "link": "https://diarioversionfinal.com/mundo/tiroteo-en-festival-de-seattle-deja-multiples-victimas/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/e2a2f4e16e80b41c11f63c12c0ac7da69e418fc4m-1-1068x601-1-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-27T14:33:36.990Z"
+  "lastUpdate": "2026-07-27T16:56:58.774Z"
 };
