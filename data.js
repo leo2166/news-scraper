ab212e4540ff33b98433846f78b84513de1edf63
GitHub Action <actions@github.com>
2026-07-30 16:26:55 +0000

    data: update news and rates at Thu Jul 30 16:26:55 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Jueves, 30 Julio 2026"
     },
     "binance": {
-      "usdt": "852,10"
+      "usdt": "848,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,52 +15,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "#Datos: Clasificación de activos públicos anticipa oportunidades para el sector privado",
-      "link": "https://www.bancaynegocios.com/datos-clasificacion-de-activos-publicos-anticipa-oportunidades-para-el-sector-privado/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2021/01/Sidor-hojalata-1100x572-1.jpg"
+      "title": "ABC: Congreso de EEUU investiga destino del ingreso petrolero venezolano controlado por Trump",
+      "link": "https://www.bancaynegocios.com/abc-congreso-de-eeuu-investiga-destino-del-ingreso-petrolero-venezolano-controlado-por-trump/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2018/07/congreso-de-eeuu-e1696102102403.jpg"
     },
     {
       "source": "Infobae",
-      "title": "El chavismo se fractura entre denuncias de traición y rechazo al tutelaje extranjero",
-      "link": "https://www.infobae.com/venezuela/2026/07/30/el-chavismo-se-fractura-entre-denuncias-de-traicion-y-rechazo-al-tutelaje-extranjero/",
-      "image": "https://www.infobae.com/resizer/v2/Z6T7P476Y5G5NBZKR6CBGPKLKE.jpg?auth=afe78880f0ba5b393d66f1421d369becd1011f75e094fa6ed5680c18df6f9298&smart=true&width=350&height=197&quality=85"
+      "title": "Proponen crear una ONG para asumir la reconstrucción de zonas afectadas por los terremotos en Venezuela",
+      "link": "https://www.infobae.com/venezuela/2026/07/30/proponen-crear-una-ong-para-asumir-la-reconstruccion-de-zonas-afectadas-por-los-terremotos-en-venezuela/",
+      "image": "https://www.infobae.com/resizer/v2/AEI7M3ID45EA7PU5ETR6OWWXJI.JPG?auth=bfedbaa80e0b3ab351ff6f394272d3e7f10fc828dba053fddfcdc3913870af91&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Detenido por asesinar a detective del Cicpc en el estado Miranda",
-      "link": "https://noticialdia.com/al-dia/detenido-por-asesinar-a-detective-del-cicpc-en-el-estado-miranda/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-61.webp?x35181"
+      "title": "Capturado sujeto solicitado por homicidio y usurpación de identidad en el Puente sobre el Lago",
+      "link": "https://noticialdia.com/al-dia/capturado-sujeto-solicitado-por-homicidio-y-usurpacion-de-identidad-en-el-puente-sobre-el-lago/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/Plantilla-kelly-65.webp?x35181"
     },
     {
       "source": "CNN en Español",
-      "title": "Egipto fue atacado en una guerra que cada vez involucra a más países",
-      "link": "https://cnnespanol.cnn.com/2026/07/30/mundo/live-news/guerra-israel-iran-estados-unidos-estrecho-de-ormuz-3-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/egipto-ataque.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "El “Súper Niño” ya está aquí: este es el impacto que espera América Latina",
+      "link": "https://cnnespanol.cnn.com/2026/07/30/latinoamerica/super-nino-paises-medidas-clima-extremo-calor-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/c62987a2eff97a9f6052c9daa849666805a5623ew.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Presidenta (E) Delcy Rodríguez se reúne con el FMI para analizar las perspectivas económicas de Venezuela",
-      "link": "https://noticiasvenevision.com/noticias/politica/presidenta-e-delcy-rodriguez-se-reune-con-el-fmi-para-analizar-las-perspectivas-economicas-de-venezuela",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/img_6873-2048x1365-bb3123.jpg"
+      "title": "Tomás Guanipa aboga por garantías electorales y fin de inhabilitaciones políticas ante nueva fase de negociación",
+      "link": "https://noticiasvenevision.com/noticias/politica/tomas-guanipa-aboga-por-garantias-electorales-y-fin-de-inhabilitaciones-politicas-ante-nueva-fase-de-negociacion",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/img_9939.jpg-7879fd.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "SUBIÓ A 30 LA CIFRA DE FALLECIDOS TRAS SISMO EN JAPÓN",
-      "link": "https://noticierodigital.com/2026/07/subio-a-30-la-cifra-de-fallecidos-tras-sismo-en-japon/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/sismoJapon-627x376.jpg"
+      "title": "ALCALDE DE CHACAO REPORTÓ QUE INCENDIO EN LOCAL DEL CCCT «YA FUE CONTROLADO»",
+      "link": "https://noticierodigital.com/2026/07/alcalde-de-chacao-reporto-que-incendio-en-local-del-ccct-ya-fue-controlado/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/Diseno-sin-titulo-1-10-627x376.png"
     },
     {
       "source": "La Verdad",
-      "title": "Ordenan captura de Evo Morales por las protestas de mayo y junio en Bolivia",
-      "link": "https://laverdad.com/ordenan-captura-de-evo-morales-por-las-protestas-de-mayo-y-junio-en-bolivia/",
-      "image": "https://laverdad.com/wp-content/uploads/2025/01/merlin_164864307_b2a9b7af-2fea-4b71-8060-2d5bea5031d6-superJumbo.jpg"
+      "title": "Aumenta a 34 la cifra de muertos por el terremoto de magnitud 7,1 Japón",
+      "link": "https://laverdad.com/aumenta-a-34-la-cifra-de-muertos-por-el-terremoto-de-magnitud-71-japon/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/07/Ascienden-a-34-los-muertos-por-el-terremoto-en-Japon-segun-las-autoridades-locales-scaled.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Congreso de EE. UU. investiga destino de más de 13.000 millones de dólares del petróleo venezolano",
-      "link": "https://diarioversionfinal.com/mundo/congreso-de-ee-uu-investiga-destino-de-mas-de-13-000-millones-de-dolares-del-petroleo-venezolano/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Congreso-EEUU-750x430.jpg"
+      "title": "Desarrollan en Singapur una nueva vacuna contra la cepa Bundibugyo del ébola",
+      "link": "https://diarioversionfinal.com/mundo/desarrollan-en-singapur-una-nueva-vacuna-contra-la-cepa-bundibugyo-del-ebola/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/12/ebola_interno-dc5d98-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-07-30T14:00:57.462Z"
+  "lastUpdate": "2026-07-30T16:25:55.453Z"
 };
