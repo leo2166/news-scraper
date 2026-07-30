fd775c4d8afae72d043552687c32b41a59a0a10d
GitHub Action <actions@github.com>
2026-07-30 00:00:29 +0000

    data: update news and rates at Thu Jul 30 00:00:29 UTC 2026

--- data.js
+++ data.js
@@ -15,39 +15,39 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Pagan \"Bono de Corresponsabilidad y Formación\" de julio a trabajadores de nómina especial: equivale a US$119,99",
-      "link": "https://www.bancaynegocios.com/pagan-bono-de-corresponsabilidad-y-formacion-de-julio-a-trabajadores-de-nomina-especial-equivale-a-us119punto99/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/bolivares-dolares.webp"
+      "title": "Inician pago del Bono \"Cuadrantes de Paz\" de julio: Equivale a US$74,83",
+      "link": "https://www.bancaynegocios.com/inician-pago-del-bono-cuadrantes-de-paz-de-julio-equivale-a-us-74punto83/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/bolivares-e1745959276971.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Exministro de Chávez afirma que corrupción e ineficiencia destruyeron la red sismológica de Venezuela",
-      "link": "https://www.infobae.com/venezuela/2026/07/29/exministro-de-chavez-afirma-que-corrupcion-e-ineficiencia-destruyeron-la-red-sismologica-de-venezuela/",
-      "image": "https://www.infobae.com/resizer/v2/LGIICVN6VZDBBIW3IPLNLV2D5M.jpg?auth=189e785eced4a1871acef8bf885b5fec78d84ed9f7f6b7fe1a2ef0e9a61b9ca8&smart=true&width=350&height=197&quality=85"
+      "title": "Venezuela enfrenta riesgos sanitarios y ambientales un mes después de los terremotos que dejaron más de 5.000 muertos",
+      "link": "https://www.infobae.com/venezuela/2026/07/29/venezuela-enfrenta-riesgos-sanitarios-y-ambientales-un-mes-despues-de-los-terremotos-que-dejaron-mas-de-5000-muertos/",
+      "image": "https://www.infobae.com/resizer/v2/3Z336QVQIFCTHJSO276VYHC2ZU.jpg?auth=f4c43f670a23f9fbb9fdfdc85a3e90a644dffda249d6f6e082fbaa99b0f222e4&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Agresor de la empresaria en Bogotá dio su versión de los hechos: Alega \"estrés extremo\" por abusos laborales",
-      "link": "https://noticialdia.com/viral/agresor-de-la-empresaria-en-bogota-dio-su-version-de-los-hechos-alega-estres-extremo-por-abusos-laborales/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/v-efe-3.webp?x95844"
+      "title": "Arrestaron a un hombre solicitado por deserción militar en la urbanización La Coromoto",
+      "link": "https://noticialdia.com/sucesos/arrestaron-a-un-hombre-solicitado-por-desercion-militar-en-la-urbanizacion-la-coromoto/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/07/v-efe-4-2.webp?x95844"
     },
     {
       "source": "CNN en Español",
-      "title": "Los \"nepobabies\" llegan al poder en América Latina",
-      "link": "https://cnnespanol.cnn.com/2026/07/29/latinoamerica/presidentes-nepobabies-herederos-apellidos-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/20260729-nepobabies-latam-es.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "Por qué Irán reanudó los ataques tras la pausa de EE.UU.",
+      "link": "https://cnnespanol.cnn.com/2026/07/29/mundo/iran-ataques-pausa-eeuu-riesgos-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/screenshot-2026-07-29-120622.png?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Foro Penal confirma  la liberación del coronel del Ejército José Jesús Lozano Mogollón",
-      "link": "https://noticiasvenevision.com/noticias/politica/foro-penal-confirma-la-liberacion-del-coronel-del-ejercito-jose-jesus-lozano-mogollon",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/copia-de-no-borrar-plantilas-5-3dfe50.jpg"
+      "title": "Venezuela lanza un plan con la ONU para evaluar necesidades de recuperación tras sismos",
+      "link": "https://noticiasvenevision.com/noticias/politica/venezuela-lanza-un-plan-con-la-onu-para-evaluar-necesidades-de-recuperacion-tras-sismos",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/delcy-y-rampolla-22-07-2026-scaled-7567db.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "FORO PENAL CONFIRMÓ LA EXCARCELACIÓN DEL CORONEL JOSÉ JESÚS LOZANO MOGOLLÓN",
-      "link": "https://noticierodigital.com/2026/07/foro-penal-confirmo-la-excarcelacion-del-coronel-jose-jesus-lozano-mogollon/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/Jose-Jesus-Lozano-Mogollon-627x376.jpg"
+      "title": "GOBIERNO INSPECCIONÓ LAS OBRAS DE RECUPERACIÓN DEL AEROPUERTO DE MAIQUETÍA",
+      "link": "https://noticierodigital.com/2026/07/gobierno-inspecciono-las-obras-de-recuperacion-del-aeropuerto-de-maiquetia/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/torre-de-control-maiquetia-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Trump pide al Congreso de EE. UU. incluir aranceles a Irán además de las sanciones",
-      "link": "https://diarioversionfinal.com/mundo/trump-pide-al-congreso-de-ee-uu-incluir-aranceles-a-iran-ademas-de-las-sanciones/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Trump-13-07-2026-750x430.webp"
+      "title": "Las tropas israelíes bombardean el sur del Líbano e incendian viviendas",
+      "link": "https://diarioversionfinal.com/mundo/las-tropas-israelies-bombardean-el-sur-del-libano-e-incendian-viviendas/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/Bombardeos-Libano-29-07-2026-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-07-29T22:01:07.432Z"
+  "lastUpdate": "2026-07-29T23:59:27.771Z"
 };
