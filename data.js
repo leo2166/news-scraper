d69fbb51bed5d0f5c68f97fe12fc639991554ba7
GitHub Action <actions@github.com>
2026-08-01 15:01:10 +0000

    data: update news and rates at Sat Aug  1 15:01:10 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 03 Agosto 2026"
     },
     "binance": {
-      "usdt": "848,50"
+      "usdt": "849,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,9 +15,9 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "#Venezuela 2.0: Un plan maestro para reestructurar la deuda y reconstruir la economía",
-      "link": "https://www.bancaynegocios.com/venezuela-2-punto-0-un-plan-maestro-para-reestructurar-la-deuda-y-reconstruir-la-economia/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2017/09/Universidad-de-Harvard-e1524512265852.jpg"
+      "title": "#Dato: El bolívar se depreció un 15% frente al dólar en julio",
+      "link": "https://www.bancaynegocios.com/dato-el-bolivar-se-deprecio-un-15-porciento-frente-al-dolar-en-julio/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2024/06/BolivaresDolaresCambio.jpg"
     },
     {
       "source": "Infobae",
@@ -27,9 +27,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticia al Día",
-      "title": "Al menos 10 heridos dejó explosión de carro bomba frente a una comisaría en región fronteriza con Venezuela",
-      "link": "https://noticialdia.com/sucesos/al-menos-10-heridos-dejo-explosion-de-carro-bomba-frente-a-una-comisaria-en-region-fronteriza-con-venezuela/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/plantilla-erne-96.webp?x35181"
+      "title": "Rayo provoca incendio en la estación de servicio Bello Monte de Cabimas",
+      "link": "https://noticialdia.com/sucesos/rayo-provoca-incendio-en-la-estacion-de-servicio-bello-monte-de-cabimas/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/v-efe-21.webp?x35181"
     },
     {
       "source": "CNN en Español",
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "UN MUERTO Y CINCO HERIDOS DEJÓ CHOQUE MÚLTIPLE EN LA AUTOPISTA VALLE-COCHE",
-      "link": "https://noticierodigital.com/2026/08/un-muerto-y-cinco-heridos-dejo-choque-multiple-en-la-autopista-valle-coche/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/07/ana-maria-sanjuan-627x376.jpg"
+      "title": "CERRARÁN UN TRAMO DE LA AVENIDA FRANCISCO DE MIRANDA POR LA DEMOLICIÓN DE LA FACHADA DE LA TORRE PARQUE ÁVILA",
+      "link": "https://noticierodigital.com/2026/08/cerraran-un-tramo-de-la-avenida-francisco-de-miranda-por-la-demolicion-de-la-fachada-de-la-torre-parque-avila/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/Destacada-simple_20260801_102630_0000-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Venezuela y Aruba reanudan vuelos a partir de hoy",
-      "link": "https://diarioversionfinal.com/mundo/venezuela-y-aruba-reanudan-vuelos-a-partir-de-hoy/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/08/eX084YFT-image-750x430.jpg"
+      "title": "Trump califica de \"invasión\" la crisis migratoria en Ceuta y critica la respuesta de España",
+      "link": "https://diarioversionfinal.com/mundo/trump-califica-de-invasion-la-crisis-migratoria-en-ceuta-y-critica-la-respuesta-de-espana/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/07/AP26198039288934_president_donald_trump_071626-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-08-01T13:19:55.161Z"
+  "lastUpdate": "2026-08-01T15:00:13.897Z"
 };
