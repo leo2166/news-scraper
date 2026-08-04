a6f45429f6f3d89381aba20e660a3ae2d7e32175
GitHub Action <actions@github.com>
2026-08-04 04:14:41 +0000

    data: update news and rates at Tue Aug  4 04:14:41 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 04 Agosto 2026"
     },
     "binance": {
-      "usdt": "847,00"
+      "usdt": "847,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -21,27 +21,27 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Infobae",
-      "title": "La justicia de Estados Unidos fijó para el 8 de septiembre el juicio contra Alex Saab por lavado de dinero",
-      "link": "https://www.infobae.com/estados-unidos/2026/08/03/la-justicia-de-estados-unidos-fijo-para-el-8-de-septiembre-el-juicio-contra-alex-saab-por-lavado-de-dinero/",
-      "image": "https://www.infobae.com/resizer/v2/4JEAG5F4LZDRRMMKSJXTYXDRZI.JPG?auth=e21082350f625162cbdd28645fc1152e93cf70dab5f0b6864934d3fd0acee3d7&smart=true&width=350&height=197&quality=85"
+      "title": "Expertos de Estados Unidos inspeccionaron la principal hidroeléctrica de Venezuela para definir su modernización",
+      "link": "https://www.infobae.com/venezuela/2026/08/04/expertos-de-estados-unidos-inspeccionaron-la-principal-hidroelectrica-de-venezuela-para-definir-su-modernizacion/",
+      "image": "https://www.infobae.com/resizer/v2/OA53MDZ44JAGBF5QCVVBRRZWMY.png?auth=c8bc1c79157845fa01b98e3c9d01562061d0f042c316dcec5a82b56954e82ee7&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "A 6 mil 125 aumenta la cifra de muertos por el doble terremoto",
-      "link": "https://noticialdia.com/al-dia/a-6-mil-125-aumenta-la-cifra-de-muertos-por-el-doble-terremoto/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/Diseno-sin-titulo-4.webp?x35181"
+      "title": "‎Paso peatonal de Galerías beneficia a 60 mil personas",
+      "link": "https://noticialdia.com/al-dia/paso-peatonal-de-galerias-beneficia-a-60-mil-personas/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/Diseno-sin-titulo-9.webp?x35181"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "EE.UU continuará apoyando la transición democrática institucional de Venezuela",
-      "link": "https://noticiasvenevision.com/noticias/politica/eeuu-continuara-apoyando-la-transicion-democratica-institucional-de-venezuela",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/gemini_generated_image_qnmsubqnmsubqnms-clean-e02410.png"
+      "title": "Venezuela y República Dominicana acuerdan ruta para normalizar relaciones bilaterales",
+      "link": "https://noticiasvenevision.com/noticias/politica/venezuela-y-republica-dominicana-acuerdan-ruta-para-normalizar-relaciones-bilaterales",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/images-555495.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "MAS PROPUSO QUE EL DIÁLOGO ABORDE LA RENOVACIÓN DE LOS PODERES PÚBLICOS",
-      "link": "https://noticierodigital.com/2026/08/mas-propuso-que-el-dialogo-aborde-la-renovacion-de-los-poderes-publicos/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/felipe-mujica-mas-627x376.jpg"
+      "title": "GOBIERNO INICIÓ LA REHABILITACIÓN DE MÁS DE 1.500 ESCUELAS PARA EL NUEVO AÑO ESCOLAR",
+      "link": "https://noticierodigital.com/2026/08/gobierno-inicio-la-rehabilitacion-de-mas-de-1-500-escuelas-para-el-nuevo-ano-escolar/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/hector-rodriguez-rehabilitacion-liceos-venezuela-terremotos-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -51,10 +51,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Justicia por Yexer: protestas en Chile tras asesinato de estudiante venezolano y liberación domiciliaria de implicado",
-      "link": "https://diarioversionfinal.com/mundo/justicia-por-yexer-protestas-en-chile-tras-asesinato-de-estudiante-venezolano-y-liberacion-domiciliaria-de-implicado/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/08/Caso-Yexer-03-08-2026.jpg"
+      "title": "El BID advierte que todavía no puede prestar más dinero a Venezuela",
+      "link": "https://diarioversionfinal.com/mundo/el-bid-advierte-que-todavia-no-puede-prestar-mas-dinero-a-venezuela/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/08/BID-03-04-2026-750x430.jpeg"
     }
   ],
-  "lastUpdate": "2026-08-04T00:14:07.114Z"
+  "lastUpdate": "2026-08-04T04:13:35.988Z"
 };
