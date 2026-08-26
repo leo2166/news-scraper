f63a462853e593a8c07039ae1bbeb916f0ab698d
GitHub Action <actions@github.com>
2026-08-26 22:47:29 +0000

    data: update news and rates at Wed Aug 26 22:47:29 UTC 2026

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 window.DASHBOARD_DATA = {
   "rates": {
     "bcv": {
-      "usd": "787,52",
-      "eur": "919,15",
-      "fechaValor": "Miércoles, 26 Agosto 2026"
+      "usd": "791,32",
+      "eur": "921,81",
+      "fechaValor": "Jueves, 27 Agosto 2026"
     },
     "binance": {
-      "usdt": "948,00"
+      "usdt": "954,50"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,25 +15,25 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "En Puerto de Guanta movilizan 7 mil toneladas de mercancía: reciben piezas para el sector petrolero",
-      "link": "https://www.bancaynegocios.com/en-puerto-de-guanta-movilizan-7-mil-toneladas-de-mercancia-reciben-piezas-para-el-sector-petrolero/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/08/PuertoDeGuantaVenezuela.jpeg"
+      "title": "Vicepresidente de Fedenaga: Tenemos capacidad de producir mucho más y diversificar la economía desde el campo",
+      "link": "https://www.bancaynegocios.com/vicepresidente-de-fedenaga-tenemos-capacidad-de-producir-mucho-mas-y-diversificar-la-economia-desde-el-campo/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/08/WhatsApp-Image-2026-08-26-at-5.13.48-PM.jpeg"
     },
     {
       "source": "Infobae",
-      "title": "Observatorio Venezolano de Prisiones: “Estar enfermo en prisión puede convertirse en una pena de muerte”",
-      "link": "https://www.infobae.com/venezuela/2026/08/26/observatorio-venezolano-de-prisiones-estar-enfermo-en-prision-puede-convertirse-en-una-pena-de-muerte/",
-      "image": "https://www.infobae.com/resizer/v2/UHE54FB3UNBPBBBAEKBCKSNFKA?auth=da8d87dafaef9419f80bdff864543d170a4944cd8c8d8abe4e383a5436a0445d&smart=true&width=350&height=197&quality=85"
+      "title": "Reacomodo militar: oficiales de las ZODI pasan a segundos comandos de las Regiones Estratégicas",
+      "link": "https://www.infobae.com/venezuela/2026/08/26/reacomodo-militar-oficiales-de-las-zodi-pasan-a-segundos-comandos-de-las-regiones-estrategicas/",
+      "image": "https://www.infobae.com/resizer/v2/5PI3UDH4LZG2JJLOK2UGXL5NUU.jpg?auth=68f5bbb087528ec086acc88b0a2abb85bbacdf4a4b67e5759d5a46c00749cf09&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Reportan al menos 15 casas sin electricidad en La Pícola: Familias y pacientes crónicos solicitan atención urgente",
-      "link": "https://noticialdia.com/al-dia/reportan-al-menos-15-casas-sin-electricidad-en-la-picola-familias-y-pacientes-cronicos-solicitan-atencion-urgente/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/v-efe-27.webp?x74346"
+      "title": "Presidenta (E) Delcy Rodríguez lidera el Consejo de Ministros para evaluar el avance de las obras tras el doble terremoto del 24-Jun",
+      "link": "https://noticialdia.com/al-dia/presidenta-e-delcy-rodriguez-lidera-el-consejo-de-ministros-para-evaluar-el-avance-de-las-obras-tras-el-doble-terremoto-del-24-jun/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/v-efe-7-2.webp?x74346"
     },
     {
       "source": "CNN en Español",
-      "title": "Inundaciones catastróficas en China y Nepal dejan al menos 157 muertos",
+      "title": "Inundaciones catastróficas en China y Nepal dejan más de 150 muertos",
       "link": "https://cnnespanol.cnn.com/2026/08/26/mundo/live-news/inundaciones-nepal-china-vivo-victimas-trax",
       "image": "https://media.cnn.com/api/v1/images/stellar/prod/thumb-00-00-10-11-still001.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "DELCY RODRÍGUEZ RECIBIÓ AL NUEVO COORDINADOR RESIDENTE DE LA ONU EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/08/delcy-rodriguez-recibio-al-nuevo-coordinador-residente-de-la-onu-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/Diseno-sin-titulo-3-5-627x376.png"
+      "title": "REESTRUCTURACIÓN DE DEUDA DE VENEZUELA ACTIVA ORGANIZACIÓN DE ACREEDORES COMERCIALES",
+      "link": "https://noticierodigital.com/2026/08/reestructuracion-de-deuda-de-venezuela-activa-organizacion-de-acreedores-comerciales/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/refineria-el-palito-venezuela-pdvsa-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Riada en Nepal deja 160 muertos y más de 700 personas desaparecidas",
-      "link": "https://diarioversionfinal.com/mundo/riada-en-nepal-deja-160-muertos-y-mas-de-700-personas-desaparecidas/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/08/097680920b74e85f8e4be7cf07b00b86b0f4f2a4w-750x375-1.jpg"
+      "title": "El Salvador prorroga régimen de excepción mientras ONG denuncia más muertes en cárceles",
+      "link": "https://diarioversionfinal.com/mundo/el-salvador-prorroga-regimen-de-excepcion-mientras-ong-denuncia-mas-muertes-en-carceles/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/05/El-Salvador-presos-salvadorenos-0605-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-08-26T19:01:03.969Z"
+  "lastUpdate": "2026-08-26T22:46:29.362Z"
 };
