8f3e6ff70a05621dac34b98104fe37393643a258
GitHub Action <actions@github.com>
2026-09-01 01:42:20 +0000

    data: update news and rates at Tue Sep  1 01:42:20 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 01 Septiembre 2026"
     },
     "binance": {
-      "usdt": "930,00"
+      "usdt": "939,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,39 +15,39 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Consecomercio reduce proyección de crecimiento económico hasta un 5% por desaceleración del consumo y fallas eléctricas",
-      "link": "https://www.bancaynegocios.com/consecomercio-reduce-proyeccion-de-crecimiento-economico-hasta-un-5-porciento-por-desaceleracion-del-consumo-y-fallas-electricas/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/Jose-Gregorio-Rodriguez-Pdte-Consecomercio-NVA.jpg"
+      "title": "Pagan Bono Especial de Vacaciones 2026 para docentes estadales (+Detalles)",
+      "link": "https://www.bancaynegocios.com/pagan-bono-especial-de-vacaciones-2026-para-docentes-estadales-detalles/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/bolivares-e1745959276971.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Productores agrícolas piden a Delcy Rodríguez eliminar la presión fiscal que atenta contra las inversiones en el campo",
-      "link": "https://www.infobae.com/venezuela/2026/08/31/productores-agricolas-piden-a-delcy-rodriguez-eliminar-la-presion-fiscal-que-atenta-contra-las-inversiones-en-el-campo/",
-      "image": "https://www.infobae.com/resizer/v2/AI2BH5UW5XXVLTR6B6BTVHV6O4.jpg?auth=3e7cc84ff6800e4299a5256fb3f4577d57140901b35b394a297672185d786326&smart=true&width=350&height=197&quality=85"
+      "title": "Venezuela reactivará este martes su principal aeropuerto tras más de dos meses paralizado por los devastadores terremotos",
+      "link": "https://www.infobae.com/venezuela/2026/08/31/venezuela-reactivara-este-martes-su-principal-aeropuerto-tras-mas-de-dos-meses-paralizado-por-los-devastadores-terremotos/",
+      "image": "https://www.infobae.com/resizer/v2/H66X5HZ7W5GMTMELQUV23BTG4U.JPG?auth=8d6aea791e0844b89222a603df516906bd929046305ac40663c5e762b4bb80ac&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Trump afirma que ExxonMobile \"entrará\" en Venezuela mientras EEUU impulsa acuerdos petroleros",
-      "link": "https://noticialdia.com/al-dia/trump-afirma-que-exxonmobile-entrara-en-venezuela-mientras-eeuu-impulsa-acuerdos-petroleros/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/Diseno-sin-titulo-7-19.webp?x74346"
+      "title": "Barcelona remonta y golea al Rayo Vallecano para recuperar el liderato de LaLiga",
+      "link": "https://noticialdia.com/deportes/barcelona-remonta-y-golea-al-rayo-vallecano-para-recuperar-el-liderato-de-laliga/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/1_20260831_190743_0000.webp?x74346"
     },
     {
       "source": "CNN en Español",
-      "title": "Unidad de ICE intensificará investigaciones de fraude electoral en nueve estados",
-      "link": "https://cnnespanol.cnn.com/2026/08/31/eeuu/exclusiva-dhs-investigaciones-fraude-electoral-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2282426180-20260831191719451.jpg?c=16x9&q=h_720,w_1280,c_fill"
+      "title": "¿Prosperará el acuerdo petrolero entre EE.UU. y Venezuela?",
+      "link": "https://cnnespanol.cnn.com/2026/08/31/venezuela/acuerdo-petrolero-trump-agridulce-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2257950778.jpg?c=16x9&q=h_720,w_1280,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Academia de Ciencias Políticas y Sociales alerta sobre inconstitucionalidad en la reforma de la Ley del TSJ",
-      "link": "https://noticiasvenevision.com/noticias/politica/academia-de-ciencias-politicas-y-sociales-alerta-sobre-inconstitucionalidad-en-la-reforma-de-la-ley-del-tsj",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/gemini_generated_image_9y5sdh9y5sdh9y5s-e25ae9.png"
+      "title": "Delcy Rodríguez lidera reunión del PSUV para evaluar Plan Venezuela Renace",
+      "link": "https://noticiasvenevision.com/noticias/politica/delcy-rodriguez-lidera-reunion-del-psuv-para-evaluar-plan-venezuela-renace",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/photo_4949958880070929697_y-7b7023.jpg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "ASDRÚBAL OLIVEROS: «LA CONFIANZA MONETARIA NO DEBERÍA IMPONERSE; DEBERÍA GANARSE»",
-      "link": "https://noticierodigital.com/2026/08/asdrubal-oliveros-la-confianza-monetaria-no-deberia-imponerse-deberia-ganarse/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2023/02/asdrubaloliveros-bolivar-dolar-8feb2023.jpg"
+      "title": "DELCY RODRÍGUEZ INSPECCIONÓ REPARACIONES DE VIVIENDAS AFECTADAS EN ALTAMIRA",
+      "link": "https://noticierodigital.com/2026/08/delcy-rodriguez-inspecciono-reparaciones-de-viviendas-afectadas-en-altamira/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/08/delcy-rodriguez-venezuela-3-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "Cientos de obreros sobreviven en túneles de una hidroeléctrica en Nepal",
-      "link": "https://diarioversionfinal.com/mundo/cientos-de-obreros-sobreviven-en-tuneles-de-una-hidroelectrica-en-nepal/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/08/Tuneles-Nepal-31-08-2026-750x430.webp"
+      "title": "Acuerdo petrolero garantizará crudo de bajo costo para EE. UU.",
+      "link": "https://diarioversionfinal.com/mundo/acuerdo-petrolero-garantizara-crudo-de-bajo-costo-para-ee-uu/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/02/Balancin-de-petroleo-en-Cabimas-Venezuela-750x430.webp"
     }
   ],
-  "lastUpdate": "2026-08-31T22:33:21.322Z"
+  "lastUpdate": "2026-09-01T01:41:20.307Z"
 };
