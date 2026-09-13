1ee137118c2d27edd345d54e80b20519ba9e2828
GitHub Action <actions@github.com>
2026-09-13 17:56:53 +0000

    data: update news and rates at Sun Sep 13 17:56:53 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 15 Septiembre 2026"
     },
     "binance": {
-      "usdt": "957,00"
+      "usdt": "959,20"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,52 +15,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Las 4 áreas del \"Plan de Inversión y Empleo\" de Fedecámaras para la reconstrucción de la economía",
-      "link": "https://www.bancaynegocios.com/las-4-areas-del-plan-de-inversion-y-empleo-de-fedecamaras-para-la-reconstruccion-de-la-economia/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2025/04/bolivares-e1745959276971.jpg"
+      "title": "Es probable que en los próximos meses \"sí veamos un ajuste\" salarial en el país, según economista",
+      "link": "https://www.bancaynegocios.com/es-probable-que-en-los-proximos-meses-si-veamos-un-ajuste-salarial-en-el-pais-segun-economista/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/bolivares-dolares.webp"
     },
     {
       "source": "Infobae",
-      "title": "Los familiares de presos políticos exigieron a Delcy Rodríguez nuevas liberaciones en Caracas",
-      "link": "https://www.infobae.com/venezuela/2026/09/11/los-familiares-de-presos-politicos-exigieron-a-delcy-rodriguez-nuevas-liberaciones-en-caracas/",
-      "image": "https://www.infobae.com/resizer/v2/NNU6TA3LSNEVBDB5KJTQU257RE.JPG?auth=0f6b7eebd975feb97b1d2c2ed4f4f1e7d72f9b8eebfd22bb3919f1abfbfe2c5c&smart=true&width=350&height=197&quality=85"
+      "title": "Un ex parlamentario venezolano aseguró que la industria petrolera nacional “necesita reglas claras y confianza institucional”",
+      "link": "https://www.infobae.com/venezuela/2026/09/13/un-ex-parlamentario-venezolano-aseguro-que-la-industria-petrolera-nacional-necesita-reglas-claras-y-confianza-institucional/",
+      "image": "https://www.infobae.com/resizer/v2/EVI2VMYVQBDMVGA473O6SOQXJ4.jpeg?auth=361d3a7ff150a896dc62d117551d3f34f85274b7c820e364d599441b01951e3d&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Se registró doble sismo en Yaracuy este 12-S",
-      "link": "https://noticialdia.com/sucesos/se-registro-doble-sismo-en-yaracuy-este-12-s/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/plantilla-erne-32-4.webp?x74346"
+      "title": "Le arrancó la oreja de un mordisco a su agresor para defenderse: Buscan a exfuncionario por propinarle salvaje golpiza a su pareja en San Jacinto",
+      "link": "https://noticialdia.com/al-dia/le-arranco-la-oreja-de-un-mordisco-a-su-agresor-para-defenderse-buscan-a-exfuncionario-por-propinarle-salvaje-golpiza-a-su-pareja-en-san-jacinto/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/v-efe-48.webp?x74346"
     },
     {
       "source": "CNN en Español",
-      "title": "CEO de Anthropic responde a CNN si la IA podría matar a toda la humanidad",
-      "link": "https://cnnespanol.cnn.com/2026/09/12/ciencia/video/cnn-entrevista-ceo-anthropic-ia-humanidad-peligro-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/sequence-01-00-00-58-19-still001.png?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Esta familia mariachi vivió un “calvario” tras ser detenida por ICE",
+      "link": "https://cnnespanol.cnn.com/2026/09/13/eeuu/familia-mariachi-calvario-detenida-ice-grillete-apoyo-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2221277439.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Defensora del Pueblo afirma que sistema de justicia debe adaptarse a realidad pluricultural del país",
-      "link": "https://noticiasvenevision.com/noticias/politica/defensora-del-pueblo-afirma-que-sistema-de-justicia-debe-adaptarse-a-realidad-pluricultural-del-pais",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/img_1150-41beec.JPG"
+      "title": "Yon Goicoechea: \"Tenemos que construir consensos de país para tener una justicia adecuada, pero no es que tenemos que esperar que todo se solucione para que haya una elección\"",
+      "link": "https://noticiasvenevision.com/noticias/politica/yon-goicoechea-tenemos-que-construir-consensos-de-pais-para-tener-una-justicia-adecuada-pero-no-es-que-tenemos-que-esperar-que-todo-se-solucione-para-que-haya-una-eleccion",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/vv-media-venevision-abriendo-puertas-yon-goicoechea-13sep2026-641abc.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "EXALCALDE DE MARACAIBO EXIGE LA LIBERTAD DEL EMPRESARIO JOSÉ ENRIQUE RINCÓN",
-      "link": "https://noticierodigital.com/2026/09/exalcalde-de-maracaibo-exige-la-libertad-del-empresario-jose-enrique-rincon/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2024/10/rafael-ramirez-colina.jpg"
+      "title": "JESUS PALACIOS: «DOLARIZAR NO RESUELVE EL PROBLEMA DEL ALTO COSTO DE LA VIDA EN VENEZUELA»",
+      "link": "https://noticierodigital.com/2026/09/jesus-palacios-dolarizar-no-resuelve-el-problema-del-alto-costo-de-la-vida-en-venezuela/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/11/pago-en-dolares-referencial-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Capturan en Costa Rica a criminal panameño vinculado al “Tren de Aragua”",
-      "link": "https://laverdad.com/capturan-en-costa-rica-a-criminal-panameno-vinculado-al-tren-de-aragua/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/09/Capturan-en-Costa-Rica-a-criminal-panameno-vinculado-al-Tren-de-Aragua-tras-seis-anos-en-fuga.jpg"
+      "title": "Papa León XIV recuerda a las víctimas del 11 de septiembre y pide rezar por la paz",
+      "link": "https://laverdad.com/papa-leon-xiv-recuerda-a-las-victimas-del-11-de-septiembre-y-pide-rezar-por-la-paz/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/09/62a329bd0ad88600a872838aa1214352b5e1220c-scaled.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Nueva York en alerta por fuertes lluvias y posibles inundaciones",
-      "link": "https://diarioversionfinal.com/mundo/nueva-york-en-alerta-por-fuertes-lluvias-y-posibles-inundaciones/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/rkPHMfw4-image-750x430.jpg"
+      "title": "Mike Johnson promete consenso en el Congreso para cheque de Trump",
+      "link": "https://diarioversionfinal.com/mundo/mike-johnson-promete-consenso-en-el-congreso-para-cheque-de-trump/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/okmvR5m0-image-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-09-13T14:05:21.399Z"
+  "lastUpdate": "2026-09-13T17:55:55.185Z"
 };
