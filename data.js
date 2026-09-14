75758fdff356655e5ea4d16996755ad0afcba65a
GitHub Action <actions@github.com>
2026-09-14 21:45:33 +0000

    data: update news and rates at Mon Sep 14 21:45:33 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Martes, 15 Septiembre 2026"
     },
     "binance": {
-      "usdt": "957,90"
+      "usdt": "952,35"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,27 +15,27 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Fedecámaras: Volumen de negocios empresariales se ralentiza con alza de 0,5% en el segundo trimestre",
-      "link": "https://www.bancaynegocios.com/fedecamaras-volumen-de-negocios-empresariales-se-ralentiza-con-alza-de-0coma5-porciento-en-el-segundo-trimestre/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/08/TrabajadoresConstruccionVenezuela.jpg"
+      "title": "Asocav anticipa que Venezuela cerrará 2026 con crecimiento menor al 2% en importaciones",
+      "link": "https://www.bancaynegocios.com/asocav-anticipa-que-venezuela-cerrara-2026-con-crecimiento-menor-al-2-porciento-en-importaciones/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/PuertoDeLaGuairaMaquinasContenedores.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Federación de Maestros de Venezuela: las escuelas carecen de servicios y los salarios son de hambre",
-      "link": "https://www.infobae.com/venezuela/2026/09/14/federacion-de-maestros-de-venezuela-las-escuelas-carecen-de-servicios-y-los-salarios-son-de-hambre/",
-      "image": "https://www.infobae.com/resizer/v2/NTM4BQFU7BGL5DTVB6LQY6YDP4.jpg?auth=6b210d6dd12a382cf14e485e0c84aa8e865c3e249eb63b104b35a51e745bc9f4&smart=true&width=350&height=197&quality=85"
+      "title": "Una nueva alianza opositora venezolana exige elecciones presidenciales con María Corina Machado como candidata",
+      "link": "https://www.infobae.com/venezuela/2026/09/14/una-nueva-alianza-opositora-venezolana-exige-elecciones-presidenciales-con-maria-corina-machado-como-candidata/",
+      "image": "https://www.infobae.com/resizer/v2/XNVIRQ7GOFEP7ICCMI7UJQBM5Q.JPG?auth=4126e67f18a522491ff9a10c3208ae0a90fee77a2ad80cec0e0d7dcab1a21dbd&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Pablo López es el único venezolano nominado al premio Roberto Clemente 2026",
-      "link": "https://noticialdia.com/deportes/pablo-lopez-es-el-unico-venezolano-nominado-al-premio-roberto-clemente-2026/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/WhatsApp-Image-2026-09-14-at-1.54.18-PM.webp?x74346"
+      "title": "En diciembre: Ni \"bajará Pacheco\" ni \"hielitos decembrinos\"",
+      "link": "https://noticialdia.com/nacionales/en-diciembre-ni-bajara-pacheco-ni-hielitos-decembrinos/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/Diseno-sin-titulo-2026-09-14T124343.722.webp?x74346"
     },
     {
       "source": "CNN en Español",
       "title": "Más de 20 estados demandan al Gobierno de Trump por nueva norma de inmigración",
       "link": "https://cnnespanol.cnn.com/2026/09/14/eeuu/green-cards-estados-demanda-trump-inmigrante-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/mamdani-still-22982858-1538040-602-still.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/ap26257587696295-20260914190228364.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -45,9 +45,9 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "ASDRÚBAL OLIVEROS ALERTÓ QUE BCV «FRENA FINANCIAMIENTO PARA CONTENER EL DÓLAR»",
-      "link": "https://noticierodigital.com/2026/09/asdrubal-oliveros-alerto-que-bcv-frena-financiamiento-para-contener-el-dolar/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2021/08/asdrubaloliveros-venezuelapasodepresion-a-estancamiento-31ago2021.jpg"
+      "title": "EE. UU. EVALÚA INCLUIR EL CARBÓN VENEZOLANO EN FUTUROS ACUERDOS ENERGÉTICOS",
+      "link": "https://noticierodigital.com/2026/09/ee-uu-evalua-incluir-el-carbon-venezolano-en-futuros-acuerdos-energeticos/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/03/doug-burgum-eeuu-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -57,10 +57,10 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Diario Versión Final",
-      "title": "De la Espriella advierte sobre el vandalismo en protestas universitarias",
-      "link": "https://diarioversionfinal.com/mundo/de-la-espriella-advierte-sobre-el-vandalismo-en-protestas-universitarias/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/9E5CISjx-image-750x430.jpg"
+      "title": "Al-Taawoun bajo la lupa por cánticos sobre Diogo Jota",
+      "link": "https://diarioversionfinal.com/mundo/al-taawoun-bajo-la-lupa-por-canticos-sobre-diogo-jota/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2025/07/Diogo-Jota-04-07-2025-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-09-14T17:58:39.556Z"
+  "lastUpdate": "2026-09-14T21:44:31.719Z"
 };
