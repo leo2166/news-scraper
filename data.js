9593dc67d709f211289c8292a0f3648578a7e4b1
GitHub Action <actions@github.com>
2026-09-16 03:38:29 +0000

    data: update news and rates at Wed Sep 16 03:38:29 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Miércoles, 16 Septiembre 2026"
     },
     "binance": {
-      "usdt": "956,00"
+      "usdt": "958,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -27,27 +27,27 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticia al Día",
-      "title": "James Rodríguez anuncia su retiro de la selección de Colombia",
-      "link": "https://noticialdia.com/deportes/james-rodriguez-anuncia-su-retiro-de-la-seleccion-de-colombia/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/Diseno-sin-titulo_20260915_184210_0000.webp?x74346"
+      "title": "Fallece el periodista deportivo Juan Vené a los 97 años",
+      "link": "https://noticialdia.com/deportes/fallece-el-periodista-deportivo-juan-vene-a-los-97-anos/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/v-efe-11-3.webp?x74346"
     },
     {
       "source": "CNN en Español",
-      "title": "Alex Saab se declara culpable de lavado de activos en EE.UU.",
-      "link": "https://cnnespanol.cnn.com/2026/09/15/eeuu/alex-saab-exministro-venezueladeclara-culpable-lavado-orix",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/82428afc14035e9f2922ece502cc3e7258d41ebb.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Sheinbaum afianza su gobierno pese a las contradicciones",
+      "link": "https://cnnespanol.cnn.com/2026/09/15/mexico/dos-caras-claudia-sheinbaum-gobierno-contradicciones-orix",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2293789177.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "Venezuela y Reino Unido acuerdan elevar las relaciones diplomáticas",
-      "link": "https://noticiasvenevision.com/noticias/politica/venezuela-y-reino-unido-acuerdan-elevar-las-relaciones-diplomaticas",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/images-8-8f4cda.jpg"
+      "title": "Venezuela recibe al nuevo embajador de la Unión Europea",
+      "link": "https://noticiasvenevision.com/noticias/politica/venezuela-recibe-al-nuevo-embajador-de-la-union-europea-en-caracas",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/x36ei5pa7fbyzjmgqpqozzskuy-77ddf0.avif"
     },
     {
       "source": "Noticiero Digital",
-      "title": "PJ EXIGIÓ QUE LOS RESPONSABLES DEL CASO ALEX SAAB «RINDAN CUENTAS ANTE LA JUSTICIA»",
-      "link": "https://noticierodigital.com/2026/09/pj-exigio-que-los-responsables-del-caso-alex-saab-rindan-cuentas-ante-la-justicia/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/02/primero_justicia_16dic-1-627x376.jpg"
+      "title": "SISMO DE MAGNITUD 3,8 SACUDIÓ ESTE MARTES EL ESTADO FALCÓN",
+      "link": "https://noticierodigital.com/2026/09/sismo-de-magnitud-38-sacudio-este-martes-el-estado-falcon/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/09/sismo-funvisis-churuguara-627x376.jpg"
     },
     {
       "source": "La Verdad",
@@ -62,5 +62,5 @@ window.DASHBOARD_DATA = {
       "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/2saAzlxa-image-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-09-15T23:26:16.336Z"
+  "lastUpdate": "2026-09-16T03:37:31.423Z"
 };
