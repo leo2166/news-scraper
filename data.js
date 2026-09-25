1cd86ba52e4ff210e22fde873ddcf9fdd4f32740
GitHub Action <actions@github.com>
2026-09-25 16:42:55 +0000

    data: update news and rates at Fri Sep 25 16:42:55 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Viernes, 25 Septiembre 2026"
     },
     "binance": {
-      "usdt": "968,00"
+      "usdt": "963,00"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,52 +15,52 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Se restablecen más del 97% de las operaciones en Puerto La Guaira, afirma ministro",
-      "link": "https://www.bancaynegocios.com/se-restablecen-mas-del-97-porciento-de-las-operaciones-en-puerto-la-guaira-afirma-ministro/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/07/PuertoDeLaGuairaMaquinasContenedores.jpg"
+      "title": "Proponen que se discuta en la AN una ley para la recuperación e indemnización a todos los habitantes de La Guaira",
+      "link": "https://www.bancaynegocios.com/proponen-que-se-discuta-en-la-an-una-ley-para-la-recuperacion-e-indemnizacion-a-todos-los-habitantes-de-la-guaira/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/AsambleaNacional.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Venezuela anunció la recuperación casi total de las operaciones en el puerto de La Guaira tras los daños del doble terremoto",
-      "link": "https://www.infobae.com/venezuela/2026/09/25/venezuela-anuncio-la-recuperacion-casi-total-de-las-operaciones-en-el-puerto-de-la-guaira-tras-los-danos-del-doble-terremoto/",
-      "image": "https://www.infobae.com/resizer/v2/CNUR52US2BGDTOZ3YXDZCGTO3Y.jfif?auth=9580f1f4656bcc0e6e5d7c3d81ae40d263344a65044c12d7110dd1604982a8d1&smart=true&width=350&height=197&quality=85"
+      "title": "Oposición exige a Delcy Rodríguez que convierta su discurso en hechos concretos y que llame a elecciones",
+      "link": "https://www.infobae.com/venezuela/2026/09/25/oposicion-exige-a-delcy-rodriguez-que-convierta-su-discurso-en-hechos-concretos-y-que-llame-a-elecciones/",
+      "image": "https://www.infobae.com/resizer/v2/NLA77PKFFZASNFK323YCUORFO4.jpg?auth=000ce98807c8022da600896035b9d76861b4b43e05485246862956f18b3f6f39&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Sello venezolano: Jackson Chourio y Antonio Senzatela guían la victoria 100 de Cerveceros en la temporada",
-      "link": "https://noticialdia.com/deportes/sello-venezolano-jackson-chourio-y-antonio-senzatela-guian-la-victoria-100-de-cerveceros-en-la-temporada/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/Diseno-sin-titulo_20260924_235944_0000.webp?x74346"
+      "title": "Polisur fortalece las comunicaciones tácticas y habilita portal web para atención directa a los ciudadanos",
+      "link": "https://noticialdia.com/?p=673372",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/08/Diseno-sin-titulo_20260817_141640_0000.webp"
     },
     {
       "source": "CNN en Español",
-      "title": "Trump parece ofrecer lo opuesto a lo que la mayoría en EE.UU. desean",
-      "link": "https://cnnespanol.cnn.com/2026/09/25/eeuu/estrategia-trump-elecciones-mitad-mandato-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2288266917-20260924235807332-20260925045345204.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Corte Suprema reactiva herramienta sobre datos de ciudadanía para auditorías de votantes",
+      "link": "https://cnnespanol.cnn.com/2026/09/25/eeuu/trump-corte-suprema-datos-ciudadania-votantes-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/corte-suprema-c-ap26237600924836.jpg?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
-      "title": "AN2015 recibe propuesta para incorporar a universidades en hoja de ruta para reinstitucionalización del país",
-      "link": "https://noticiasvenevision.com/noticias/politica/an2015-recibe-propuesta-para-incorporar-a-universidades-en-hoja-de-ruta-para-reinstitucionalizacion-del-pais",
-      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/7ac6676e-e144-44e7-965e-575254d8864e-285e10.jpg"
+      "title": "Presidenta (E) Delcy Rodríguez atribuye fallas eléctricas al impacto de la ola de calor",
+      "link": "https://noticiasvenevision.com/noticias/politica/delcy-rodriguez-atribuye-apagones-a-ola-de-calor-y-pico-de-demanda-electrica-de-16400-mw",
+      "image": "https://w2a-noticierovenevision-net.s3.amazonaws.com/public/media/images/whatsapp-image-2026-09-25-at-6.23.33-am-827027.jpeg"
     },
     {
       "source": "Noticiero Digital",
-      "title": "DANIEL NOBOA PIDIÓ DEFINIR UNA FECHA PARA ELECCIONES LIBRES EN VENEZUELA",
-      "link": "https://noticierodigital.com/2026/09/daniel-noboa-pidio-definir-una-fecha-para-elecciones-libres-en-venezuela/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2025/01/daniel-noboa-3524-627x376.jpg"
+      "title": "BENJAMÍN RAUSSEO SUFRIÓ UN EPISODIO CARDIOVASCULAR EN ANZOÁTEGUI",
+      "link": "https://noticierodigital.com/2026/09/benjamin-rausseo-sufrio-un-episodio-cardiovascular-en-anzoategui/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2025/04/benjamin-rausseo-2-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Trump ofrece a Xi Jinping trabajar para un “futuro mejor”",
-      "link": "https://laverdad.com/trump-ofrece-a-xi-jinping-trabajar-para-un-futuro-mejor/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/09/Donald-Trump-Xi-Jinping-Casa-Blanca.jpg"
+      "title": "León XIV reúne a 250.000 personas en su multitudinario recorrido por París",
+      "link": "https://laverdad.com/leon-xiv-reune-a-250-000-personas-en-su-multitudinario-recorrido-por-paris/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/09/papa-leon-XVI-scaled.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Exfuncionario chavista en Sucre es el exdiplomático detenido en España con droga",
-      "link": "https://diarioversionfinal.com/mundo/exfuncionario-chavista-en-sucre-es-el-exdiplomatico-detenido-en-espana-con-droga/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/Jose-Escorihuela-24-09-2026-1-640x430.jpg"
+      "title": "Avance en comercio: EE.UU. y China acuerdan nuevos términos para bienes",
+      "link": "https://diarioversionfinal.com/mundo/avance-en-comercio-ee-uu-y-china-acuerdan-nuevos-terminos-para-bienes/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/bozjxyUs-image-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-09-25T11:17:39.211Z"
+  "lastUpdate": "2026-09-25T16:41:55.387Z"
 };
