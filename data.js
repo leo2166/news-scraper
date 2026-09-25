47c9637fafa210a3715ffbba112a3e0930c994ac
GitHub Action <actions@github.com>
2026-09-25 23:53:00 +0000

    data: update news and rates at Fri Sep 25 23:53:00 UTC 2026

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.DASHBOARD_DATA = {
       "fechaValor": "Lunes, 28 Septiembre 2026"
     },
     "binance": {
-      "usdt": "964,80"
+      "usdt": "967,60"
     },
     "kontigo": {
       "usd": "N/A"
@@ -15,27 +15,27 @@ window.DASHBOARD_DATA = {
   "news": [
     {
       "source": "Banca y Negocios",
-      "title": "Proponen que se discuta en la AN una ley para la recuperación e indemnización a todos los habitantes de La Guaira",
-      "link": "https://www.bancaynegocios.com/proponen-que-se-discuta-en-la-an-una-ley-para-la-recuperacion-e-indemnizacion-a-todos-los-habitantes-de-la-guaira/",
-      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/03/AsambleaNacional.jpg"
+      "title": "Créditos entregados a comerciantes formales en La Guaira suman US$20 millones, indica gobernador",
+      "link": "https://www.bancaynegocios.com/creditos-entregados-a-comerciantes-formales-en-la-guaira-suman-us20-millones-indica-gobernador/",
+      "image": "https://www.bancaynegocios.com/wp-content/uploads/2026/09/comercios-de-la-guaira.jpg"
     },
     {
       "source": "Infobae",
-      "title": "Oposición exige a Delcy Rodríguez que convierta su discurso en hechos concretos y que llame a elecciones",
-      "link": "https://www.infobae.com/venezuela/2026/09/25/oposicion-exige-a-delcy-rodriguez-que-convierta-su-discurso-en-hechos-concretos-y-que-llame-a-elecciones/",
-      "image": "https://www.infobae.com/resizer/v2/NLA77PKFFZASNFK323YCUORFO4.jpg?auth=000ce98807c8022da600896035b9d76861b4b43e05485246862956f18b3f6f39&smart=true&width=350&height=197&quality=85"
+      "title": "El dirigente opositor venezolano Enrique Márquez exigió al Gobierno un plan de emergencia para reducir los apagones",
+      "link": "https://www.infobae.com/venezuela/2026/09/25/el-dirigente-opositor-venezolano-enrique-marquez-exigio-al-gobierno-un-plan-de-emergencia-para-reducir-los-apagones/",
+      "image": "https://www.infobae.com/resizer/v2/7JNQ34PBURBF5MIXGX2TWHQXCI.JPG?auth=f89291ff7ef2f6c04338aaeda18a8dd58a8ec8411865fd2baeee7182cf29e0b2&smart=true&width=350&height=197&quality=85"
     },
     {
       "source": "Noticia al Día",
-      "title": "Desde el 17 de noviembre: Nueva ruta aérea conectará a Maracaibo con Riohacha",
-      "link": "https://noticialdia.com/principal/desde-el-17-de-noviembre-nueva-ruta-aerea-conectara-a-maracaibo-con-riohacha/",
-      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/aeropuerto-rio-hacha.webp?x74346"
+      "title": "Gobernación benefició a 34 pacientes con cateterismo y angioplastia a través del ‘Plan Corazon Zuliano’",
+      "link": "https://noticialdia.com/al-dia/gobernacion-beneficio-a-34-pacientes-con-cateterismo-y-angioplastia-a-traves-del-plan-corazon-zuliano/",
+      "image": "https://noticialdia.com/wp-content/firewall/uploads/2026/09/Diseno-sin-titulo-9-17.webp?x74346"
     },
     {
       "source": "CNN en Español",
-      "title": "Corte Suprema reactiva herramienta sobre datos de ciudadanía para auditorías de votantes",
-      "link": "https://cnnespanol.cnn.com/2026/09/25/eeuu/trump-corte-suprema-datos-ciudadania-votantes-trax",
-      "image": "https://media.cnn.com/api/v1/images/stellar/prod/corte-suprema-c-ap26237600924836.jpg?c=16x9&q=h_438,w_780,c_fill"
+      "title": "Las elecciones de 2026 podrían ser las peores del siglo para los republicanos",
+      "link": "https://cnnespanol.cnn.com/2026/09/25/eeuu/elecciones-intermedias-peores-partido-republicano-trax",
+      "image": "https://media.cnn.com/api/v1/images/stellar/prod/screenshot-2026-09-25-132324.png?c=16x9&q=h_438,w_780,c_fill"
     },
     {
       "source": "Noticias Venevisión",
@@ -45,22 +45,22 @@ window.DASHBOARD_DATA = {
     },
     {
       "source": "Noticiero Digital",
-      "title": "ENRIQUE MÁRQUEZ EXIGE AL GOBIERNO QUE HABLE CLARO A LOS VENEZOLANOS: «¿CUÁL ES EL PLAN PARA RECUPERAR EL SISTEMA ELÉCTRICO?»",
-      "link": "https://noticierodigital.com/2026/09/enrique-marquez-exige-al-gobierno-que-hable-claro-a-los-venezolanos-cual-es-el-plan-para-recuperar-el-sistema-electrico/",
-      "image": "https://noticierodigital.com/wp-content/uploads/2026/02/0b23bddd98a72ffe8913602794e31a3a991b6db2w-627x376.jpg"
+      "title": "GOBIERNO Y AN 2015 MANTIENEN SEGUNDA RONDA DE DIÁLOGO EN CARACAS",
+      "link": "https://noticierodigital.com/2026/09/gobierno-y-an-2015-mantienen-segunda-ronda-de-dialogo-en-caracas/",
+      "image": "https://noticierodigital.com/wp-content/uploads/2026/09/dialogo-politico-venezuela-627x376.jpg"
     },
     {
       "source": "La Verdad",
-      "title": "Tribunal Supremo permite al gobierno de Trump aplicar un sistema de control de votantes",
-      "link": "https://laverdad.com/tribunal-supremo-permite-al-gobierno-de-trump-aplicar-un-sistema-de-control-de-votantes/",
-      "image": "https://laverdad.com/wp-content/uploads/2026/09/elecciones-legislativas-EE.-UU.jpg"
+      "title": "Asesinan a tiros a 2 policías en la región colombiana del Catatumbo",
+      "link": "https://laverdad.com/asesinan-a-tiros-a-2-policias-en-la-region-colombiana-del-catatumbo/",
+      "image": "https://laverdad.com/wp-content/uploads/2026/09/Asesinan-a-tiros-a-2-policias-en-la-region-colombiana-del-Catatumbo.jpg"
     },
     {
       "source": "Diario Versión Final",
-      "title": "Avance en comercio: EE.UU. y China acuerdan nuevos términos para bienes",
-      "link": "https://diarioversionfinal.com/mundo/avance-en-comercio-ee-uu-y-china-acuerdan-nuevos-terminos-para-bienes/",
-      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/bozjxyUs-image-750x430.jpg"
+      "title": "La lucha contra el antisemitismo debe ser prioridad del nuevo secretario de la ONU",
+      "link": "https://diarioversionfinal.com/mundo/la-lucha-contra-el-antisemitismo-debe-ser-prioridad-del-nuevo-secretario-de-la-onu/",
+      "image": "https://media.diarioversionfinal.com/wp-content/uploads/2026/09/NEdSavZK-image-750x430.jpg"
     }
   ],
-  "lastUpdate": "2026-09-25T20:17:35.301Z"
+  "lastUpdate": "2026-09-25T23:52:02.682Z"
 };
